f26bc10e964217a7c059abe4805c448b4f4cd24f
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-22 15:01:02 -0400

    Added const.go

---
+++ cmd/util/const.go
@@ -0,0 +1,8 @@
+package util
+
+const (
+	// "clientIdentifier" holds the Pocket Network Client Identifier.
+	clientIdentifier = "pocket_core"
+	// "version" holds the Pocket Core Client version.
+	version = "0.0.1"
+)
