62f51d8b5bb68c9198db4ece5871c0ebc78e3a42
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-22 11:54:29 -0400

    Changed 'utili' to 'util'

--- cmd/pocket_core/main.go
+++ cmd/pocket_core/main.go
@@ -1,17 +1,19 @@
 // Pocket Core: This is the starting point of the CLI.
 package main
 
-import (
-	"github.com/pocket_network/pocket-core/cmd/util"
+import "fmt"
+
+const (
+	clientIdentifier = "pocket_core"
+	version = "0.0.1"
 )
 
-//TODO add logging
-
-/*
-"Main" is the starting function of the client.
- Keep main as light as possible by calling accessory functions.
-*/
 func main() {
-	util.ParseFlags()
-	util.CLI_Test()
+	welcome()
+}
+
+func welcome(){
+	fmt.Println("Client:\t\t", clientIdentifier)
+	fmt.Println("Version:\t", version)
+
 }
