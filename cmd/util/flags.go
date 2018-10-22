4beb244ab14ea51374d367bab6c75f505835a202
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-22 14:55:35 -0400

    Created basic CLI and flags

--- cmd/util/flags.go
+++ cmd/util/flags.go
@@ -4,61 +4,56 @@ package util
 import (
 	"flag"
 	"fmt"
-
-	"github.com/pocket_network/pocket-core/rpc"
 )
 
-// flags.go specifies startup command flags for the client
-
+const (
+	// "clientIdentifier" holds the Pocket Network Client Identifier.
+	clientIdentifier = "pocket_core"
+	// "version" holds the Pocket Core Client version.
+	version = "0.0.1"
+)
 var (
 	// A string variable derived from flags, used to setup data directory of Pocket Core.
 	datadir = flag.String("datadir", "/path/to/data/dir", "setup the data directory for the DB and keystore")
 	// A boolean variable derived from flags, that describes whether or not to print the version of the client.
 	print_version = flag.Bool("version", false, "whether or not to print the version of the client")
-	// A boolean variable derived from flags, that describes whether or not to start the client rpc server.
-	client_rpc = flag.Bool("clientrpc", false, "whether or not to start the rpc server")
-	// A string variable derived from flags, that specifies which port to run the listener for the client rpc (default :8545)
-	client_rpcport = flag.String("clientrpcport", "8545", "specified port to run rpc")
-	// A boolean variable derived from flags, that describes whether or not to start the relay rpc server.
-	relay_rpc = flag.Bool("relayrpc", false, "whether or not to start the rpc server")
-	// A string variable derived from flags, that specifies which port to run the listener for the relay rpc (default :8546)
-	relay_rpcport = flag.String("relayrpcport", "8546", "specified port to run rpc")
-	// A boolean variable derived from flags, that describes whether or not to start the web sockets server
+	// A boolean variable derived from flags, that describes whether or not to start the rpc server.
+	rpc = flag.Bool("rpc", false, "whether or not to start the rpc server")
+	// A string variable derived from flags, that specifies which port to run the listener for the rpc (default :8545)
+	rpcport = flag.String("rpcport", "8545", "specified port to run rpc")
+	// A Boolean variable derived from flags, that describes whether or not to start the web sockets server
 	ws = flag.Bool("ws", false, "whether or not to start the ws server")
 	// A string variable derived from flags, that specifies the websocket listening interface (default :localhost).
 	wsaddr = flag.String("wsaddr", "localhost", "specified ws listening addr")
 	// A string variable derived from flags, that specifies the websocket listening port (default :8546).
 	wsport = flag.String("wsport", "8546", "specifying ws listening port")
+
 )
 
 /*
 "ParseFlags" reads in specific command line arguments passed to the client and executes events based
 on the input.
-*/
+ */
 func ParseFlags() {
 	flag.Parse()
 }
 
-//TODO move tests
-func CLI_Test() {
+/*
+"PrintClientInfo" prints startup information about the client
+ */
+func PrintClientInfo(){
 	fmt.Println("CLIENT ID:", clientIdentifier)
-	if *print_version {
+	if *print_version{
 		fmt.Println("VERSION:", version)
 	}
 	fmt.Println("DATADIR:", *datadir)
-	fmt.Println("CLIENT RPC:", *client_rpc)
-	if *client_rpc {
-		fmt.Println("CLIENT RPCPORT:", *client_rpcport)
-		go rpc.StartClientRPC(*client_rpcport)
+	fmt.Println("RPC:", *rpc)
+	if *rpc{
+		fmt.Println("RPCPORT:",*rpcport)
 	}
-	fmt.Println("RELAY RPC:", *relay_rpc)
-	if *relay_rpc {
-		fmt.Println("RELAY RPCPORT:", *relay_rpcport)
-		rpc.StartRelayRPC(*relay_rpcport) // TODO should be changed to goroutine
-	}
-	fmt.Println("WS:", *ws)
-	if *ws {
-		fmt.Println("WSADDR:", *wsaddr)
+	fmt.Println("WS:", *ws);
+	if *ws{
+		fmt.Println("WSADDR:",*wsaddr)
 		fmt.Println("WSPORT:", *wsport)
 	}
 }
