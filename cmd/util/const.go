f91976ecc0efa236fefb58a37a90d6774d0c8470
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-22 15:01:02 -0400

    Added const.go

--- cmd/util/const.go
+++ cmd/util/const.go
@@ -1,11 +1,8 @@
-// This package is for command line specific utilities.
 package util
 
-// "const.go" contains constants that are useful for cmd.
-
 const (
-	// "clientIdentifier" holds the Pocket Network client Identifier.
+	// "clientIdentifier" holds the Pocket Network Client Identifier.
 	clientIdentifier = "pocket_core"
-	// "version" holds the Pocket Core client version.
+	// "version" holds the Pocket Core Client version.
 	version = "0.0.1"
 )
