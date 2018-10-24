aa722cf97548a20fb84e4606e1b4b26ea16461eb
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-24 13:31:57 -0400

    API structure created

--- cmd/pocket_core/main.go
+++ cmd/pocket_core/main.go
@@ -3,11 +3,16 @@ package main
 
 import (
 	"github.com/pocket_network/pocket-core/cmd/util"
+	"github.com/pocket_network/pocket-core/rpc"
 )
+
+//TODO add logging
 
 // "Main" is the starting function of the client.
 // Keep main as light as possible by calling accessory functions.
 func main() {
 	util.ParseFlags()
 	util.PrintClientInfo()
+	// TODO change to flag function
+	rpc.StartRPC("8080")
 }
