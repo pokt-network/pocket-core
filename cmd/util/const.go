a541584718734d39b1229cf676dcba6c0298e2ba
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-29 19:35:24 -0400

    Added comments / fixed concurrency / fixed docs

--- cmd/util/const.go
+++ cmd/util/const.go
@@ -1,9 +1,8 @@
 // This package is for command line specific utilities.
 package util
 
-/*
-"const.go" contains constants that are useful for cmd.
-  */
+// "const.go" contains constants that are useful for cmd.
+
 const (
 	// "clientIdentifier" holds the Pocket Network client Identifier.
 	clientIdentifier = "pocket_core"
