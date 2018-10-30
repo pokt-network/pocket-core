0377520ba72ab79672a3fe8a1eb62c04524fe549
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-30 08:35:27 -0400

    Added hello, world functions to all api paths

--- rpc/client/handlers/account.go
+++ rpc/client/handlers/account.go
@@ -3,6 +3,7 @@ package handlers
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/pocket_network/pocket-core/rpc/shared"
 	"net/http"
 )
 
@@ -12,61 +13,61 @@ import (
  "GetAccount" handles the localhost:<client-port>/v1/account call.
  */
 func GetAccount(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "IsAccountActive" handles the localhost:<client-port>/v1/account/active call.
  */
 func IsAccountActive(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetAccountBalance" handles the localhost:<client-port>/v1/account/balance call.
  */
 func GetAccountBalance(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetDateJoined" handles the localhost:<client-port>/v1/account/joined call.
  */
 func GetDateJoined(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetAccountKarma" handles the localhost:<client-port>/v1/account/karma call.
  */
 func GetAccountKarma(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetLastActive" handles the localhost:<client-port>/v1/account/last_active call.
  */
 func GetLastActive(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetAccTxCount" handles the localhost:<client-port>/v1/account/transaction_count call.
  */
 func GetAccTxCount(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetAccSessCount" handles the localhost:<client-port>/v1/account/session_count call.
  */
 func GetAccSessCount(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "GetAccStatus" handles the localhost:<client-port>/v1/account/status call.
  */
 func GetAccStatus(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
--- rpc/relay/handlers/relay.go
+++ rpc/relay/handlers/relay.go
@@ -3,6 +3,7 @@ package handlers
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/pocket_network/pocket-core/rpc/shared"
 	"net/http"
 )
 
@@ -12,19 +13,19 @@ import (
  "RelayOptions" handles the localhost:<relay-port>/v1/relay call.
  */
 func RelayOptions(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "RelayRead" handles the localhost:<relay-port>/v1/relay/read call.
  */
 func RelayRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
 
 /*
  "RelayWrite" handles the localhost:<relay-port>/v1/relay/write call.
  */
 func RelayWrite(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// TODO
+	shared.WriteResponse(w, "Hello, World!")
 }
