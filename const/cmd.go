3b3a785185326c5850ac9ba0ed7805dd81700420
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-10-30 08:52:47 -0400

    Moved cmd to global constant directory

---
+++ const/cmd.go
@@ -0,0 +1,12 @@
+// This package holds global constants.
+package _const
+
+
+// "cmd.go" contains constants that are useful for cmd.
+
+const (
+	// "clientIdentifier" holds the Pocket Network Client Identifier.
+	ClientIdentifier = "pocket_core"
+	// "version" holds the Pocket Core Client version.
+	Version = "0.0.1"
+)
