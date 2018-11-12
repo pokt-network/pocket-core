94e017541498ad4209e64f8d385bdfcd54f6f086
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-11-12 12:50:04 -0500

    Fixed all logging changes

--- logs/models.go
+++ logs/models.go
@@ -2,16 +2,37 @@
 package logs
 
 import (
+	"github.com/logmatic/logmatic-go"
 	"github.com/sirupsen/logrus"
 )
 
-type Log struct{ // TODO consider optimizations with enum like structure to give options to log creator
-	Filename 	string 					`json:"filename"`
-	Format 		logrus.Formatter 		`json:"format"`
-	Level		logrus.Level 			`json:"Level"`
-	// fields below
-	FilePath	string 					`json:"filepath"`	// where the log message came from
-	LineNumber 	string 					`json:"LineNumber"`	// specific line number from the message
-	Message		string					`json:"message"`
+var (
+	InfoLevel     = LogLevel{logrus.InfoLevel}
+	DebugLevel    = LogLevel{logrus.DebugLevel}
+	WaringLevel   = LogLevel{logrus.WarnLevel}
+	PanicLevel    = LogLevel{logrus.PanicLevel}
+	FatalLevel    = LogLevel{logrus.FatalLevel}
+	ErrorLevel    = LogLevel{logrus.ErrorLevel}
+	TraceLevel    = LogLevel{logrus.TraceLevel}
+	JSONLogFormat = LogFormat{&logmatic.JSONFormatter{}}
+)
+
+type Log struct {
+	Name         string    `json:"filename"`
+	Fmt          LogFormat `json:"format"`
+	Lev          LogLevel  `json:"Lev"`
+	FilePath     string    `json:"filepath"`   // where the log message came from
+	FunctionName string    `json:"functionname"`
+	LineNumber   string    `json:"LineNumber"` // specific line number from the message
+	Message      string    `json:"message"`
 }
 
+// TODO consider optimizations with enum like structure to give options to log creator
+
+type LogLevel struct {
+	level logrus.Level
+}
+
+type LogFormat struct {
+	format logrus.Formatter
+}
