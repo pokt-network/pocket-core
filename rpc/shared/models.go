5dab092e0c73d922a5006421dbc0e4c70cb8764f
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-11-21 09:55:52 -0500

    Updated test

--- rpc/shared/models.go
+++ rpc/shared/models.go
@@ -6,6 +6,28 @@ package shared
 
 
 /*
+"JSONResponse" is a metadata and data response in JSON format.
+ */
+type JSONResponse struct {
+	Data string `json:"data"`
+}
+
+/*
+"JSONErrorResponse" is an error response in JSON format.
+ */
+type JSONErrorResponse struct {
+	Error *APIError `json:"error"`
+}
+
+/*
+"APIError" is an error feedback structure containing a title and a status.
+ */
+type APIError struct {
+	Status int    `json:"error"`
+	Title  string `json:"title"`
+}
+
+/*
 "Example" is a basic JSON structure serving as a placeholder.
  */
 type Example struct {
--- rpc/shared/responses.go
+++ rpc/shared/responses.go
@@ -10,28 +10,6 @@ import (
 // "responses.go is the interface for the types of responses for the API.
 
 /*
-"JSONResponse" is a metadata and data response in JSON format.
- */
-type JSONResponse struct {
-	Data string `json:"data"`
-}
-
-/*
-"JSONErrorResponse" is an error response in JSON format.
- */
-type JSONErrorResponse struct {
-	Error *APIError `json:"error"`
-}
-
-/*
-"APIError" is an error feedback structure containing a title and a status.
- */
-type APIError struct {
-	Status int    `json:"error"`
-	Title  string `json:"title"`
-}
-
-/*
 "WriteResponse" writes a normal JSON response.
  */
 func WriteResponse(w http.ResponseWriter, m string) {
