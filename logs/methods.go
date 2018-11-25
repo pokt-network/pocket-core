7c703d9a9e1ba2c48cbb38a20f37a79ac8b41f3a
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2018-11-25 00:42:24 -0200

    fix method call

--- logs/methods.go
+++ logs/methods.go
@@ -1,23 +1,23 @@
 package logs
 
 import (
-	"github.com/pokt-network/pocket-core/util"
 	"strconv"
 	"time"
+
+	"github.com/pokt-network/pocket-core/util"
 )
 
-func LogConstructorAndLog(message string,level LogLevel, format LogFormat) {
+func LogConstructorAndLog(message string, level LogLevel, format LogFormat) {
 	currentTime := time.Now()
-	f,t :=util.MyCaller()
-	filepath,ln:=f.FileLine(t)
-	log:=&Log{}
-	log.Name=currentTime.Format("2006.01.02 15:04:05")
-	log.FunctionName=f.Name()
-	log.FilePath= filepath
+	f, t := util.Caller()
+	filepath, ln := f.FileLine(t)
+	log := &Log{}
+	log.Name = currentTime.Format("2006.01.02 15:04:05")
+	log.FunctionName = f.Name()
+	log.FilePath = filepath
 	log.Lev = level
-	log.Fmt =format
-	log.LineNumber= strconv.Itoa(ln)
-	log.Message=message
+	log.Fmt = format
+	log.LineNumber = strconv.Itoa(ln)
+	log.Message = message
 	Logger(*log)
 }
-
