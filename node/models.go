5d92b05279e05830cc27e04fe24baf1e92c1a278
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-11-27 14:43:21 -0500

    Added node attributes

--- node/models.go
+++ node/models.go
@@ -4,7 +4,13 @@ package node
 // models.go maintains the structures for the node package.
 
 type Node struct{
-	// TODO add general node data here
+	RemoteIP 		string 				// holds the remote IP address
+	LocalIP 		string 				// holds the local IP address
+	RelayPort		string				// specifies the port for relay API
+	ClientPort 		string				// specifies the port for the client API
+	ClientID		string				// holds the identifier string for the client "pocket_core"
+	CliVersion		string				// holds the version of the client
+	HostedChains	[]string			// holds the hosted blockchains
 }
 
 type Validator struct{
@@ -14,5 +20,6 @@ type Validator struct{
 
 type Service struct{
 	// TODO add Service specific data here
+	// NOTE: May not be applicable considering all nodes on the pocket network are service nodes
 	Node
 }
