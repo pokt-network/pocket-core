8f9a7557048f682786b615163e38cb5d91907d8f
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-12-18 18:56:34 -0500

    finished messenger

--- message/handlers.go
+++ message/handlers.go
@@ -4,8 +4,13 @@ package message
 import "fmt"
 
 // TODO call upon message handlers based on payload index
+// TODO think about concurrency
 
+/*
+"HandleMessage" is the parent function that calls upon specific message handler functions
+				based on the index
+ */
 func HandleMessage(message *Message){
-	fmt.Println(message.Payload.Data)
+	fmt.Println(message.Payload.Data)	// currently just prints out the message but should use a switch soon
 }
 
--- tests/net/session/Conn_test.go
+++ tests/net/session/Conn_test.go
@@ -4,26 +4,32 @@ import (
 	"github.com/pokt-network/pocket-core/message"
 	"github.com/pokt-network/pocket-core/net/session"
 	"testing"
+	"time"
 )
 
 func TestConnection(t *testing.T) {
+	// establish a new listener
 	const port = "3333"
 	const host = "localhost"
 	go session.Listen(port,host)
+	// create a new peer
 	peer1 := session.NewPeer()
 	peer1.CreateConnection(port,host)
 	// get peer2 from the peerlist
-	for session.GetSessionPeerlist()[peer1.Conn.LocalAddr().String()]==(session.Peer{}){
-
-	}
+	for session.GetSessionPeerlist()[peer1.Conn.LocalAddr().String()]==(session.Peer{}){} // wait for peer registration
+	// get peer2 from global peer list
 	peer2 := session.GetSessionPeerlist()[peer1.Conn.LocalAddr().String()]
-	// Have both listen to demonstrate sending and receiving ability
-	go peer1.Receive() // test if you can receive and send
-	go peer2.Receive() // test if you can receive and send
-	// Create a new message to send
+	// create a new message to send
 	peer1Payload := message.Payload{0,"I am  peer 1: "+peer1.Conn.LocalAddr().String()}
 	peer2Payload := message.Payload{0,"I am  peer 2: "+peer2.Conn.LocalAddr().String()}
+	// send several messages back and forth
 	peer1.Send(message.NewMessage(peer1Payload))
 	peer2.Send(message.NewMessage(peer2Payload))
+	peer1.Send(message.NewMessage(peer1Payload))
+	peer2.Send(message.NewMessage(peer2Payload))
+	// don't close the connection without waiting for a second to allow for threads to complete their process
+	time.Sleep(1 * time.Second)
+	// close the connections
 	peer1.CloseConnection()
+	peer2.CloseConnection()
 }
