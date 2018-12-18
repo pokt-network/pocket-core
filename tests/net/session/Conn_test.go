04dfd5bf55645d2104b230d1b0c116771727828c
Andrew Nguyen <amnguyen1@mail.usf.edu>
2018-12-18 17:01:49 -0500

    wip threading issues

--- tests/net/session/Conn_test.go
+++ tests/net/session/Conn_test.go
@@ -1,7 +1,6 @@
 package session
 
 import (
-	"fmt"
 	"github.com/pokt-network/pocket-core/message"
 	"github.com/pokt-network/pocket-core/net/session"
 	"testing"
@@ -10,20 +9,21 @@ import (
 func TestConnection(t *testing.T) {
 	const port = "3333"
 	const host = "localhost"
-	go session.Listen(port,host)                                   // start the server
+	go session.Listen(port,host)
 	peer1 := session.NewPeer()
 	peer1.CreateConnection(port,host)
-	t.Log(peer1.Conn.RemoteAddr())
-	fmt.Println(peer1.Conn.LocalAddr().String())
 	// get peer2 from the peerlist
 	for session.GetSessionPeerlist()[peer1.Conn.LocalAddr().String()]==(session.Peer{}){
 
 	}
 	peer2 := session.GetSessionPeerlist()[peer1.Conn.LocalAddr().String()]
+	// Have both listen to demonstrate sending and receiving ability
+	go peer1.Receive() // test if you can receive and send
+	go peer2.Receive() // test if you can receive and send
 	// Create a new message to send
-	pl := message.Payload{0,"I am peer 2 : "+peer2.Conn.LocalAddr().String()}
-	m:= message.NewMessage(pl)
-	peer2.Send(m)
-	peer1.Receive()
+	peer1Payload := message.Payload{0,"I am  peer 1: "+peer1.Conn.LocalAddr().String()}
+	peer2Payload := message.Payload{0,"I am  peer 2: "+peer2.Conn.LocalAddr().String()}
+	peer1.Send(message.NewMessage(peer1Payload))
+	peer2.Send(message.NewMessage(peer2Payload))
 	peer1.CloseConnection()
 }
