55fe1d53649be43eba5e8db3c2400273b51f1ac9
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-01-03 10:21:51 -0500

    comment cleanup

--- rpc/relay/dispatch.go
+++ rpc/relay/dispatch.go
@@ -52,29 +52,29 @@ func DispatchServe(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 // TODO NEED a separate dispatch file with calls like these
 */
 func DispatchFind(sessionKey string) []node.Node {
-	bigSessionKey := new(big.Int)           // create new big integer to store sessionKey in
-	bigSessionKey.SetString(sessionKey, 16) // convert hex string into big integer
-	peerList := peers.GetPeerList()         // get the global peerlist
-	peerList.Lock()                         // TODO currently locking the peerlist, however this will all change when p2p is integerated
+	bigSessionKey := new(big.Int)           					// create new big integer to store sessionKey in
+	bigSessionKey.SetString(sessionKey, 16) 				// convert hex string into big integer
+	peerList := peers.GetPeerList()         					// get the global peerlist
+	peerList.Lock()                         					// TODO currently locking the peerlist, however this will all change when p2p is integerated
 	defer peerList.Unlock()
-	m := make(map[uint64]node.Node)                      // map the nodes to the corresponding difference
-	keys := make([]uint64, len(peerList.List))           // store the keys (to easily sort)
-	sortedNodes := make([]node.Node, len(peerList.List)) // resulting array that holds the sorted nodes ordered by difference
-	var i = 0                                            // loop count
-	for gid, curNode := range peerList.List {            // for each curNode in the peerlist
-		id := new(big.Int)                                 // setup a new big integer to hold the converted ID
-		id.SetString(gid, 16)                              // convert the hex GID into a bigInteger for comparison
-		difference := big.NewInt(0).Sub(bigSessionKey, id) // find the difference between the two
-		difference.Abs(difference)                         // take absolute of the difference for comparison
-		m[difference.Uint64()] = curNode                   // map the corresponding difference -> curNode
-		keys[i] = difference.Uint64()                      // store the difference in the keys array
-		i++                                                // increment the count
+	m := make(map[uint64]node.Node)                      		// map the nodes to the corresponding difference
+	keys := make([]uint64, len(peerList.List))           		// store the keys (to easily sort)
+	sortedNodes := make([]node.Node, len(peerList.List)) 		// resulting array that holds the sorted nodes ordered by difference
+	var i = 0                                            		// loop count
+	for gid, curNode := range peerList.List {            		// for each curNode in the peerlist
+		id := new(big.Int)                                 		// setup a new big integer to hold the converted ID
+		id.SetString(gid, 16)                             // convert the hex GID into a bigInteger for comparison
+		difference := big.NewInt(0).Sub(bigSessionKey, id) 	// find the difference between the two
+		difference.Abs(difference)                         		// take absolute of the difference for comparison
+		m[difference.Uint64()] = curNode                   		// map the corresponding difference -> curNode
+		keys[i] = difference.Uint64()                      		// store the difference in the keys array
+		i++                                                		// increment the count
 	}
 	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] }) // sort the keys
-	for i, k := range keys {                                           // after filling out the difference for all in peerList
-		sortedNodes[i] = m[k] // store the nodes in order by difference
+	for i, k := range keys {                                    // after filling out the difference for all in peerList
+		sortedNodes[i] = m[k] 									// store the nodes in order by difference
 	}
-	return sortedNodes // return the sorted order
+	return sortedNodes 											// return the sorted order
 }
 
 /*
