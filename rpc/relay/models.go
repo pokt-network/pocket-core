5c47e4eb83832f54811cfbf7d3f3dd141b22db57
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-01-09 15:59:41 -0500

    dispatchServe WIP

--- rpc/relay/models.go
+++ rpc/relay/models.go
@@ -1,12 +1,15 @@
 // This package contains files for the Relay API
 package relay
 
+import "github.com/pokt-network/pocket-core/node"
+
 /*
 "models.go" defines API models in this file.
 */
 
 type Dispatch struct {
-	DevID string `json:"devid"`
+	DevID 		string 				`json:"devid"`
+	Blockchains []node.Blockchain 	`json:"blockchains"`
 }
 
 /*
