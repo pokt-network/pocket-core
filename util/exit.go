052651b410340e3ed40bdbfd51e1877199a37116
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-06 15:07:57 -0500

    improved error message for EnsureWL in node module, updated http.go to throw err for bad header, fixed WL bug in node module, added flags endpoint for future automation of the README config section

--- util/exit.go
+++ util/exit.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ExitGracefully(message string) {
-	logs.NewLog("Shutting down Pocket Core because "+message, logs.InfoLevel, logs.JSONLogFormat)
-	fmt.Println("Shutting down Pocket Core because " + message)
+	logs.NewLog("Shutting down Pocket Core: "+message, logs.InfoLevel, logs.JSONLogFormat)
+	fmt.Println("Shutting down Pocket Core: " + message)
 	os.Exit(0)
 }
