846ab94d4e87b7310996bf9d88ad25c0aa031968
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-02-08 08:46:22 -0500

    added info logs for IP address in util module

--- util/ip.go
+++ util/ip.go
@@ -3,11 +3,14 @@ package util
 import (
 	"io/ioutil"
 	"net/http"
+
+	"github.com/pokt-network/pocket-core/logs"
 )
 
 func IP() (string, error) {
 	url := "https://api.ipify.org?format=text"
 	resp, err := http.Get(url)
+	logs.NewLog("Getting IP address from ipify.org...", logs.InfoLevel, logs.JSONLogFormat)
 	if err != nil {
 		return ",", err
 	}
@@ -16,5 +19,6 @@ func IP() (string, error) {
 	if err != nil {
 		return ",", err
 	}
+	logs.NewLog("IP returned: "+string(ip)+" from ipify!", logs.InfoLevel, logs.JSONLogFormat)
 	return string(ip), nil
 }
