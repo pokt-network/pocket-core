f8ab836dfd3fd281026ff95d3821b40b5f9f890c
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-02-12 11:18:54 -0500

    removed white space from cmd/db/dyanamodb.go and printed to stderr in util/exit.go for exit gracefully function

--- util/exit.go
+++ util/exit.go
@@ -9,6 +9,6 @@ import (
 
 func ExitGracefully(message string) {
 	logs.NewLog("Shutting down Pocket Core: "+message, logs.InfoLevel, logs.JSONLogFormat)
-	fmt.Println("Shutting down Pocket Core: " + message)
+	fmt.Fprint(os.Stderr, "Shutting down Pocket Core: "+message)
 	os.Exit(0)
 }
