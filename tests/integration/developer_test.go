5f43a653c6382b19e9b991681bf541ba5117e55c
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-02-15 09:57:02 -0500

    finished developer integration test: Relay

--- tests/integration/developer_test.go
+++ tests/integration/developer_test.go
@@ -2,27 +2,35 @@ package integration
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
-	
+
 	"github.com/pokt-network/pocket-core/config"
-	"github.com/pokt-network/pocket-core/node"
+	"github.com/pokt-network/pocket-core/const"
 	"github.com/pokt-network/pocket-core/util"
 )
 
 /*
 Integration Testing Assumptions:
-1) Dispatcher is hosting a testrpc instance that is labeled as (Blockchain: 0 | NetworkID: 0 | Version: 0)
+1) Dispatcher is hosting a testrpc instance that is labeled as (Blockchain: 0 | NetworkID: 0 | Version: 0) in chains.json file
 2) Dispatcher has white listed DEVID1 (Dev) and GID1 (SN)
+3) Dispatcher is running on DispIP:DisRPort
  */
 
-func TestDispatch(t *testing.T) {
-	blockchain := node.Blockchain{Name: "ethereum", NetID: "0", Version: "0"}
-	c := node.Chains()
-	c[blockchain]=node.HostedChain{Blockchain: blockchain, Port: "8080", Medium: "rpc"}
-	
-	const ethereumRequest = "{\"jsonrpc\":\"2.0\",\"method\":\"web3_clientVersion\",\"params\":[],\"id\":67}"
-	const relayRequest = "{\"blockchain\":\"ethereum\", \"networkid\":\"0\", \"version\":\"0\", \"data\":\"" + ethereumRequest + "\", \"devid\":\"DEVID1\"}"
-	fmt.Println(relayRequest)
-	resp, _:= util.RPCRequest("http://"+config.GlobalConfig().DisIP+":"+config.GlobalConfig().DisRPort+"/v1/relay", relayRequest, util.POST)
-	fmt.Println(resp)
+func TestRelay(t *testing.T) {
+	filepath, err := filepath.Abs("fixtures" + _const.FILESEPARATOR + "relay.json")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	b, err := ioutil.ReadFile(filepath)
+	fmt.Println(string(b))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	resp, err := util.RPCRequ("http://"+config.GlobalConfig().DisIP+":"+config.GlobalConfig().DisRPort+"/v1/relay", b, util.POST)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Log(resp)
 }
