bdea51aafd5b6bcb9232f0e914c28e79378ea886
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-21 11:14:02 -0500

    updated the configuration file to allow automatic conversion of datadirectory config files

--- const/filename.go
+++ const/filename.go
@@ -1,9 +1,9 @@
 package _const
 
 var (
-	REPORTFILENAME = DATADIR + FILESEPARATOR + "reports.json"
-	SNWLFILENAME   = DATADIR + FILESEPARATOR + "service_whitelist.json"
-	DWLFILENAME    = DATADIR + FILESEPARATOR + "developer_whitelist.json"
-	PEERFILENAME   = DATADIR + FILESEPARATOR + "peers.json"
-	CHAINSFILENAME = DATADIR + FILESEPARATOR + "chains.json"
+	REPORTFILENAME = "reports.json"
+	SNWLFILENAME   = "service_whitelist.json"
+	DWLFILENAME    = "developer_whitelist.json"
+	PEERFILENAME   = "peers.json"
+	CHAINSFILENAME = "chains.json"
 )
