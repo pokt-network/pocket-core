8a2ab70b54f0c4361b78fd43b8289e9d02bd2f44
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-02-22 09:03:28 -0500

    added dbreigion flag, encorporated host attribute into hostedChains and edited the testChains feature

--- const/db.go
+++ const/db.go
@@ -2,7 +2,7 @@ package _const
 
 const (
 	DBTABLENAME = "dispatchPeers"
-	DBREIGON    = "us-east-2"
+	DBREGION    = "us-east-2"
 	DBENDPOINT  = "dynamodb.us-east-2.amazonaws.com"
 	DBREFRESH   = 5
 )
