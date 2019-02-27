593de64c77e92cf6f048b315da0e684f4af33f7a
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-27 08:49:01 -0500

    removed unnecessary rpc routes and the report functionality

--- rpc/relay/routes.go
+++ rpc/relay/routes.go
@@ -12,8 +12,6 @@ func Routes() shared.Routes {
 	routes := shared.Routes{
 		shared.Route{Name: "Version", Method: "GET", Path: "/v1", HandlerFunc: Version},
 		shared.Route{Name: "WriteRoutes", Method: "GET", Path: "/v1/routes", HandlerFunc: WriteRoutes},
-		shared.Route{Name: "Report", Method: "POST", Path: "/v1/report", HandlerFunc: Report},
-		shared.Route{Name: "ReportInfo", Method: "GET", Path: "/v1/report", HandlerFunc: ReportInfo},
 		shared.Route{Name: "Dispatch", Method: "POST", Path: "/v1/dispatch", HandlerFunc: Dispatch},
 		shared.Route{Name: "DispatchInfo", Method: "GET", Path: "/v1/dispatch", HandlerFunc: DispatchInfo},
 		shared.Route{Name: "Relay", Method: "POST", Path: "/v1/relay/", HandlerFunc: Relay},
