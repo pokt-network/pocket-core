9c29f588ca1c4c6d0f2b90d5af3aadc612a8cfce
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-27 08:37:35 -0500

    removed the mvp code from the constants directory including dispatch and db

--- const/filename.go
+++ const/filename.go
@@ -2,8 +2,6 @@ package _const
 
 var (
 	REPORTFILENAME = "reports.json"
-	SNWLFILENAME   = "service_whitelist.json"
-	DWLFILENAME    = "developer_whitelist.json"
 	PEERFILENAME   = "peers.json"
 	CHAINSFILENAME = "chains.json"
 )
