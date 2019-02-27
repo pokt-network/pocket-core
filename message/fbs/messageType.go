66a107ee5c4af755cb3a17046a622472e33bf713
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-27 11:33:21 -0500

    ran gofmt

--- message/fbs/messageType.go
+++ message/fbs/messageType.go
@@ -3,13 +3,13 @@
 package fbs
 
 type MessageType = int32
+
 const (
 	MessageTypeUNDEFINED MessageType = 0
 	MessageTypeDISCHELLO MessageType = 1
 )
 
 var EnumNamesMessageType = map[MessageType]string{
-	MessageTypeUNDEFINED:"UNDEFINED",
-	MessageTypeDISCHELLO:"DISCHELLO",
+	MessageTypeUNDEFINED: "UNDEFINED",
+	MessageTypeDISCHELLO: "DISCHELLO",
 }
-
