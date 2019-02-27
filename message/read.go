2befbfd780a62021176174e639d8d4ba687e981f
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-27 10:39:24 -0500

    added undefined message type

--- message/read.go
+++ message/read.go
@@ -6,18 +6,20 @@ import (
 	"github.com/pokt-network/pocket-core/message/fbs"
 )
 
-func UnmarshalMessage(flatBuffer []byte) Message {
-	message := fbs.GetRootAsMessage(flatBuffer, 0)
-	return Message{message.Type(), message.PayloadBytes(), message.Timestamp()}
-}
-
 func RouteMessageByPayload(m Message) (interface{}, error) {
 	switch m.Type_ {
+	case fbs.MessageTypeUNDEFINED:
+		return m.Payload, errors.New("undefined message type")
 	case fbs.MessageTypeDISC_HELLO:
 		return UnmarshalHelloMessage(m.Payload)
 	default:
-		return nil, errors.New("unsupported message type" + string(m.Type_))
+		return m.Payload, errors.New("unsupported message type" + string(m.Type_))
 	}
+}
+
+func UnmarshalMessage(flatBuffer []byte) Message {
+	message := fbs.GetRootAsMessage(flatBuffer, 0)
+	return Message{message.Type(), message.PayloadBytes(), message.Timestamp()}
 }
 
 func UnmarshalHelloMessage(flatBuffer []byte) (*HelloMessage, error) {
