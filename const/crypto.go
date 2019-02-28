a69d34e87bda23087fee98f6139334ba09e69f61
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-28 09:18:43 -0500

    updated hash functions to be SHA1 for now

--- const/crypto.go
+++ const/crypto.go
@@ -7,6 +7,6 @@ import (
 // TODO will change SHA to algorithm decided on by core team
 const (
 	// defines the session hashing algorithm
-	SESSIONHASHINGALGORITHM  = crypto.SHA3_256
-	VALIDATEHASHINGALGORITHM = crypto.SHA3_256
+	SESSIONHASHINGALGORITHM  = crypto.SHA1
+	VALIDATEHASHINGALGORITHM = crypto.SHA1
 )
