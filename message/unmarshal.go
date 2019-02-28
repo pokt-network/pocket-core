3a9f16c0c7305e4ace5269dd2619227fe6e0cb46
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-28 09:21:06 -0500

    fixed message/unmarhshal.go (validateMessage function)

--- message/unmarshal.go
+++ message/unmarshal.go
@@ -2,7 +2,7 @@ package message
 
 import (
 	"errors"
-
+	
 	"github.com/pokt-network/pocket-core/message/fbs"
 	"github.com/pokt-network/pocket-core/service"
 )
@@ -21,18 +21,21 @@ func UnmarshalHelloMessage(flatBuffer []byte) (*HelloMessage, error) {
 	return &HelloMessage{string(helloMessage.GidBytes())}, nil
 }
 
-func UnmarshalValidateMessage(flatBuffer []byte) ValidateMessage {
-	validateMessage := fbs.GetRootAsValidateMessage(flatBuffer, 0)
-	fbsRelay := &fbs.Relay{}
-	validateMessage.Relay(fbsRelay)
-	return ValidateMessage{ConvertFBSRelay(fbsRelay), validateMessage.HashBytes()}
+func UnmarshalRelay(flatbuffer []byte) (*service.Relay, error) {
+	relay := fbs.GetRootAsRelay(flatbuffer, 0)
+	return &service.Relay{Blockchain: string(relay.Blockchain()), NetworkID: string(relay.Netid()), Version: string(relay.Version()), Data: string(relay.Data()), DevID: string(relay.DevID())}, nil
 }
 
-func ConvertFBSRelay(fbsRelay *fbs.Relay) service.Relay {
-	return service.Relay{
-		Blockchain: string(fbsRelay.Blockchain()),
-		NetworkID:  string(fbsRelay.NetworkID()),
-		Version:    string(fbsRelay.Version()),
-		Data:       string(fbsRelay.Data()),
-		DevID:      string(fbsRelay.DevID())}
+func UnmarshalValidateMessage(flatbuffer []byte) (*ValidateMessage, error) {
+	vm := fbs.GetRootAsValidateMessage(flatbuffer, 0)
+	r := &fbs.Relay{}
+	vm.Relay(r)
+	return &ValidateMessage{
+		Relay: service.Relay{
+			Blockchain: string(r.Blockchain()),
+			NetworkID:  string(r.Netid()),
+			Version:    string(r.Version()),
+			Data:       string(r.Data()),
+			DevID:      string(r.DevID())},
+		Hash: vm.HashBytes()}, nil
 }
