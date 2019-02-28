3b209549c483ad6c4b09b1574bb216be6c42396c
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-02-28 09:42:38 -0500

    added HandleValidateMessage()

--- message/handlers.go
+++ message/handlers.go
@@ -2,8 +2,9 @@ package message
 
 import (
 	"errors"
-
+	
 	"github.com/pokt-network/pocket-core/message/fbs"
+	"github.com/pokt-network/pocket-core/service"
 )
 
 func RouteMessageByPayload(m Message) error {
@@ -12,6 +13,8 @@ func RouteMessageByPayload(m Message) error {
 		return nil
 	case fbs.MessageTypeDISCHELLO:
 		return HandleDISCHELLOMessage(m)
+	case fbs.MessageTypeVALIDATE:
+		return HandleValidateMessage(m)
 	default:
 		return errors.New("unsupported message type" + string(m.Type_))
 	}
@@ -27,5 +30,18 @@ func HandleDISCHELLOMessage(m Message) error { // TODO
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func HandleValidateMessage(m Message) error { // TODO
+	vm, err := UnmarshalValidateMessage(m.Payload)
+	if err != nil {
+		return err
+	}
+	b, err := service.Validate(vm.Relay, vm.Hash)
+	if err != nil {
+		return err
+	}
+	b = b // arbitrary
 	return nil
 }
