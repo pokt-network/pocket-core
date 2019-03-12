2946f2f5f2db256a8d8206e653e3e3a0da7fb45b
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-03-12 15:42:31 -0400

    ran gofmt

--- const/dispatcher.go
+++ const/dispatcher.go
@@ -2,10 +2,10 @@ package _const
 
 const (
 	// for centralized dispatcher
-	DISPATCHGID        = "DISPATCH"
-	DISPATCHIP         = "localhost"
-	DISPATCHRELAYPORT  = "8081"
-	DISMODENORMAL      = 0
-	DISMODEMIGRATE     = 1
-	DISMODEDEPRECATED  = 2
+	DISPATCHGID       = "DISPATCH"
+	DISPATCHIP        = "localhost"
+	DISPATCHRELAYPORT = "8081"
+	DISMODENORMAL     = 0
+	DISMODEMIGRATE    = 1
+	DISMODEDEPRECATED = 2
 )
