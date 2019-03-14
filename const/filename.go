1f86c16e7eb8a05a809d78e5fc47524a163ec79e
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-03-14 10:40:30 -0400

    invalid path bug fixed for reports.json

--- const/filename.go
+++ const/filename.go
@@ -1,7 +1,7 @@
 package _const
 
 const (
-	REPORTFILENAMEPLACEHOLDER = "<your_data_directory>/reports.json"
+	REPORTFILENAMEPLACEHOLDER = "reports.json"
 	SNWLFILENAMEPLACEHOLDER   = "<your_data_directory>/service_whitelist.json"
 	DWLFILENAMEPLACEHOLDER    = "<your_data_directory>/developer_whitelist.json"
 	CHAINFILEPLACEHOLDER      = "<your_data_directory>/chains.json"
