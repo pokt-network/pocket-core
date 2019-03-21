a58bcaaf2c57560a01c04b1ad349e86cdd9be726
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-03-21 09:07:32 -0400

    made all changes for dev experience team

--- rpc/relay/routes.go
+++ rpc/relay/routes.go
@@ -17,7 +17,7 @@ func Routes() shared.Routes {
 		shared.Route{Name: "Dispatch", Method: "POST", Path: "/v1/dispatch", HandlerFunc: Dispatch},
 		shared.Route{Name: "DispatchInfo", Method: "GET", Path: "/v1/dispatch", HandlerFunc: DispatchInfo},
 		shared.Route{Name: "Relay", Method: "POST", Path: "/v1/relay/", HandlerFunc: Relay},
-		shared.Route{Name: "RelayInfo", Method: "GET", Path: "/v1/relay/", HandlerFunc: RelayInfo},
+		shared.Route{Name: "RelayInfo", Method: "GET", Path: "/v1/relay", HandlerFunc: RelayInfo},
 		shared.Route{Name: "Register", Method: "POST", Path: "/v1/register", HandlerFunc: Register},
 		shared.Route{Name: "UnRegister", Method: "POST", Path: "/v1/unregister", HandlerFunc: UnRegister},
 		shared.Route{Name: "RegisterInfo", Method: "GET", Path: "/v1/register", HandlerFunc: RegisterInfo},
