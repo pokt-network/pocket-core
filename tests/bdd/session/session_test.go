f9c685d38e163d662ccab819b8b2ed56fdc05403
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-03-21 13:18:58 -0400

    Milestone proposal + formatting

--- tests/bdd/session/session_test.go
+++ tests/bdd/session/session_test.go
@@ -1,7 +1,13 @@
 package session_test
 
 import (
+	"github.com/google/flatbuffers/go"
+	"path/filepath"
+	
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pokt-network/pocket-core/core"
+	"github.com/pokt-network/pocket-core/types"
 )
 
 // ************************************************************************************************************
@@ -11,176 +17,245 @@ import (
 //
 // Unanswered Questions?
 // - Where is the seed data coming from? 99% sure -> *WORLD STATE*
-// - How is the state of the session nodes maintained?
 // - Is service request validation ddos safe?
 // - Forking behavior
+// TODO deciding on GID format
 // ************************************************************************************************************
 
 var _ = Describe("Session", func() {
-
+	
 	Describe("Session Creation \\ Computing", func() {
-
-		Context("Invalid Seed Data", func() {
-			// n-1 Block Hash (probably world state), blockchains requested [list] (probably  world state), node [list] (probably world state))
-
+		
+		devid := []byte(core.SHA3FromString("foo"))
+		blockhash := core.SHA3FromString("foo")
+		requestedChain := core.Blockchain{Name: "eth", NetID: "1", Version: "1"}
+		marshalBC, err := core.MarshalBlockchain(flatbuffers.NewBuilder(0), requestedChain)
+		capacity := 100
+		if err != nil {
+			Fail(err.Error())
+		}
+		requestedChainHash := core.SHA3FromBytes(marshalBC)
+		absPath, _ := filepath.Abs("../fixtures/xsmallnodepool.json")
+		nodelist, err := core.FileToNodes(absPath)
+		if err != nil {
+			Fail(err.Error())
+		}
+		
+		Context("Invalid SessionSeed Data", func() {
+			
 			Context("Parameters are missing or null", func() {
-
-				It("should return `missing parameters` error", func() {
-					// Code goes here ...
+				
+				Context("Missing Devid", func() {
+					NoDevIDSeed := core.SessionSeed{BlockHash: blockhash, RequestedChain: requestedChainHash, NodeList: nodelist, Capacity: capacity}
+					It("should return missing devid error", func() {
+						_, err := core.NewSession(NoDevIDSeed)
+						Expect(err).To(Equal(core.NoDevIDError))
+					})
+				})
+				
+				Context("Missing Blockhash", func() {
+					NoBlockhashSeed := core.SessionSeed{DevID: devid, RequestedChain: requestedChainHash, NodeList: nodelist, Capacity: capacity}
+					It("should return missing blockhash error", func() {
+						_, err := core.NewSession(NoBlockhashSeed)
+						Expect(err).To(Equal(core.NoBlockHashError))
+					})
+				})
+				
+				Context("Missing Requested Chain", func() {
+					NoRequestedChain := core.SessionSeed{DevID: devid, BlockHash: blockhash, NodeList: nodelist, Capacity: capacity}
+					It("should return missing requested chain error", func() {
+						_, err := core.NewSession(NoRequestedChain)
+						Expect(err).To(Equal(core.NoReqChainError))
+					})
+				})
+				
+				Context("Missing Nodelist", func() {
+					NoNodeListSeed := core.SessionSeed{DevID: devid, BlockHash: blockhash, RequestedChain: requestedChainHash, Capacity: capacity}
+					It("should return missing nodelist error", func(){
+						_, err := core.NewSession(NoNodeListSeed)
+						Expect(err).To(Equal(core.NoNodeListError))
+					})
+				})
+				
+				Context("Missing Capacity", func() {
+					NoCapacitySeed := core.SessionSeed{DevID: devid, BlockHash: blockhash, RequestedChain: requestedChainHash, NodeList: nodelist}
+					It("should return missing capacity error", func(){
+						_, err := core.NewSession(NoCapacitySeed)
+						Expect(err).To(Equal(core.NoCapacityError))
+					})
 				})
 			})
-
-			Context("Devid1 is incorrect...", func() {
-
+			
+			Context("Devid is incorrect...", func() {
+				
 				Context("Devid is incorrect format", func() {
-
+					invalidDevIDSeed := core.SessionSeed{DevID: []byte("invalidtest"), BlockHash: blockhash, RequestedChain: requestedChainHash, NodeList: nodelist, Capacity: capacity}
 					It("should return `invalid developer id` error", func() {
-						// Code goes here ...
+						_, err := core.NewSession(invalidDevIDSeed)
+						Expect(err).To(Equal(core.InvalidDevIDFormatError))
+					})
+				})
+				
+				Context("Devid is not found in world state", func() {
+					
+					PIt("should error", func() {
+						// TODO need a world state
 					})
 				})
 			})
-
+			
 			Context("Block Hash is incorrect...", func() {
-				// n-1 block
+				
 				Context("Not a valid block hash format", func() {
-
+					invalidBlockHashFormatSeed := core.SessionSeed{DevID: devid, BlockHash: []byte("foo"), RequestedChain: requestedChainHash, NodeList: nodelist, Capacity:capacity}
 					It("should return `invalid block hash` error", func() {
-						// Code goes here ...
+						_, err := core.NewSession(invalidBlockHashFormatSeed)
+						Expect(err).To(Equal(core.InvalidBlockHashFormatError))
 					})
 				})
-
-				Context("Block hash is expired", func() {
-
-					It("should error", func() {
-						// Code goes here ...
-					})
-				})
-			})
-
-			Context("Blockchains list invalid...", func() {
-
-				Context("No nodes are associated with a blockchain in the list", func() {
-
-					It("should return `invalid blockchain list` error", func() {
-						// Code goes here ...
+				
+				PContext("Block hash is expired", func() {
+					
+					PIt("should error", func() {
+						// TODO need a world state
 					})
 				})
 			})
-
-			Context("Node list is invalid...", func() {
-
-				Context("Node structure is in the incorrect format", func() {
-
-					It("should return `invalid node list` error", func() {
-						// Code goes here ...
+			
+			Context("Requested Blockchain is invalid...", func() {
+				
+				Context("No nodes are associated with a blockchain", func() {
+					noNodesSeed := core.SessionSeed{DevID: devid, BlockHash: blockhash, RequestedChain: core.SHA3FromString("foo"), NodeList: nodelist, Capacity: capacity}
+					It("should return `invalid blockchain` error", func() {
+						_, err := core.NewSession(noNodesSeed)
+						Expect(err).To(Equal(core.InsufficientNodesError))
 					})
 				})
-				// ... any other reason may be incorrect?
 			})
 		})
-
-		Context("Valid Seed Data", func() {
-
+		
+		Context("Valid SessionSeed Data", func() {
+			absPath, _ := filepath.Abs("../fixtures/mediumnodepool.json")
+			validSeed, _ := core.NewSessionSeed(devid, absPath, requestedChainHash, blockhash, capacity)
+			s, err := core.NewSession(validSeed)
+			It("should not have returned any error", func() {
+				Expect(err).To(BeNil())
+			})
 			Describe("Generating a valid session", func() {
-
 				It("should generate a session key", func() {
-					// Code goes here ...
+					Expect(s.Key).ToNot(BeNil())
+					Expect(len(s.Key)).ToNot(BeZero())
 				})
-
+				
 				Describe("Node selection", func() {
-
-					It("should find the 5 closest nodes to the session key", func() {
-						// Code goes here ...
+					
+					It("should find the core.NODECOUNT closest nodes to the session key", func() {
+						Expect(len(s.Nodes)).To(Equal(core.NODECOUNT))
 					})
-
+					
 					It("should contain no duplicated nodes", func() {
-						// Code goes here ...
+						check := types.NewSet()
+						for _, node := range s.Nodes {
+							Expect(check.Contains(node.GID)).To(BeFalse())
+							check.Add(node.GID)
+						}
 					})
-
-					Describe("Nodes in an evenly distributed fashion", func() {
-
+					
+					Describe("SessionNodes in an evenly distributed fashion", func() {
+						
 						Context("Small pool of nodes, small number of trials", func() {
-
-							It("should result in evenly distributed nodes", func() {
-								// Code goes here ...
+							
+							PIt("should result in evenly distributed nodes", func() {
+								// TODO using golangs built in random
+								// TODO need crypto consideration to make truly random
 							})
 						})
-
+						
 						Context("Small pool of nodes, large number of trials", func() {
-
-							It("should be evenly distributed", func() {
-								// Code goes here ...
+							
+							PIt("should be evenly distributed", func() {
+								// TODO using golangs built in random
+								// TODO need crypto consideration to make truly random
 							})
 						})
-
+						
 						Context("Large pool of nodes, small number of trials", func() {
-
-							It("should be evenly distributed", func() {
-								// Code goes here ...
+							
+							PIt("should be evenly distributed", func() {
+								// TODO using golangs built in random
+								// TODO need crypto consideration to make truly random
 							})
 						})
-
+						
 						Context("Large pool of nodes, large number of trials", func() {
-
-							It("should be evenly distributed", func() {
-								// Code goes here ...
+							
+							PIt("should be evenly distributed", func() {
+								// TODO using golangs built in random
+								// TODO need crypto consideration to make truly random
 							})
 						})
 					})
 				})
-
-				Describe("Role assignment", func() {
-
-					It("should assign roles to each node", func() {
-						// including service nodes, validator nodes, and the delegated minter
-					})
-
-					It("should check the validity of the assigned roles", func() {
-						// validators must meet the admission requirements
-						// no duplicates
-					})
-
-					It("should assign roles to nodes proportional to the protocol guidelines", func() {
-						// 1:2 ratio for servicers to validators currently
-					})
-				})
-
+				
 				Describe("Deterministic from the seed data", func() {
-
+					
 					Context("2 sessions derived from valid same seed data", func() {
 						It("should be = and valid", func() {
-							// code goes here...
+							s1, _ := core.NewSession(validSeed)
+							s2, _ := core.NewSession(validSeed)
+							s3, _ := core.NewSession(validSeed)
+							s4, _ := core.NewSession(validSeed)
+							Expect(s1).To(Equal(s2))
+							Expect(s2).To(Equal(s3))
+							Expect(s3).To(Equal(s4))
 						})
 					})
-
+					
 					Context("2 sessions derived from different valid seed data", func() {
+						validSeed1, _ := core.NewSessionSeed(core.SHA3FromString("foo"), absPath, requestedChainHash, blockhash, capacity)
+						validSeed2, _ := core.NewSessionSeed(core.SHA3FromString("bar"), absPath, requestedChainHash, blockhash, capacity)
 						It("should be != and valid", func() {
-							// code goes here...
+							s1, _ := core.NewSession(validSeed1)
+							s2, _ := core.NewSession(validSeed2)
+							Expect(s1).ToNot(Equal(s2))
 						})
 					})
 				})
-
-				Describe("Expose node info", func() {
-
-					Describe("For the developer", func() {
-
-						It("should expose the devID", func() {
-							// Code goes here ...
+				
+				Describe("Expose session data", func() {
+					Context("Node data", func() {
+						It("should not be nil or zero value", func(){
+							Expect(s.Nodes).ToNot(BeNil())
+							Expect(len(s.Nodes)).ToNot(BeZero())
 						})
 					})
-
-					Describe("For the nodes", func() {
-
-						It("should expose the validator node host, port and net protocol", func() {
-							// Code goes here ...
+					Context("Blockhash", func() {
+						It("should not be nil or zero value", func() {
+							Expect(s.BlockHash).ToNot(BeNil())
+							Expect(len(s.BlockHash)).ToNot(BeZero())
 						})
-
-						It("should expose the unique identifier", func() {
-							// Code goes here ...
+					})
+					Context("Devid", func() {
+						It("should not be nil or zero value", func() {
+							Expect(s.DevID).ToNot(BeNil())
+							Expect(s.DevID).ToNot(BeZero())
 						})
-
-						It("should expose the role", func() {
-							// Code goes here ...
+					})
+					Context("Chain", func() {
+						It("should not be nil or zero value", func() {
+							Expect(s.Chain).ToNot(BeNil())
+							Expect(s.Chain).ToNot(BeZero())
+						})
+					})
+					Context("Key", func() {
+						It("should not be nil or zero value", func() {
+							Expect(s.Key).ToNot(BeNil())
+							Expect(len(s.Key)).ToNot(BeZero())
+						})
+					})
+					Context("Capacity", func() {
+						It("should not be nil or zero value", func() {
+							Expect(s.Capacity).ToNot(BeZero())
 						})
 					})
 				})
