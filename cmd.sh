151b3240102dafd510f465084f1cbf5de07cd760
Luis de Leon <luyzdeleon@gmail.com>
2019-04-08 09:35:11 -0400

    Changed default for requestTimeout flag to 0

--- cmd.sh
+++ cmd.sh
@@ -13,7 +13,7 @@ set -o nounset
 # AWS_SECRET_ACCESS_KEY = aws secret key to download the S3 / connect to dynamodb (dispatch only)
 # POCKET_CORE_DISPATCH_IP = Dispatch node address (defaults to 127.0.0.1)
 # POCKET_CORE_DISPATCH_PORT = Dispatch node port (defaults to 8081)
-# POCKET_CORE_REQUEST_TIMEOUT = Default request timeout (in ms) to use when doing network requests (defaults to 400)
+# POCKET_CORE_REQUEST_TIMEOUT = Default request timeout (in ms) to use when doing network requests (defaults to 0)
 
 # Dispatch only
 # AWS_DYNAMODB_ENDPOINT = AWS dynamodb endpoint (defaults to dynamodb.us-east-1.amazonaws.com)
@@ -38,18 +38,17 @@ if [ ${POCKET_CORE_NODE_TYPE:-service} = "dispatch" ]; then
 	  --disip ${POCKET_CORE_DISPATCH_IP:-127.0.0.1} \
 	  --disrport ${POCKET_CORE_DISPATCH_PORT:-8081} \
 	  --gid ${POCKET_CORE_DISPATCH_GID:-GID1} \
-	  --requestTimeout ${POCKET_CORE_REQUEST_TIMEOUT:-400}
+	  --requestTimeout ${POCKET_CORE_REQUEST_TIMEOUT:-0}
 
 elif [ ${POCKET_CORE_NODE_TYPE:-service} = "service" ]; then
 	echo 'Starting pocket-core service'
-
 	exec pocket-core --datadirectory ${POCKET_PATH_DATADIR:-datadir} \
 		--disip ${POCKET_CORE_DISPATCH_IP:-dispatch.pokt.network} \
 		--gid ${POCKET_CORE_SERVICE_GID:-GID2} \
 		--ip ${POCKET_CORE_SERVICE_IP:-127.0.0.1} \
 		--disrport ${POCKET_CORE_DISPATCH_PORT:-443} \
 		--port ${POCKET_CORE_SERVICE_PORT:-8081} \
-		--requestTimeout ${POCKET_CORE_REQUEST_TIMEOUT:-400}
+		--requestTimeout ${POCKET_CORE_REQUEST_TIMEOUT:-0}
 
 else
 	echo 'Need to specify a node type, either dispatch or service.'
