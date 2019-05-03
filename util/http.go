902a3dcdb4c4afa10189c0f3592a74cc5be9646f
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-05-03 12:44:01 -0400

    added response.body.close() to some http calls

--- util/http.go
+++ util/http.go
@@ -66,5 +66,6 @@ func rpcRequ(url string, req *http.Request) (string, error) {
 	if err != nil {
 		return "", errors.New("Unable to unmarshal response: " + err.Error())
 	}
+	defer resp.Body.Close()
 	return string(body), nil
 }
