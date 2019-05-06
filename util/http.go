21302a778e4d4d16cc3433cfec1e2466bc13ea44
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-05-06 09:51:44 -0400

    fixed nil pointer error

--- util/http.go
+++ util/http.go
@@ -23,10 +23,10 @@ func (m Method) String() string {
 // "RPCRequ" executes an RPC request
 func RPCRequ(url string, data []byte, m Method) (string, error) {
 	req, err := http.NewRequest(m.String(), url, bytes.NewBuffer(data))
-	req.Close = true
 	if err != nil {
 		return "", errors.New("Cannot convert struct to json " + err.Error())
 	}
+	defer req.Body.Close()
 	return rpcRequ(url, req)
 }
 
@@ -40,23 +40,31 @@ func StructRPCReq(url string, data interface{}, m Method) (string, error) {
 	}
 	// create new post request
 	req, err := http.NewRequest(m.String(), url, bytes.NewBuffer(j))
-	req.Close = true
 	// hanlde error
 	if err != nil {
 		return "", errors.New("Cannot create request " + err.Error())
 	}
+	defer req.Body.Close()
 	return rpcRequ(url, req)
 }
 
 func rpcRequ(url string, req *http.Request) (string, error) {
+	if req == nil {
+		return "", errors.New("nil request passed to function")
+	}
+	if req.Body == nil {
+		return "", errors.New("nil request passed to function")
+	}
+	defer req.Body.Close()
 	// setup header for json data
 	req.Header.Set("Content-Type", "application/json")
 	// setup http client
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", errors.New("Unable to do request " + err.Error())
+		return "", errors.New("unable to do request " + err.Error())
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		body, _ := ioutil.ReadAll(resp.Body)
 		return "", errors.New(string(body))
@@ -64,8 +72,7 @@ func rpcRequ(url string, req *http.Request) (string, error) {
 	// get body of response
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", errors.New("Unable to unmarshal response: " + err.Error())
+		return "", errors.New("unable to unmarshal response: " + err.Error())
 	}
-	defer resp.Body.Close()
 	return string(body), nil
 }
