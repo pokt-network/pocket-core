9adcc20f1ffa42011aed62c4b8f360e997e22507
Luis de Leon <luyzdeleon@gmail.com>
2019-09-11 17:49:59 -0400

    Refactored crypto module

--- crypto/address.go
+++
@@ -1,6 +0,0 @@
-package crypto
-
-import "github.com/tendermint/tendermint/crypto"
-
-// wrapper around tendermint address
-type Address crypto.Address
--- crypto/keys.go
+++ crypto/keys.go
@@ -1,24 +1,111 @@
 package crypto
 
 import (
-	crypt "github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	errors "github.com/pokt-network/pocket-core/error"
 )
 
-// wrapper around tendermint public key
-type PublicKey crypt.PubKey
-
-// wrapper around tendermint private key
-type PrivateKey crypt.PrivKey
-
-func NewKeypair() (privateKey PrivateKey, publicKey PublicKey) {
-	privateKey = NewPrivateKey()
-	publicKey = privateKey.PubKey()
-	return
+// wrapper around go-secp256k1 public key
+type PublicKey struct {
+	ecdsaPubKey ecdsa.PublicKey
 }
 
-func NewPrivateKey() PrivateKey {
-	return secp256k1.GenPrivKey()
+// wrapper around go-secp256k1 private key
+type PrivateKey struct {
+	ecdsaPrivKey *ecdsa.PrivateKey
 }
 
-// todo need function to convert hex string or bytes into public key
+// Private Key functions
+// TODO: Create a function to import private key from []byte or hex dump.
+
+// Generates a new private key
+func NewPrivateKey() (PrivateKey, error) {
+	ecdsaPrivKey, err := ecdsa.GenerateKey(S256(), rand.Reader)
+	if err != nil {
+		return PrivateKey{}, err
+	}
+	return PrivateKey{ecdsaPrivKey}, nil
+}
+
+// Returns the private key public key
+func (privKey *PrivateKey) GetPublicKey() PublicKey {
+	return PublicKey{privKey.ecdsaPrivKey.PublicKey}
+}
+
+// Dumps the PrivateKey on a []bytes
+func (privKey *PrivateKey) Bytes() []byte {
+	if privKey == nil {
+		return nil
+	}
+	return paddedBigBytes(privKey.ecdsaPrivKey.D, privKey.ecdsaPrivKey.Params().BitSize/8)
+}
+
+// Signs a message, the message has to be [32]byte
+func (privKey *PrivateKey) Sign(message []byte) ([]byte, error) {
+	if privKey == nil {
+		return nil, errors.InvalidPrivateKeyError
+	}
+	return secp256k1.Sign(message, privKey.Bytes())
+}
+
+// Public Key functions
+
+// Dumps the PublicKey on a []bytes
+func (pubKey *PublicKey) Bytes() []byte {
+	if pubKey == nil {
+		return nil
+	}
+	return elliptic.Marshal(S256(), pubKey.ecdsaPubKey.X, pubKey.ecdsaPubKey.Y)
+}
+
+// Creates a PublicKey instance from bytes
+func publicKeyFromBytes(bytes []byte) PublicKey {
+	s256curve := S256()
+	x, y := s256curve.ScalarBaseMult(bytes)
+	//s256curve.ScalarMult
+	ecdsaPublicKey := ecdsa.PublicKey{Curve: s256curve, X: x, Y: y}
+	return PublicKey{ecdsaPublicKey}
+}
+
+// KeyPair functions
+
+// Creates a new key pair
+func NewKeypair() (PrivateKey, PublicKey, error) {
+	var privKey PrivateKey
+	var pubKey PublicKey
+	privKey, err := NewPrivateKey()
+	if err != nil {
+		return privKey, pubKey, err
+	}
+	pubKey = privKey.GetPublicKey()
+
+	return privKey, pubKey, nil
+}
+
+// Signature Functions
+
+// "GetPublicKeyFromSignature" confirms the public key from the signature.
+func GetPublicKeyBytesFromSignature(messageHash, signature []byte) ([]byte, error) {
+	// RecoverPubkey returns the public key of the signer.
+	// msg must be the 32-byte Hash of the message to be signed.
+	// sig must be a 65-byte compact ECDSA signature containing the
+	// recovery id as the last element.
+	return secp256k1.RecoverPubkey(messageHash, signature)
+}
+
+// "VerifySignature" verifies the signature with the public key.
+func VerifySignature(pubKey PublicKey, messageHash []byte, signature []byte) bool {
+	// VerifySignature checks that the given pubkey created signature over message.
+	// The signature should be in [R || S] format. !!! Remove V (last byte)
+	return secp256k1.VerifySignature(pubKey.Bytes(), messageHash, signature[:len(signature)-1])
+}
+
+// "VerifySignature" verifies the signature with the public key bytes.
+func VerifySignatureWithPubKeyBytes(pubKeyBytes []byte, messageHash []byte, signature []byte) bool {
+	// VerifySignatureWithPubKeyBytes checks that the given pubkey bytes created signature over message.
+	// The signature should be in [R || S] format. !!! Remove V (last byte)
+	return secp256k1.VerifySignature(pubKeyBytes, messageHash, signature[:len(signature)-1])
+}
