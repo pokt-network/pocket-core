e9b6979f79353d9cb77deeef560add270e9a4543
Luis de Leon <luyzdeleon@gmail.com>
2019-09-11 17:48:21 -0400

    Closes #439 / Fixes AAT BDD Tests

--- tests/bdd/aat/aat_test.go
+++ tests/bdd/aat/aat_test.go
@@ -2,7 +2,6 @@ package aat_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 // ************************************************************************************************************
@@ -19,36 +18,36 @@ var _ = Describe("Application Auth Token", func() {
 
 		PContext("Invalid Token", func() {
 			PContext("Attributes are missing or null", func() {
-				PContext("Missing version", func{
+				PContext("Missing version", func() {
 					PIt("should return missing version error", func() {})
 				})
 
-				PContext("Missing message", func{
+				PContext("Missing message", func() {
 					PIt("should return missing message error", func() {})
 				})
 
-				PContext("Missing appPubKey", func{
+				PContext("Missing appPubKey", func() {
 					PIt("should return missing appPubKey error", func() {})
 				})
 
-				PContext("Missing signature", func{
+				PContext("Missing signature", func() {
 					PIt("should return missing signature error", func() {})
 				})
 			})
 
 			PContext("version is incorrect", func() {
 				PContext("Version not in semver format", func() {
-					PIt("should return invalid version format error", func () {})
+					PIt("should return invalid version format error", func() {})
 				})
 
 				PContext("Version not supported", func() {
-					PIt("should return version not supported error", func () {})
+					PIt("should return version not supported error", func() {})
 				})
 			})
 
 			PContext("message is incorrect", func() {
 				PContext("Attributes are missing or null", func() {
-					PContext("Missing applicationAddress", func{
+					PContext("Missing applicationAddress", func() {
 						PIt("should return missing applicationAddress error", func() {})
 					})
 				})
@@ -56,17 +55,17 @@ var _ = Describe("Application Auth Token", func() {
 
 			PContext("appPubKey is incorrect", func() {
 				PContext("appPubKey has an invalid format", func() {
-					PIt("should return invalid appPubKey error", func () {})
+					PIt("should return invalid appPubKey error", func() {})
 				})
 			})
 
 			PContext("signature is incorrect", func() {
 				PContext("appPubKey doesn't match signature", func() {
-					PIt("should return invalid signature error", func () {})
+					PIt("should return invalid signature error", func() {})
 				})
 
 				PContext("signature doesn't match message", func() {
-					PIt("should return invalid signature error", func () {})
+					PIt("should return invalid signature error", func() {})
 				})
 			})
 		})
