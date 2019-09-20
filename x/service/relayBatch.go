b42c92c4b8fa6f410a506585f69596fe26943037
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-09-20 09:49:53 -0400

    added specifics types for list and set

--- x/service/relayBatch.go
+++ x/service/relayBatch.go
@@ -1,3 +1,17 @@
-package service
+package types
 
-//todo
+import "github.com/pokt-network/pocket-core/x/service"
+
+type RelayBatch struct {
+	RelayBatchHeader
+	Evidence
+}
+
+type RelayBatchHeader struct {
+	SessionHash       string
+	ApplicationPubKey string
+}
+
+type RelayBatches map[RelayBatchHeader]RelayBatch // public key of Application -> RelayBatch
+
+type Evidence []service.ServiceAuthentication
