47dc37e0f6f7fe2e54212380506a692a0b816b51
aqt01 <lowell.abbott@gmail.com>
2019-09-27 17:10:03 -0400

    Add graceful exit

--- cmd/pocket_core/main.go
+++ cmd/pocket_core/main.go
@@ -2,12 +2,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pokt-network/pocket-core/config"
 	"github.com/pokt-network/pocket-core/crypto"
 	"github.com/pokt-network/pocket-core/db"
 	"github.com/pokt-network/pocket-core/logs"
 	"github.com/pokt-network/pocket-core/node"
 	"github.com/pokt-network/pocket-core/rpc"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // "init" is a built in function that is automatically called before main.
@@ -23,6 +27,16 @@ func main() {
 
 // "startClient" Starts the client with the given initial configuration.
 func startClient() {
+
+	// We trap kill $signals (2,3,15,9)
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		os.Kill,
+		os.Interrupt)
+
 	// initializes the configuration from flags and defaults
 	config.Init()
 	// builds the proper structure on pc for core client to operate
@@ -45,6 +59,14 @@ func startClient() {
 	node.Register()
 	// // logs the client starting
 	logs.NewLog("Started Pocket Core", logs.InfoLevel, logs.JSONLogFormat)
-	// hang and wait for exit signal
-	node.WaitForExit()
+
+	// Capture signal
+	<-signalChannel
+
+	// Exit gracefully printing the exit signal
+	defer func() {
+		message := fmt.Sprintf("Exit signal %v received\n", signalChannel)
+		node.WaitForExit(message)
+	}()
+
 }
