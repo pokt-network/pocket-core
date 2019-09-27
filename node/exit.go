36269bbce0fba4bb08c04222d9fd200d251391da
aqt01 <lowell.abbott@gmail.com>
2019-09-27 17:10:03 -0400

    Add graceful exit

--- node/exit.go
+++ node/exit.go
@@ -2,10 +2,8 @@ package node
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-
 	"github.com/pokt-network/pocket-core/logs"
+	"os"
 )
 
 // "ExitGracefully" is the shutdown sequece of Pocket Core
@@ -18,17 +16,11 @@ func ExitGracefully(message string) {
 	}
 	logs.NewLog("Shutting down Pocket Core: "+message, logs.InfoLevel, logs.JSONLogFormat)
 	fmt.Fprint(os.Stdout, "Shutting down Pocket Core: "+message)
-	os.Exit(0)
+	// Exit honoring deferred calls
+	os.Exit(3)
 }
 
 // "WaitForExit" listens for interrupt signal and calls unregister
-func WaitForExit() {
-	// Catches OS system interrupt signal and calls unregister
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
-	select {
-	case sig := <-c:
-		ExitGracefully(sig.String() + " command executed.")
-	}
+func WaitForExit(message string) {
+	ExitGracefully(message)
 }
