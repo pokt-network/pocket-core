096209b2ca26d932c3a4c2604461fbd25e7d0818
aqt01 <lowell.abbott@gmail.com>
2019-10-01 15:33:22 -0400

    Add trapping kill signals  (2,3,15,9) and exitting with 3 for honoring deferred signals

--- util/exit.go
+++ util/exit.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"os"
-	"os/signal"
 
 	"github.com/pokt-network/pocket-core/logs"
 )
@@ -11,16 +10,5 @@ func ExitGracefully(message string) {
 	logs.Log("Shutting down Pocket Core: "+message, logs.InfoLevel, logs.JSONLogFormat)
 	logs.Log("Shutting down Pocket Core: "+message, logs.InfoLevel, logs.TextLogFormatter)
 
-	os.Exit(0)
-}
-
-func WaitForExit() {
-	// Catches OS system interrupt signal and calls unregister
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
-	select {
-	case sig := <-c:
-		ExitGracefully(sig.String() + " command executed.")
-	}
+	os.Exit(3)
 }
