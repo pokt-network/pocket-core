50a0b02972aeccbedaaeb84684ca338122cad203
aqt01 <lowell.abbott@gmail.com>
2019-10-01 15:33:22 -0400

    Add trapping kill signals  (2,3,15,9) and exitting with 3 for honoring deferred signals

--- cmd/pocket_core/main.go
+++ cmd/pocket_core/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/pokt-network/pocket-core/util"
 	"math/rand"
 	"time"
+	"os"
+	"fmt"
+	"os/signal"
+	"syscall"
 )
 
 // "init" is a built in function that is automatically called before main.
@@ -23,6 +27,16 @@ func main() {
 
 // "startClient" Starts the client with the given initial configuration.
 func startClient() {
+	
+	// We trap kill signals (2,3,15,9)
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		os.Kill,
+		os.Interrupt)
+
 	// initializes the configuration from flags and defaults
 	config.Init()
 	// builds the proper structure on pc for core client to operate
@@ -35,6 +49,12 @@ func startClient() {
 	logs.Log("Started Pocket Core", logs.InfoLevel, logs.TextLogFormatter)
 	// print the configuration
 	config.Print()
-	// wait for the interrupt command
-	util.WaitForExit()
+	// Capture signal
+	<-signalChannel
+
+	// Exits gracefully printing the exit signal
+	defer func() {
+		message := fmt.Sprintf("Exit signal %v received\n", signalChannel)
+		util.ExitGracefully(message)
+	}()
 }
