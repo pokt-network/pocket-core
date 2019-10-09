95b3785bee37bc34b4f7915d5ecb3b35720611d5
aqt01 <lowell.abbott@gmail.com>
2019-10-09 17:21:35 -0400

    Appends config.Logformat at end of file and logs json format or text format depending of the variable given

--- logs/methods.go
+++ logs/methods.go
@@ -61,7 +61,7 @@ func Log(message string, level LogLevel, format LogFormat) error {
 func logger(l log, format LogFormat) error {
 	// open/create the new log file
 	f, err := os.OpenFile(config.GlobalConfig().DD+string(filepath.Separator)+"logs"+
-		string(filepath.Separator)+l.Name, os.O_WRONLY|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		string(filepath.Separator)+l.Name+string(*config.Logformat), os.O_WRONLY|os.O_CREATE|os.O_APPEND, os.ModePerm)
 	if err != nil {
 		return err
 	}
@@ -72,21 +72,42 @@ func logger(l log, format LogFormat) error {
 	// We create empty log fields by default
 	lg := logrus.WithFields(logrus.Fields{})
 
-	// If using textformatter, we output to stdout, in case of json we output to json
+	// If using textformatter, we output to stdout
 	if reflect.TypeOf(format.format) == reflect.TypeOf(&logrus.TextFormatter{}) {
 		logrus.SetOutput(os.Stdout)
 
+	// If we are using json, we just log to file .log or .json depending of logformat
 	} else if reflect.TypeOf(format.format) == reflect.TypeOf(&logrus.JSONFormatter{}) {
-		lg = logrus.WithFields(
-			logrus.Fields{
-				"FilePath":     l.FilePath,     // the path of the file the log was called from
-				"LineNumber":   l.LineNumber,   // the line number of the file the log was called from
-				"FunctionName": l.FunctionName, // the function name of the file the log was called
-			})
-		logrus.SetOutput(f)
+		if *config.Logformat == ".json" {
+			lg = logrus.WithFields(
+				logrus.Fields{
+					"FilePath":     l.FilePath,     // the path of the file the log was called from
+					"LineNumber":   l.LineNumber,   // the line number of the file the log was called from
+					"FunctionName": l.FunctionName, // the function name of the file the log was called
+				})
+			logrus.SetOutput(f)
+		} else if *config.Logformat == ".log" {
+			Textformatter := new(logrus.TextFormatter)
+			Textformatter.TimestampFormat = "02-01-2006 15:04:05"
+			Textformatter.FullTimestamp = true
+
+			lg = logrus.WithFields(
+				logrus.Fields{
+					"FilePath":     l.FilePath,     // the path of the file the log was called from
+					"LineNumber":   l.LineNumber,   // the line number of the file the log was called from
+					"FunctionName": l.FunctionName, // the function name of the file the log was called
+				})
+
+			logrus.SetFormatter(Textformatter)
+			logrus.SetOutput(f)
+		}
 
 	}
+	return writeToLog(l, lg)
 
+}
+
+func writeToLog(l log, lg *logrus.Entry) error {
 	switch l.Lev.level {
 	case logrus.InfoLevel:
 		lg.Info(l.Message)
