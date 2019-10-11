9e1d31bed0d5ece08a9a1a096629df136581f51a
aqt01 <lowell.abbott@gmail.com>
2019-10-11 16:44:33 -0400

    Adds logSize, logAge, logBackups and logCompress flags

--- config/config.go
+++ config/config.go
@@ -12,28 +12,36 @@ import (
 )
 
 type config struct {
-	CID       string `json:"clientid"`     // This variable holds a client identifier string.
-	Ver       string `json:"version"`      // This variable holds the client version string.
-	DD        string `json:"datadir"`      // This variable holds the working directory string.
-	CRPC      bool   `json:"crpc"`         // This variable describes if the client rpc is running.
-	CRPCPort  string `json:"crpcport"`     // This variable holds the client rpc port string.
-	RRPC      bool   `json:"rrpc"`         // This variable describes if the relay rpc is running.
-	RRPCPort  string `json:"rrpcport"`     // This variable holds the relay rpc port string.
-	CFile     string `json:"hostedchains"` // This variable holds the filepath to the chains.json.
-	Logformat string `json:"logformat"`    // This variable holds the filepath to the chains.json.
+	CID         string `json:"clientid"`     // This variable holds a client identifier string.
+	Ver         string `json:"version"`      // This variable holds the client version string.
+	DD          string `json:"datadir"`      // This variable holds the working directory string.
+	CRPC        bool   `json:"crpc"`         // This variable describes if the client rpc is running.
+	CRPCPort    string `json:"crpcport"`     // This variable holds the client rpc port string.
+	RRPC        bool   `json:"rrpc"`         // This variable describes if the relay rpc is running.
+	RRPCPort    string `json:"rrpcport"`     // This variable holds the relay rpc port string.
+	CFile       string `json:"hostedchains"` // This variable holds the filepath to the chains.json.
+	LogFormat   string `json:"logformat"`    // This variable changes the log storage format
+	LogSize     int    `json:"logsize"`      // This variable sets the log file size specified in megabytes.
+	LogAge      int    `json:"logage"`       // This variable sets the log file age specified in days.
+	LogBackups  int    `json:"logbackups"`   // This variable sets the log file backup quantity.
+	LogCompress bool   `json:"logcompress"`  // This variable enabled the log compression.
 
 }
 
 var (
-	c         *config
-	once      sync.Once
-	dd        = flag.String("datadirectory", _const.DATADIR, "setup the data directory for the DB and keystore")
-	rRpcPort  = flag.String("relayrpcport", "8081", "specified port to run relay rpc")
-	cFile     = flag.String("cfile", _const.CHAINSFILENAME, "specifies the filepath for chains.json")
-	cRpcPort  = flag.String("clientrpcport", "8080", "specified port to run client rpc")
-	cRpc      = flag.Bool("clientrpc", true, "whether or not to start the rpc server")
-	rRpc      = flag.Bool("relayrpc", true, "whether or not to start the rpc server")
-	Logformat = flag.String("logformat", ".json", "Log format for storing logs [.json, .log] .json is used by default")
+	c           *config
+	once        sync.Once
+	dd          = flag.String("datadirectory", _const.DATADIR, "setup the data directory for the DB and keystore")
+	rRpcPort    = flag.String("relayrpcport", "8081", "specified port to run relay rpc")
+	cFile       = flag.String("cfile", _const.CHAINSFILENAME, "specifies the filepath for chains.json")
+	cRpcPort    = flag.String("clientrpcport", "8080", "specified port to run client rpc")
+	cRpc        = flag.Bool("clientrpc", true, "whether or not to start the rpc server")
+	rRpc        = flag.Bool("relayrpc", true, "whether or not to start the rpc server")
+	logFormat   = flag.String("logformat", ".json", "Log format for storing logs.  ex.:[.json, .log] ('.json' is used by default)")
+	logSize     = flag.Int("logsize", 500, "Log size of logs before rotating. Size is given in Megabytes. ex.: [500 = 500MB] ('500' used by default)")
+	logAge      = flag.Int("logage", 15, "Log rotate age. Age is given in days. ex.: [20 = 20 days] ('15' used by default)")
+	logBackups  = flag.Int("logbackups", 10, "Log backup quantity. Quantity of logfiles to keep before deleting. ex.: [20 = 20 backups files] ('15' used by default)")
+	logCompress = flag.Bool("logcompress", true, "Enables log compress. Quantity of logfiles to keep before deleting. ex.: [20 = 20 backups files] ('15' used by default)")
 )
 
 // "Init" initializes the configuration object.
@@ -88,5 +96,9 @@ func newConfiguration() {
 		*rRpc,
 		*rRpcPort,
 		*cFile,
-		*Logformat}
+		*logFormat,
+		*logSize,
+		*logAge,
+		*logBackups,
+		*logCompress}
 }
