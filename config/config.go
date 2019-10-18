ef08b8c3bce465deadb768634a2a0eedabc47d0c
aqt01 <lowell.abbott@gmail.com>
2019-10-17 22:36:57 -0400

    Set default --logformat to .json only when user pass --logdir

--- config/config.go
+++ config/config.go
@@ -12,38 +12,43 @@ import (
 )
 
 type config struct {
-	CID         string `json:"clientid"`     // This variable holds a client identifier string.
-	Ver         string `json:"version"`      // This variable holds the client version string.
-	DD          string `json:"datadir"`      // This variable holds the working directory string.
-	CRPC        bool   `json:"crpc"`         // This variable describes if the client rpc is running.
-	CRPCPort    string `json:"crpcport"`     // This variable holds the client rpc port string.
-	RRPC        bool   `json:"rrpc"`         // This variable describes if the relay rpc is running.
-	RRPCPort    string `json:"rrpcport"`     // This variable holds the relay rpc port string.
-	CFile       string `json:"hostedchains"` // This variable holds the filepath to the chains.json.
-	LogFormat   string `json:"logformat"`    // This variable changes the log storage format.
-	LogDir	    string `json:"logdir"`       // This variable changes the log storage dir. 
-
+	CID       string `json:"clientid"`     // This variable holds a client identifier string.
+	Ver       string `json:"version"`      // This variable holds the client version string.
+	DD        string `json:"datadir"`      // This variable holds the working directory string.
+	CRPC      bool   `json:"crpc"`         // This variable describes if the client rpc is running.
+	CRPCPort  string `json:"crpcport"`     // This variable holds the client rpc port string.
+	RRPC      bool   `json:"rrpc"`         // This variable describes if the relay rpc is running.
+	RRPCPort  string `json:"rrpcport"`     // This variable holds the relay rpc port string.
+	CFile     string `json:"hostedchains"` // This variable holds the filepath to the chains.json.
+	LogFormat string `json:"logformat"`    // This variable changes the log storage format.
+	LogDir    string `json:"logdir"`       // This variable changes the log storage dir.
 
 }
 
 var (
-	c           *config
-	once        sync.Once
-	dd          = flag.String("datadirectory", _const.DATADIR, "setup the data directory for the DB and keystore")
-	rRpcPort    = flag.String("relayrpcport", "8081", "specified port to run relay rpc")
-	cFile       = flag.String("cfile", _const.CHAINSFILENAME, "specifies the filepath for chains.json")
-	cRpcPort    = flag.String("clientrpcport", "8080", "specified port to run client rpc")
-	cRpc        = flag.Bool("clientrpc", true, "whether or not to start the rpc server")
-	rRpc        = flag.Bool("relayrpc", true, "whether or not to start the rpc server")
-	logFormat   = flag.String("logformat", "", "Log format for storing logs.  ex.:[.json, .log] ('.json' is used by default)")
-	logDir      = flag.String("logdir", _const.DATADIR + "/logs", "setup the log directory.  ex.:['/var/log/'] ('~/.pocket/logs' is used by default)")
-
+	c         *config
+	once      sync.Once
+	dd        = flag.String("datadirectory", _const.DATADIR, "setup the data directory for the DB and keystore")
+	rRpcPort  = flag.String("relayrpcport", "8081", "specified port to run relay rpc")
+	cFile     = flag.String("cfile", _const.CHAINSFILENAME, "specifies the filepath for chains.json")
+	cRpcPort  = flag.String("clientrpcport", "8080", "specified port to run client rpc")
+	cRpc      = flag.Bool("clientrpc", true, "whether or not to start the rpc server")
+	rRpc      = flag.Bool("relayrpc", true, "whether or not to start the rpc server")
+	logFormat = flag.String("logformat", "", "Log format for storing logs.  ex.:[.json, .log] ('.json' is used by default)")
+	logDir    = flag.String("logdir", _const.DATADIR+"/logs", "setup the log directory.  ex.:['/var/log/'] ('~/.pocket/logs' is used by default)")
 )
 
 // "Init" initializes the configuration object.
 func Init() {
 	// built in function to parse the flags above.
 	flag.Parse()
+
+	// In case user sets logdir flag without logformat, we assume logformat to be .json by default
+	if isFlagPassed("logdir") == true {
+		if isFlagPassed("logformat") == false {
+			flag.Set("logformat", ".json")
+		}
+	}
 	// sets up filepaths for config files
 	filePaths()
 	// returns the thread safe c of the client configuration.
@@ -71,6 +76,17 @@ func Print() {
 	data, _ := json.MarshalIndent(c, "", "    ")
 	var output = fmt.Sprintf("%s", string(data))
 	logger(output)
+}
+
+// Validate if a flag has value
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
 
 // "GlobalConfig()" returns the configuration object in a thread safe manner.
