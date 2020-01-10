4ebccd57c60355e46fcd01f8ee59e7197d55102a
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-01-10 02:01:18 -0400

    add application tests

---
+++ x/apps/keeper/application_test.go
@@ -0,0 +1,169 @@
+package keeper
+
+import (
+	"github.com/pokt-network/pocket-core/x/apps/exported"
+	"github.com/pokt-network/pocket-core/x/apps/types"
+	sdk "github.com/pokt-network/posmint/types"
+	"reflect"
+	"testing"
+)
+
+func TestApplication_SetAndGetApplication(t *testing.T) {
+	application := getBondedApplication()
+
+	tests := []struct {
+		name        string
+		application types.Application
+		want        bool
+	}{
+		{
+			name:        "get and set application",
+			application: application,
+			want:        true,
+		},
+		{
+			name:        "not found",
+			application: application,
+			want:        false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true)
+
+			if tt.want {
+				keeper.SetApplication(context, tt.application)
+			}
+
+			if _, found := keeper.GetApplication(context, tt.application.Address); found != tt.want {
+				t.Errorf("Applicaiton.GetApplication() = got %v, want %v", found, tt.want)
+			}
+		})
+	}
+}
+
+func TestApplication_CalculateAppRelays(t *testing.T) {
+	application := getBondedApplication()
+
+	tests := []struct {
+		name        string
+		application types.Application
+		want        sdk.Int
+	}{
+		{
+			name:        "calculates App relays",
+			application: application,
+			want:        sdk.NewInt(100000000000),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true)
+
+			if got := keeper.CalculateAppRelays(context, tt.application); !got.Equal(tt.want) {
+				t.Errorf("Applicaiton.CalculateAppRelays() = got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestApplication_GetAllAplications(t *testing.T) {
+	application := getBondedApplication()
+
+	tests := []struct {
+		name        string
+		application types.Application
+		want        types.Applications
+	}{
+		{
+			name:        "gets all applications",
+			application: application,
+			want:        types.Applications([]types.Application{application}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true)
+
+			keeper.SetApplication(context, tt.application)
+
+			if got := keeper.GetAllApplications(context); !reflect.DeepEqual(got, tt.want){
+				t.Errorf("Applicaiton.GetAllApplications() = got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestApplication_GetAplications(t *testing.T) {
+	application := getBondedApplication()
+
+	tests := []struct {
+		name        string
+		application types.Application
+		maxRetrieve uint16
+		want        types.Applications
+	}{
+		{
+			name:        "gets all applications",
+			application: application,
+			maxRetrieve: 2,
+			want:        types.Applications([]types.Application{application}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true)
+
+			keeper.SetApplication(context, tt.application)
+
+			if got := keeper.GetApplications(context, tt.maxRetrieve); !reflect.DeepEqual(got, tt.want){
+				t.Errorf("Applicaiton.GetAllApplications() = got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestApplication_IterateAndExecuteOverApps(t *testing.T) {
+	application := getBondedApplication()
+	secondApp := getBondedApplication()
+
+	tests := []struct {
+		name        string
+		application types.Application
+		secondApplication types.Application
+		want       int
+	}{
+		{
+			name:        "iterates over all applications",
+			application: application,
+			secondApplication: secondApp,
+			want:       2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			context, _, keeper := createTestInput(t, true)
+
+			keeper.SetApplication(context, tt.application)
+			keeper.SetApplication(context, tt.secondApplication)
+			n := 0
+			fn := modifyStakedTokens(&n)
+			keeper.IterateAndExecuteOverApps(context, fn)
+			if n != tt.want {
+				t.Errorf("Applicaiton.IterateAndExecuteOverApps() = got %v, want %v", n, tt.want)
+			}
+		})
+	}
+}
+
+func modifyStakedTokens(i *int) func (index int64, application exported.ApplicationI) (stop bool){
+	return func(index int64, application exported.ApplicationI) (stop bool) {
+		app := application.(types.Application)
+		app.StakedTokens = sdk.NewInt(100)
+		if index == 1 {
+			stop = true
+		}
+		*i++
+		return
+	}
+}
