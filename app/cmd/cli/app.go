7bc6cb41fb66d40879c59dfab9f68c2ece24651b
Otto V <ottoevargas@gmail.com>
2020-02-04 18:17:35 -0400

    adding prompt issue #596

--- app/cmd/cli/app.go
+++ app/cmd/cli/app.go
@@ -50,6 +50,7 @@ var appUnstakeCmd = &cobra.Command{
 	Long:  `Unstake an app from the network, changing it's status to Unstaking. Prompts the user for the <fromAddr> account passphrase.`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Enter Password: ")
 		res, err := app.UnstakeApp(args[0], app.Credentials())
 		if err != nil {
 			panic(err)
@@ -61,9 +62,9 @@ var appUnstakeCmd = &cobra.Command{
 var createAATCmd = &cobra.Command{
 	Use:   "create-aat <appAddr> <clientPubKey>",
 	Short: "Creates an application authentication token",
-	Long: `Creates a signed application authentication token (version 0.0.1 of the AAT spec), that can be embedded into application software for Relay servicing. 
-Will prompt the user for the <appAddr> account passphrase. 
-Read the Application Authentication Token documentation for more. 
+	Long: `Creates a signed application authentication token (version 0.0.1 of the AAT spec), that can be embedded into application software for Relay servicing.
+Will prompt the user for the <appAddr> account passphrase.
+Read the Application Authentication Token documentation for more.
 NOTE: USE THIS METHOD AT YOUR OWN RISK. READ THE APPLICATION SECURITY GUIDELINES IN ORDER TO UNDERSTAND WHAT'S THE RECOMMENDED AAT CONFIGURATION FOR YOUR APPLICATION:`,
 	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
