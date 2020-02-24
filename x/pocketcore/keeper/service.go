1c5e5c7e0876c5258306cb1f1064c169d146b1c3
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-02-24 17:34:21 -0400

    add logs to pocketcore module

--- x/pocketcore/keeper/service.go
+++ x/pocketcore/keeper/service.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"fmt"
 	"encoding/hex"
 	pc "github.com/pokt-network/pocket-core/x/pocketcore/types"
 	sdk "github.com/pokt-network/posmint/types"
@@ -8,6 +9,7 @@ import (
 
 // this is the main call for a service node handling a relay request
 func (k Keeper) HandleRelay(ctx sdk.Context, relay pc.Relay) (*pc.RelayResponse, sdk.Error) {
+	ctx.Logger().Info(fmt.Sprintf("HandleRelay(Relay = %+v)", relay))
 	// get the latest session block height because this relay will correspond with the latest session
 	sessionBlockHeight := k.GetLatestSessionBlock(ctx).BlockHeight()
 	// retrieve all service nodes available from world state to do session generation (the session data is needed to service)
@@ -26,10 +28,12 @@ func (k Keeper) HandleRelay(ctx sdk.Context, relay pc.Relay) (*pc.RelayResponse,
 	}
 	// ensure the validity of the relay
 	if err := relay.Validate(ctx, selfNode, hostedBlockchains, sessionBlockHeight, int(k.SessionNodeCount(ctx)), allNodes, app); err != nil {
+		ctx.Logger().Error(fmt.Errorf("could not validate for %v, %v, %v %v, %v, %v \n", selfNode, hostedBlockchains, sessionBlockHeight, int(k.SessionNodeCount(ctx)), allNodes, app).Error())
 		return nil, err
 	}
 	// store the proof before execution, because the proof corresponds to the previous relay
 	if err := relay.HandleProof(ctx, sessionBlockHeight); err != nil {
+		ctx.Logger().Error(fmt.Errorf("could not handle proof for %v", sessionBlockHeight).Error())
 		return nil, err
 	}
 	// attempt to execute
@@ -51,6 +55,7 @@ func (k Keeper) HandleRelay(ctx sdk.Context, relay pc.Relay) (*pc.RelayResponse,
 	// sign the response
 	sig, _, er := (k.Keybase).Sign(selfNode.GetAddress(), k.coinbasePassphrase, resp.Hash())
 	if er != nil {
+		ctx.Logger().Error(fmt.Errorf("could not sign response for address: %v with hash: %v \n", selfNode.GetAddress().String(), resp.Hash()).Error())
 		return nil, pc.NewKeybaseError(pc.ModuleName, er)
 	}
 	resp.Signature = hex.EncodeToString(sig)
