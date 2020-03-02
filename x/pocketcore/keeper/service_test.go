0d9ca8df1a1f3784f92f3ca040babbebdde50819
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-03-01 23:30:59 -0400

    update remaining tests on pocketcore module

--- x/pocketcore/keeper/service_test.go
+++ x/pocketcore/keeper/service_test.go
@@ -1,74 +1,95 @@
 package keeper
 
-//
-//func TestKeeper_HandleRelay(t *testing.T) {
-//	ethereum, err := types.NonNativeChain{
-//		Ticker:  "eth",
-//		Netid:   "4",
-//		Version: "v1.9.9",
-//		Client:  "geth",
-//		Inter:   "",
-//	}.HashString()
-//	if err != nil {
-//		t.Fatalf(err.Error())
-//	}
-//	ctx, _, _, _, keeper := createTestInput(t, false)
-//	ak := keeper.appKeeper.(appsKeeper.Keeper)
-//	clientPrivateKey := getRandomPrivateKey()
-//	clientPubKey := clientPrivateKey.PublicKey().RawString()
-//	appPrivateKey := getRandomPrivateKey()
-//	apk := appPrivateKey.PublicKey()
-//	appPubKey := apk.RawString()
-//	// add app to world state
-//	app := appsTypes.NewApplication(sdk.Address(apk.Address()), apk, []string{ethereum}, sdk.NewInt(10000000))
-//	// calculate relays
-//	app.MaxRelays = ak.CalculateAppRelays(ctx, app)
-//	// set the vals from the data
-//	ak.SetApplication(ctx, app)
-//	ak.SetStakedApplication(ctx, app)
-//	kp, _ := keeper.Keybase.GetCoinbase()
-//	npk := kp.PublicKey
-//	nodePubKey := npk.RawString()
-//	validRelay := types.Relay{
-//		Payload: types.Payload{
-//			Data:    "{\"jsonrpc\":\"2.0\",\"method\":\"web3_clientVersion\",\"params\":[],\"id\":67}",
-//			Method:  "",
-//			Path:    "",
-//			Headers: nil,
-//		},
-//		Proof: types.RelayProof{
-//			Entropy:            1,
-//			SessionBlockHeight: 1,
-//			ServicerPubKey:     nodePubKey,
-//			Blockchain:         ethereum,
-//			Token: types.AAT{
-//				Version:              "0.0.1",
-//				ApplicationPublicKey: appPubKey,
-//				ClientPublicKey:      clientPubKey,
-//				ApplicationSignature: "",
-//			},
-//			Signature: "",
-//		},
-//	}
-//	appSig, er := appPrivateKey.Sign(validRelay.Proof.Token.Hash())
-//	if er != nil {
-//		t.Fatalf(er.Error())
-//	}
-//	validRelay.Proof.Token.ApplicationSignature = hex.EncodeToString(appSig)
-//	clientSig, er := clientPrivateKey.Sign(validRelay.Proof.Hash())
-//	if er != nil {
-//		t.Fatalf(er.Error())
-//	}
-//	validRelay.Proof.Signature = hex.EncodeToString(clientSig)
-//	defer gock.Off() // Flush pending mocks after test execution
-//
-//	gock.New("https://www.google.com").
-//		Post("/").
-//		Reply(200).
-//		BodyString("bar")
-//	resp, err := keeper.HandleRelay(ctx, validRelay)
-//	assert.Nil(t, err)
-//	assert.NotNil(t, resp)
-//	assert.NotEmpty(t, resp)
-//	assert.Equal(t, resp.Response, "bar")
-//}
+import (
+	"encoding/hex"
+	appsKeeper "github.com/pokt-network/pocket-core/x/apps/keeper"
+	appsTypes "github.com/pokt-network/pocket-core/x/apps/types"
+	"github.com/pokt-network/pocket-core/x/pocketcore/types"
+	sdk "github.com/pokt-network/posmint/types"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+	"testing"
+)
+
+func TestKeeper_HandleRelay(t *testing.T) {
+	ethereum, err := types.NonNativeChain{
+		Ticker:  "eth",
+		Netid:   "4",
+		Version: "v1.9.9",
+		Client:  "geth",
+		Inter:   "",
+	}.HashString()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	ctx, _, _, _, keeper, keys := createTestInput(t, false)
+	mockCtx := new(Ctx)
+
+	ak := keeper.appKeeper.(appsKeeper.Keeper)
+	clientPrivateKey := getRandomPrivateKey()
+	clientPubKey := clientPrivateKey.PublicKey().RawString()
+	appPrivateKey := getRandomPrivateKey()
+	apk := appPrivateKey.PublicKey()
+	appPubKey := apk.RawString()
+	// add app to world state
+	app := appsTypes.NewApplication(sdk.Address(apk.Address()), apk, []string{ethereum}, sdk.NewInt(10000000))
+	// calculate relays
+	app.MaxRelays = ak.CalculateAppRelays(ctx, app)
+	// set the vals from the data
+	ak.SetApplication(ctx, app)
+	ak.SetStakedApplication(ctx, app)
+	kp, _ := keeper.Keybase.GetCoinbase()
+	npk := kp.PublicKey
+	nodePubKey := npk.RawString()
+	validRelay := types.Relay{
+		Payload: types.Payload{
+			Data:    "{\"jsonrpc\":\"2.0\",\"method\":\"web3_clientVersion\",\"params\":[],\"id\":67}",
+			Method:  "",
+			Path:    "",
+			Headers: nil,
+		},
+		Proof: types.RelayProof{
+			Entropy:            1,
+			SessionBlockHeight: 1000,
+			ServicerPubKey:     nodePubKey,
+			Blockchain:         ethereum,
+			Token: types.AAT{
+				Version:              "0.0.1",
+				ApplicationPublicKey: appPubKey,
+				ClientPublicKey:      clientPubKey,
+				ApplicationSignature: "",
+			},
+			Signature: "",
+		},
+	}
+	appSig, er := appPrivateKey.Sign(validRelay.Proof.Token.Hash())
+	if er != nil {
+		t.Fatalf(er.Error())
+	}
+	validRelay.Proof.Token.ApplicationSignature = hex.EncodeToString(appSig)
+	clientSig, er := clientPrivateKey.Sign(validRelay.Proof.Hash())
+	if er != nil {
+		t.Fatalf(er.Error())
+	}
+	validRelay.Proof.Signature = hex.EncodeToString(clientSig)
+	defer gock.Off() // Flush pending mocks after test execution
+
+	gock.New("https://www.google.com").
+		Post("/").
+		Reply(200).
+		BodyString("bar")
+
+	mockCtx.On("KVStore", keeper.storeKey).Return(ctx.KVStore(keeper.storeKey))
+	mockCtx.On("KVStore", keys["pos"]).Return(ctx.KVStore(keys["pos"]))
+	mockCtx.On("KVStore", keys["params"]).Return(ctx.KVStore(keys["params"]))
+	mockCtx.On("KVStore", keys["application"]).Return(ctx.KVStore(keys["application"]))
+	mockCtx.On("BlockHeight").Return(ctx.BlockHeight())
+	mockCtx.On("MustGetPrevCtx", int64(1000)).Return(ctx)
+	mockCtx.On("MustGetPrevCtx", keeper.GetLatestSessionBlockHeight(mockCtx)).Return(ctx)
+
+	resp, err := keeper.HandleRelay(mockCtx, validRelay)
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, resp.Response, "bar")
+}
--- x/pocketcore/keeper/session_test.go
+++ x/pocketcore/keeper/session_test.go
@@ -6,62 +6,73 @@ import (
 	"testing"
 )
 
-//func TestKeeper_Dispatch(t *testing.T) {
-//	ctx, _, _, _, keeper := createTestInput(t, false)
-//	appPrivateKey := getRandomPrivateKey()
-//	appPubKey := appPrivateKey.PublicKey().RawString()
-//	ethereum, err := types.NonNativeChain{
-//		Ticker:  "eth",
-//		Netid:   "4",
-//		Version: "v1.9.9",
-//		Client:  "geth",
-//		Inter:   "",
-//	}.HashString()
-//	bitcoin, err := types.NonNativeChain{
-//		Ticker:  "btc",
-//		Netid:   "1",
-//		Version: "0.19.0.1",
-//		Client:  "",
-//		Inter:   "",
-//	}.HashString()
-//	if err != nil {
-//		t.Fatalf(err.Error())
-//	}
-//	// create a session header
-//	validHeader := types.SessionHeader{
-//		ApplicationPubKey:  appPubKey,
-//		Chain:              ethereum,
-//		SessionBlockHeight: 976,
-//	}
-//	// create an invalid session header
-//	invalidHeader := types.SessionHeader{
-//		ApplicationPubKey:  appPubKey,
-//		Chain:              bitcoin,
-//		SessionBlockHeight: 976,
-//	}
-//	res, err := keeper.Dispatch(ctx, validHeader)
-//	assert.Nil(t, err)
-//	assert.Equal(t, res.Chain, ethereum)
-//	assert.Equal(t, res.SessionBlockHeight, int64(976))
-//	assert.Equal(t, res.ApplicationPubKey, appPubKey)
-//	assert.Equal(t, res.SessionHeader, validHeader)
-//	assert.Len(t, res.SessionNodes, 5)
-//	_, err = keeper.Dispatch(ctx, invalidHeader)
-//	assert.NotNil(t, err)
-//}
+func TestKeeper_Dispatch(t *testing.T) {
+	ctx, _, _, _, keeper, keys := createTestInput(t, false)
+	appPrivateKey := getRandomPrivateKey()
+	appPubKey := appPrivateKey.PublicKey().RawString()
+	ethereum, err := types.NonNativeChain{
+		Ticker:  "eth",
+		Netid:   "4",
+		Version: "v1.9.9",
+		Client:  "geth",
+		Inter:   "",
+	}.HashString()
+	bitcoin, err := types.NonNativeChain{
+		Ticker:  "btc",
+		Netid:   "1",
+		Version: "0.19.0.1",
+		Client:  "",
+		Inter:   "",
+	}.HashString()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// create a session header
+	validHeader := types.SessionHeader{
+		ApplicationPubKey:  appPubKey,
+		Chain:              ethereum,
+		SessionBlockHeight: 976,
+	}
+	// create an invalid session header
+	invalidHeader := types.SessionHeader{
+		ApplicationPubKey:  appPubKey,
+		Chain:              bitcoin,
+		SessionBlockHeight: 976,
+	}
+	mockCtx := new(Ctx)
+	mockCtx.On("KVStore", keeper.storeKey).Return(ctx.KVStore(keeper.storeKey))
+	mockCtx.On("KVStore", keys["pos"]).Return(ctx.KVStore(keys["pos"]))
+	mockCtx.On("KVStore", keys["params"]).Return(ctx.KVStore(keys["params"]))
+	mockCtx.On("MustGetPrevCtx", validHeader.SessionBlockHeight).Return(ctx)
+	mockCtx.On("BlockHeight").Return(ctx.BlockHeight())
+	res, err := keeper.Dispatch(mockCtx, validHeader)
+	assert.Nil(t, err)
+	assert.Equal(t, res.SessionHeader.Chain, ethereum)
+	assert.Equal(t, res.SessionHeader.SessionBlockHeight, int64(976))
+	assert.Equal(t, res.SessionHeader.ApplicationPubKey, appPubKey)
+	assert.Equal(t, res.SessionHeader, validHeader)
+	assert.Len(t, res.SessionNodes, 5)
+	_, err = keeper.Dispatch(mockCtx, invalidHeader)
+	assert.NotNil(t, err)
+}
 
 func TestKeeper_IsSessionBlock(t *testing.T) {
-	notSessionContext, _, _, _, keeper := createTestInput(t, false)
+	notSessionContext, _, _, _, keeper, _ := createTestInput(t, false)
 	assert.False(t, keeper.IsSessionBlock(notSessionContext))
 }
 
-//func TestKeeper_GetLatestSessionBlock(t *testing.T) {
-//	notSessionContext, _, _, _, keeper := createTestInput(t, false)
-//	assert.Equal(t, keeper.GetLatestSessionBlock(notSessionContext).BlockHeight(), int64(976))
-//}
+func TestKeeper_GetLatestSessionBlock(t *testing.T) {
+	ctx, _, _, _, keeper, keys := createTestInput(t, false)
+
+	mockCtx := new(Ctx)
+	mockCtx.On("KVStore", keys["params"]).Return(ctx.KVStore(keys["params"]))
+	mockCtx.On("BlockHeight").Return(ctx.BlockHeight())
+	mockCtx.On("MustGetPrevCtx", keeper.GetLatestSessionBlockHeight(mockCtx)).Return(ctx)
+	assert.Equal(t, ctx.BlockHeight(), keeper.GetLatestSessionBlock(mockCtx).BlockHeight())
+}
 
 func TestKeeper_IsPocketSupportedBlockchain(t *testing.T) {
-	ctx, _, _, _, keeper := createTestInput(t, false)
+	ctx, _, _, _, keeper, _ := createTestInput(t, false)
 	sb := []string{"ethereum"}
 	notSB := "bitcoin"
 	p := types.Params{
