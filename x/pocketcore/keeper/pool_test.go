4c7da3db641a7d201e6ac677c063d6e0e8782999
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-03-01 23:29:54 -0400

    update pool tests on pocketcore module

--- x/pocketcore/keeper/pool_test.go
+++ x/pocketcore/keeper/pool_test.go
@@ -7,27 +7,27 @@ import (
 )
 
 func TestKeeper_StakeDenom(t *testing.T) {
-	ctx, _, _, _, k := createTestInput(t, false)
+	ctx, _, _, _, k, _ := createTestInput(t, false)
 	stakeDenom := types.DefaultStakeDenom
 	assert.Equal(t, stakeDenom, k.posKeeper.StakeDenom(ctx))
 }
 
 func TestKeeper_GetNodesStakedTokens(t *testing.T) {
-	ctx, vals, _, _, k := createTestInput(t, false)
+	ctx, vals, _, _, k, _ := createTestInput(t, false)
 	assert.NotZero(t, len(vals))
 	tokens := vals[0].StakedTokens
 	assert.Equal(t, k.GetNodesStakedTokens(ctx), tokens.Mul(types.NewInt(int64(len(vals)))))
 }
 
 func TestKeeper_GetAppsStakedTokens(t *testing.T) {
-	ctx, _, apps, _, k := createTestInput(t, false)
+	ctx, _, apps, _, k, _ := createTestInput(t, false)
 	assert.NotZero(t, len(apps))
 	tokens := apps[0].StakedTokens
 	assert.Equal(t, k.GetNodesStakedTokens(ctx), tokens.Mul(types.NewInt(int64(len(apps)))))
 }
 
 func TestKeeper_GetTotalStakedTokens(t *testing.T) {
-	ctx, vals, apps, _, k := createTestInput(t, false)
+	ctx, vals, apps, _, k, _:= createTestInput(t, false)
 	assert.NotZero(t, len(apps))
 	appToken := apps[0].StakedTokens
 	appTokens := appToken.Mul(types.NewInt(int64(len(apps))))
