5949973cdfef65f918f5cac1a36146f1119d4e90
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-03-10 15:48:00 -0400

    add version CLI command

--- app/cmd/cli/root.go
+++ app/cmd/cli/root.go
@@ -2,13 +2,14 @@ package cli
 
 import (
 	"fmt"
-	"github.com/pokt-network/pocket-core/app"
-	"github.com/pokt-network/pocket-core/app/cmd/rpc"
-	"github.com/spf13/cobra"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"github.com/pokt-network/pocket-core/app"
+	"github.com/pokt-network/pocket-core/app/cmd/rpc"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -23,6 +24,8 @@ var (
 )
 
 const defaultSeeds = `610CF8A6E8CEFBADED845F1C1DC3B10A670BE26B@node1.testnet.pokt.network:26656, E6946760D9833F49DA39AAE9500537BEF6F33A7A@node2.testnet.pokt.network:26656, 7674A47CC977326F1DF6CB92C7B5A2AD36557EA2@node3.testnet.pokt.network:26656, C7B7B7665D20A7172D0C0AA58237E425F333560A@node4.testnet.pokt.network:26656, F6DC0B244C93232283CD1D8443363946D0A3D77A@node5.testnet.pokt.network:26656, 86209713BEFECA0807714BCDD5B79E81073FAF8F@node6.testnet.pokt.network:26656, 915A58AE437D2C2D6F35AC11B79F42972267700D@node7.testnet.pokt.network:26656, B3D86CD8AB4AA0CB9861CB795D8D154E685A94CF@node8.testnet.pokt.network:26656, 17CA63E4FF7535A40512C550DD0267E519CAFC1A@node9.testnet.pokt.network:26656, F99386C6D7CD42A486C63CCD80F5FBEA68759CD7@node10.testnet.pokt.network:26656`
+
+var CLIVersion = fmt.Sprintf("%s%s", app.Tag, app.Version)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
