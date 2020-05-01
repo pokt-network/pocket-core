c630e8a640c4a3d426530b9aaec236243962204c
andrewnguyen22 <amnguyen1@mail.usf.edu>
2020-04-30 21:01:22 -0400

    patched test service url

--- app/cmd/rpc/common_test.go
+++ app/cmd/rpc/common_test.go
@@ -104,7 +104,7 @@ var (
 const (
 	dummyChainsHash = "00"
 	dummyChainsURL  = "https://foo.bar:8080"
-	dummyServiceURL = "0.0.0.0:8081"
+	dummyServiceURL = "https://foo.bar:8081"
 	defaultTMURI    = "tcp://localhost:26657"
 )
 
