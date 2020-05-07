63a5672cf1c2d607f200e519c19ceaacec032846
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-05-07 15:21:39 -0400

    use sdk.Error instead of error for app module coinsFromUnstakeToStaked

--- x/apps/keeper/pool.go
+++ x/apps/keeper/pool.go
@@ -1,14 +1,13 @@
 package keeper
 
 import (
-	"errors"
 	"github.com/pokt-network/pocket-core/x/apps/types"
 	sdk "github.com/pokt-network/posmint/types"
 	"github.com/pokt-network/posmint/x/auth"
 	"github.com/pokt-network/posmint/x/auth/exported"
 )
 
-// StakedRatio the fraction of the staking tokens which are currently staked
+// StakedRatio - Retrieve the fraction of the staking tokens which are currently staked
 func (k Keeper) StakedRatio(ctx sdk.Ctx) sdk.Dec {
 	stakedPool := k.GetStakedPool(ctx)
 
@@ -19,23 +18,23 @@ func (k Keeper) StakedRatio(ctx sdk.Ctx) sdk.Dec {
 	return sdk.ZeroDec()
 }
 
-// GetStakedTokens total staking tokens supply which is staked
+// GetStakedTokens - Retrieve total staking tokens supply which is staked
 func (k Keeper) GetStakedTokens(ctx sdk.Ctx) sdk.Int {
 	stakedPool := k.GetStakedPool(ctx)
 	return stakedPool.GetCoins().AmountOf(k.StakeDenom(ctx))
 }
 
-// TotalTokens staking tokens from the total supply
+// TotalTokens - Retrieve total staking tokens from the total supply
 func (k Keeper) TotalTokens(ctx sdk.Ctx) sdk.Int {
 	return k.AccountsKeeper.GetSupply(ctx).GetTotal().AmountOf(k.StakeDenom(ctx))
 }
 
-// GetStakedPool returns the staked tokens pool's module account
+// GetStakedPool - Retrieve the staked tokens pool's module account
 func (k Keeper) GetStakedPool(ctx sdk.Ctx) (stakedPool exported.ModuleAccountI) {
 	return k.AccountsKeeper.GetModuleAccount(ctx, types.StakedPoolName)
 }
 
-// moves coins from the module account to the application -> used in unstaking
+// coinsFromStakedToUnstkaed - Transfer coins from the module account to the application -> used in unstaking
 func (k Keeper) coinsFromStakedToUnstaked(ctx sdk.Ctx, application types.Application) {
 	coins := sdk.NewCoins(sdk.NewCoin(k.StakeDenom(ctx), application.StakedTokens))
 	err := k.AccountsKeeper.SendCoinsFromModuleToAccount(ctx, types.StakedPoolName, application.Address, coins)
@@ -44,10 +43,10 @@ func (k Keeper) coinsFromStakedToUnstaked(ctx sdk.Ctx, application types.Applica
 	}
 }
 
-// moves coins from the module account to application -> used in staking
-func (k Keeper) coinsFromUnstakedToStaked(ctx sdk.Ctx, application types.Application, amount sdk.Int) error {
+// coinsFromUnstakedToStaked - Transfer coins from the module account to application -> used in staking
+func (k Keeper) coinsFromUnstakedToStaked(ctx sdk.Ctx, application types.Application, amount sdk.Int) sdk.Error {
 	if amount.LT(sdk.ZeroInt()) {
-		return errors.New("cannot stake a negative amount of coins")
+		return sdk.ErrInternal("cannot stake a negative amount of coins")
 	}
 	coins := sdk.NewCoins(sdk.NewCoin(k.StakeDenom(ctx), amount))
 	err := k.AccountsKeeper.SendCoinsFromAccountToModule(ctx, sdk.Address(application.Address), types.StakedPoolName, coins)
@@ -57,7 +56,7 @@ func (k Keeper) coinsFromUnstakedToStaked(ctx sdk.Ctx, application types.Applica
 	return nil
 }
 
-// burnStakedTokens removes coins from the staked pool module account
+// burnStakedTokens - Remove coins from the staked pool module account
 func (k Keeper) burnStakedTokens(ctx sdk.Ctx, amt sdk.Int) sdk.Error {
 	if !amt.IsPositive() {
 		return nil
@@ -66,6 +65,7 @@ func (k Keeper) burnStakedTokens(ctx sdk.Ctx, amt sdk.Int) sdk.Error {
 	return k.AccountsKeeper.BurnCoins(ctx, types.StakedPoolName, coins)
 }
 
+// getFeePool - Retrieve fee pool
 func (k Keeper) getFeePool(ctx sdk.Ctx) (feePool exported.ModuleAccountI) {
 	return k.AccountsKeeper.GetModuleAccount(ctx, auth.FeeCollectorName)
 }
