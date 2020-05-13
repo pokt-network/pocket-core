2ac03aeb894005ec88f4e2cc9565fccb55af0c44
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-05-13 17:46:59 -0400

    move lint to build step

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 go get -u github.com/goware/modvendor
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.26.0
 go mod vendor
 go mod download
 modvendor -copy="**/*.c **/*.h" -v
