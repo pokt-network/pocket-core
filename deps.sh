d3238edbed1b31f28b274564354b1480746026b5
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-05-13 17:34:32 -0400

    add community linter & vet to pipeline

--- deps.sh
+++ deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 go get -u github.com/goware/modvendor
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0
 go mod vendor
 go mod download
 modvendor -copy="**/*.c **/*.h" -v
