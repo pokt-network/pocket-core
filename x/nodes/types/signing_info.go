20c2acd3d50dfe64c33ebdff07d7450930227f28
Otto V <ottoevargas@gmail.com>
2020-05-18 12:01:23 -0400

    issue #891

--- x/nodes/types/signing_info.go
+++ x/nodes/types/signing_info.go
@@ -15,6 +15,7 @@ type ValidatorSigningInfo struct {
 	JailedUntil         time.Time   `json:"jailed_until" yaml:"jailed_until"`                   // timestamp validator cannot be unjailed until
 	Tombstoned          bool        `json:"tombstoned" yaml:"tombstoned"`                       // whether or not a validator has been tombstoned (killed out of validator set)
 	MissedBlocksCounter int64       `json:"missed_blocks_counter" yaml:"missed_blocks_counter"` // missed blocks counter (to avoid scanning the array every time)
+	JailedBlocksCounter int64       `json:"jailed_blocks_counter" yaml:"jailed_blocks_counter"` // jailed blocks counter (to avoid scanning the array every time)
 }
 
 // Return human readable signing info
@@ -25,7 +26,8 @@ func (i ValidatorSigningInfo) String() string {
   Entropy Offset:        %d
   Jailed Until:          %v
   Tombstoned:            %t
-  Missed Blocks Counter: %d`,
+  Missed Blocks Counter: %d
+  Jailed Blocks Counter: %d`,
 		i.Address, i.StartHeight, i.IndexOffset, i.JailedUntil,
-		i.Tombstoned, i.MissedBlocksCounter)
+		i.Tombstoned, i.MissedBlocksCounter, i.JailedBlocksCounter)
 }
