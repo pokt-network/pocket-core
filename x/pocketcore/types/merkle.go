c382c547cc362f44b868c734eb070e0a57937463
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-08-04 20:16:55 -0400

    benchmark sortAndStructure

--- x/pocketcore/types/merkle.go
+++ x/pocketcore/types/merkle.go
@@ -3,11 +3,13 @@ package types
 import (
 	"bytes"
 	"encoding/binary"
-	"golang.org/x/crypto/blake2b"
 	"math"
 	"math/rand"
 	"reflect"
+	"sort"
 	"strconv"
+
+	"golang.org/x/crypto/blake2b"
 )
 
 type Range struct {
@@ -18,6 +20,19 @@ type Range struct {
 func (r Range) Bytes() []byte {
 	return append(uint64ToBytes(r.Lower), uint64ToBytes(r.Upper)...)
 }
+
+type proofAndRanges struct {
+	hr []HashRange
+	p  []Proof
+}
+type SortByProof proofAndRanges
+
+func (a SortByProof) Len() int { return len(a.hr) }
+func (a SortByProof) Swap(i, j int) {
+	a.hr[i], a.hr[j] = a.hr[j], a.hr[i]
+	a.p[i], a.p[j] = a.p[j], a.p[i]
+}
+func (a SortByProof) Less(i, j int) bool { return a.hr[i].Range.Upper < a.hr[j].Range.Upper }
 
 // "uint64ToBytes" - convert the uint64 to bytes
 func uint64ToBytes(a uint64) (bz []byte) {
@@ -227,6 +242,51 @@ func sortAndStructure(proofs []Proof) (d []HashRange, sortedProofs []Proof) {
 	hashRanges := make([]HashRange, numberOfProofs)
 	// sort the slice based on the numerical value of the upper value (just the decimal representation of the merkleHash)
 	hashRanges, proofs = quickSortProofs(hashRanges, proofs)
+	// keep track of previous upper (next values lower)
+	lower := uint64(0)
+	// set the lower values of each
+	for i := range proofs {
+		// the range is the previous
+		hashRanges[i].Range.Lower = lower
+		// update the lower
+		lower = hashRanges[i].Range.Upper
+	}
+	// calculate the proper length of the merkle tree
+	properLength := nextPowerOfTwo(uint(numberOfProofs))
+	// generate padding to make it a proper merkle tree
+	padding := make([]HashRange, int(properLength)-numberOfProofs)
+	// add it to the merkleHash rangeds
+	hashRanges = append(hashRanges, padding...)
+	// add padding to the end of the hashRange
+	for i := numberOfProofs; i < int(properLength); i++ {
+		hashRanges[i] = HashRange{
+			Hash:  merkleHash([]byte(strconv.Itoa(i))),
+			Range: Range{Lower: lower, Upper: lower + 1},
+		}
+		lower = hashRanges[i].Range.Upper
+	}
+	return hashRanges, proofs
+}
+
+func sortAndStructureC(proofs []Proof) (d []HashRange, sortedProofs []Proof) {
+	// get the # of proofs
+	numberOfProofs := len(proofs)
+	// initialize the hashRange
+	hashRanges := make([]HashRange, numberOfProofs)
+
+	// sort the slice based on the numerical value of the upper value (just the decimal representation of the merkleHash)
+	if hashRanges[0].Range.Upper == 0 {
+		for i := range hashRanges {
+			// save the merkleHash and sum of the Proof in the new tree slice
+			hashRanges[i].Hash = merkleHash(proofs[i].Bytes())
+			// get the inital sum (just the dec val of the merkleHash)
+			hashRanges[i].Range.Upper = sumFromHash(hashRanges[i].Hash)
+
+		}
+	}
+	sortedRangesAndProofs := proofAndRanges{hashRanges, proofs}
+	sort.Sort(SortByProof(sortedRangesAndProofs))
+	hashRanges, proofs = sortedRangesAndProofs.hr, sortedRangesAndProofs.p
 	// keep track of previous upper (next values lower)
 	lower := uint64(0)
 	// set the lower values of each
