0f532d7067d16727b303a9615b5027a3d394417f
Germany B <jeremybeal11@gmail.com>
2020-09-28 13:34:43 -0400

    Update util.go
    small typo for the cmd usage for unsafe rollback

--- app/cmd/cli/util.go
+++ app/cmd/cli/util.go
@@ -113,7 +113,7 @@ var (
 )
 
 var unsafeRollbackCmd = &cobra.Command{
-	Use:   "unsafe-rollback <height",
+	Use:   "unsafe-rollback <height>",
 	Short: "Rollbacks the blockchain, the state, and app to a previous height",
 	Long:  "Rollbacks the blockchain, the state, and app to a previous height",
 	Args:  cobra.ExactArgs(1),
