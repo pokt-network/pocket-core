cd43926f24e11cbb6786d1edf4b61efbef8be061
Otto V <ottoevargas@gmail.com>
2020-12-04 17:31:25 -0400

    Adding Preliminary Stop cmd to gracefully stop all pocket daemons running. Adding GODEBUG madvdontneed=1 at the start if not set already.

--- app/cmd/pocket_core/main.go
+++ app/cmd/pocket_core/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"github.com/pokt-network/pocket-core/app/cmd/cli"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	cli.Execute()
+	godebug := os.Getenv("GODEBUG")
+	//Check for the madvdontneed variable
+	if strings.Contains(godebug, "madvdontneed=1") {
+		cli.Execute()
+	} else {
+		env := append(os.Environ(), "GODEBUG="+"madvdontneed=1,"+godebug)
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = env
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		_ = cmd.Run()
+	}
 }
