41f1bcad11931272031df75501e3e25e5ac81911
andrewnguyen22 <amnguyen1@mail.usf.edu>
2021-01-13 15:41:27 -0500

    reverted upgrade height

--- codec/codec.go
+++ codec/codec.go
@@ -22,7 +22,7 @@ func NewCodec(anyUnpacker types.AnyUnpacker) *Codec {
 }
 
 var (
-	UpgradeHeight                    int64 = 20
+	UpgradeHeight                    int64 = 20000
 	NotProtoCompatibleInterfaceError       = errors.New("the interface passed for encoding does not implement proto marshaller")
 )
 
