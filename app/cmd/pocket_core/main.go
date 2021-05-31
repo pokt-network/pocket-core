82c854fa0eeccbc662432c5c49e3ebf58cce8e60
andrewnguyen22 <amnguyen1@mail.usf.edu>
2021-05-31 12:20:52 -0400

    unix timestamp \ only sleep on start

--- app/cmd/pocket_core/main.go
+++ app/cmd/pocket_core/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pokt-network/pocket-core/app/cmd/cli"
-	"time"
 )
 
 func main() {
-	loc, err := time.LoadLocation("America/New_York")       // use other time zones such as MST, IST
-	if err != nil {
-		panic(err)
-	}
-	t := time.Date(2021, time.May, 31, 18, 0, 0, 0, loc)
-	sleepDuration := time.Until(t)
-	fmt.Println("Sleeping for ", sleepDuration)
-	time.Sleep(sleepDuration)
 	cli.Execute()
 }
