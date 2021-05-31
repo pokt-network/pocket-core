ea30b287445015b400677e644c9b711680a7ee8c
andrewnguyen22 <amnguyen1@mail.usf.edu>
2021-05-31 11:01:21 -0400

    added sleep

--- app/cmd/pocket_core/main.go
+++ app/cmd/pocket_core/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pokt-network/pocket-core/app/cmd/cli"
+	"time"
 )
 
 func main() {
+	loc, _ := time.LoadLocation("EST")       // use other time zones such as MST, IST
+	t := time.Date(2021, time.May, 31, 18, 0, 0, 0, loc)
+	sleepDuration := time.Until(t)
+	fmt.Println("Sleeping for ", sleepDuration)
+	time.Sleep(sleepDuration)
 	cli.Execute()
 }
