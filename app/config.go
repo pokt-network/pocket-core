30ef1594d9b96f04e04d96200d80370e9bf236e0
iajrz <axel.rivas@gmail.com>
2021-10-11 17:41:01 -0400

    updates default config to use max inbound peers 14

--- app/config.go
+++ app/config.go
@@ -180,7 +180,7 @@ func UpdateConfig(datadir string) {
 	sdk.DefaultPocketConsensusConfig(GlobalConfig.TendermintConfig.Consensus)
 	GlobalConfig.TendermintConfig.P2P.AllowDuplicateIP = true
 	GlobalConfig.TendermintConfig.P2P.AddrBookStrict = false
-	GlobalConfig.TendermintConfig.P2P.MaxNumInboundPeers = 21
+	GlobalConfig.TendermintConfig.P2P.MaxNumInboundPeers = 14
 	GlobalConfig.TendermintConfig.P2P.MaxNumOutboundPeers = 7
 	GlobalConfig.TendermintConfig.RPC.GRPCMaxOpenConnections = 2500
 	GlobalConfig.TendermintConfig.RPC.MaxOpenConnections = 2500
