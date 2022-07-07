bf996b912ae59d45982c077a4f95e65902827f17
poktblade <poktblade@poktfund.com>
2022-07-07 02:24:14 -0400

    add more tests to leannode_tests

--- x/pocketcore/types/leanNode_test.go
+++ x/pocketcore/types/leanNode_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestLeanNodeAdd(t *testing.T) {
+func TestPocketNodeAdd(t *testing.T) {
 	key := GetRandomPrivateKey()
 	address := sdk.GetAddress(key.PublicKey())
 	AddPocketNode(key, log.NewNopLogger())
@@ -15,7 +15,7 @@ func TestLeanNodeAdd(t *testing.T) {
 	assert.True(t, ok)
 }
 
-func TestLeanNodeAddByAddress(t *testing.T) {
+func TestPocketNodeGetByAddress(t *testing.T) {
 	key := GetRandomPrivateKey()
 	address := sdk.GetAddress(key.PublicKey())
 	AddPocketNode(key, log.NewNopLogger())
@@ -24,9 +24,56 @@ func TestLeanNodeAddByAddress(t *testing.T) {
 	assert.NotNil(t, node)
 }
 
-func TestLeanNodeGet(t *testing.T) {
+func TestPocketNodeGet(t *testing.T) {
 	key := GetRandomPrivateKey()
 	AddPocketNode(key, log.NewNopLogger())
 	node := GetPocketNode()
 	assert.NotNil(t, node)
+}
+
+func TestPocketNodeCleanCache(t *testing.T) {
+	key := GetRandomPrivateKey()
+	AddPocketNode(key, log.NewNopLogger())
+	CleanPocketNodes()
+	assert.Nil(t, GlobalSessionCache)
+	assert.Nil(t, GlobalEvidenceCache)
+	assert.EqualValues(t, 0, len(GlobalPocketNodes))
+}
+
+func TestPocketNodeInitCache(t *testing.T) {
+	CleanPocketNodes()
+	key := GetRandomPrivateKey()
+	testingConfig := sdk.DefaultTestingPocketConfig()
+	AddPocketNode(key, log.NewNopLogger())
+	InitPocketNodeCaches(testingConfig, log.NewNopLogger())
+	address := sdk.GetAddress(key.PublicKey())
+	node, err := GetPocketNodeByAddress(&address)
+	assert.NotNil(t, GlobalSessionCache)
+	assert.NotNil(t, GlobalEvidenceCache)
+	assert.EqualValues(t, 1, len(GlobalPocketNodes))
+	assert.Nil(t, err)
+	assert.NotNil(t, node.EvidenceStore)
+	assert.NotNil(t, node.SessionStore)
+}
+
+func TestPocketNodeInitCaches(t *testing.T) {
+	CleanPocketNodes()
+	key := GetRandomPrivateKey()
+	key2 := GetRandomPrivateKey()
+	logger := log.NewNopLogger()
+	testingConfig := sdk.DefaultTestingPocketConfig()
+	testingConfig.PocketConfig.LeanPocket = true
+	AddPocketNode(key, logger)
+	AddPocketNode(key2, logger)
+	InitPocketNodeCaches(testingConfig, logger)
+	assert.NotNil(t, GlobalSessionCache)
+	assert.NotNil(t, GlobalEvidenceCache)
+	assert.EqualValues(t, 2, len(GlobalPocketNodes))
+	addresses := []sdk.Address{sdk.GetAddress(key.PublicKey()), sdk.GetAddress(key2.PublicKey())}
+	for _, address := range addresses {
+		node, err := GetPocketNodeByAddress(&address)
+		assert.Nil(t, err)
+		assert.NotNil(t, node.EvidenceStore)
+		assert.NotNil(t, node.SessionStore)
+	}
 }
