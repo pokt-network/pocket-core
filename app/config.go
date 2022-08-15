4d21389a1500237e4a29ebe1e9acdaed1e317aee
poktblade <baaspoolsllc@gmail.com>
2022-08-15 13:41:23 -0500

    Add lean pocket to update config cli

--- app/config.go
+++ app/config.go
@@ -256,6 +256,7 @@ func UpdateConfig(datadir string) {
 	GlobalConfig.PocketConfig.CtxCacheSize = sdk.DefaultCtxCacheSize
 	GlobalConfig.PocketConfig.RPCTimeout = sdk.DefaultRPCTimeout
 	GlobalConfig.PocketConfig.IavlCacheSize = sdk.DefaultIavlCacheSize
+	GlobalConfig.PocketConfig.LeanPocket = sdk.DefaultLeanPocket
 
 	// Backup and Save the File
 	var jsonFile *os.File
