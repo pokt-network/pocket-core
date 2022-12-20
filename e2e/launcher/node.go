20f3bfee3a5efe23f83e0daeb154c90da5a04066
iajrz <axel.rivas@gmail.com>
2022-12-20 12:06:09 -0400

    making return values unnamed
    I am pretty uncomfortable about the semantics of this code; I end up declaring a ton of `err` variables for no reason other than they are the second parameter, and the first one needs to be created.
    
    The tendency towards being terse is fighting the common code structuring patterns and the semantics in a royal rumble where the winner gets to still not be satisfactory, so I'll just keep this in a recognizable style and  call it quits until a guiding principle can show up and help us never face such mixed solutions of sometimes creating and sometimes reusing a variable.

--- e2e/launcher/node.go
+++ e2e/launcher/node.go
@@ -29,26 +29,26 @@ type Node struct {
 	privateKey    crypto.PrivateKey
 }
 
-func newNode(nodeConfiguration *NodeConfiguration, networkRootDirectory, genesisPath, executableLocation string) (node *Node, err error) {
+func newNode(nodeConfiguration *NodeConfiguration, networkRootDirectory, genesisPath, executableLocation string) (*Node, error) {
 	pkBytes, err := hex.DecodeString(nodeConfiguration.PrivateKey)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	privateKey, err := crypto.NewPrivateKeyBz(pkBytes)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	address := strings.ToLower(privateKey.PublicKey().Address().String())
 	nodeDataDir, err := os.MkdirTemp(networkRootDirectory, address+"-")
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	pocketCoreConfig, err := loadNodePocketCoreConfiguration(nodeConfiguration.ConfigPath)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	pocketCoreConfig.TendermintConfig.RootDir = nodeDataDir
@@ -60,41 +60,40 @@ func newNode(nodeConfiguration *NodeConfiguration, networkRootDirectory, genesis
 
 	// write config.json & genesis.json
 	if err = os.Mkdir(filepath.Join(nodeDataDir, "config"), os.ModePerm); err != nil {
-		return
+		return nil, err
 	}
 
-	if configBz, marshalErr := json.Marshal(pocketCoreConfig); err != nil {
-		return nil, marshalErr
+	if configBz, err := json.Marshal(pocketCoreConfig); err != nil {
+		return nil, err
 	} else if err = writeBytesToFile(filepath.Join(nodeDataDir, "config", "config.json"), configBz); err != nil {
 		return nil, err
 	}
 
 	if err = copyFile(genesisPath, filepath.Join(nodeDataDir, "config", "genesis.json")); err != nil {
-		return
+		return nil, err
 	}
 
 	// set identity files
 	if err = writeNodePrivValFile(privateKey, nodeDataDir, pocketCoreConfig.TendermintConfig.PrivValidatorKey); err != nil {
-		return
+		return nil, err
 	}
 
 	if err = writeNodeKeyFile(privateKey, nodeDataDir, pocketCoreConfig.TendermintConfig.NodeKey); err != nil {
-		return
+		return nil, err
 	}
 
 	if err = writePrivValState(nodeDataDir, pocketCoreConfig.TendermintConfig.PrivValidatorState); err != nil {
-		return
+		return nil, err
 	}
 
-	// initialize returnable node
-	node = &Node{
+	node := &Node{
 		Address:       address,
 		DataDir:       nodeDataDir,
 		configuration: pocketCoreConfig,
 		privateKey:    privateKey,
 		PocketServer:  NewPocketServer(executableLocation),
 	}
-	return
+	return node, nil
 }
 
 func writePrivValState(datadir, privValState string) error {
