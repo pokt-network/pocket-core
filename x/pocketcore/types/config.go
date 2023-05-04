79c7504ac1ed43768058c4756547f3bbb6923337
Jorge S. Cuesta <jckpo2@gmail.com>
2023-05-04 10:15:36 -0400

    Added support to lean node for evidence worker. Now is one worker per address. Prevent the race condition on the evidence store and do not hold relays evidence due to other address is using the queue.

--- x/pocketcore/types/config.go
+++ x/pocketcore/types/config.go
@@ -6,8 +6,10 @@ import (
 	"github.com/alitto/pond"
 	"github.com/pokt-network/pocket-core/crypto"
 	"github.com/pokt-network/pocket-core/types"
+	"github.com/puzpuzpuz/xsync"
 	"github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
+	log2 "log"
 	"time"
 )
 
@@ -18,10 +20,10 @@ const (
 )
 
 var (
-	globalRPCTimeout       time.Duration
-	GlobalPocketConfig     types.PocketConfig
-	GlobalTenderMintConfig config.Config
-	GlobalEvidenceWorker   *pond.WorkerPool
+	globalRPCTimeout        time.Duration
+	GlobalPocketConfig      types.PocketConfig
+	GlobalTenderMintConfig  config.Config
+	GlobalEvidenceWorkerMap = xsync.NewMapOf[*pond.WorkerPool]()
 )
 
 func InitConfig(chains *HostedBlockchains, logger log.Logger, c types.Config) {
@@ -41,15 +43,59 @@ func InitConfig(chains *HostedBlockchains, logger log.Logger, c types.Config) {
 	SetRPCTimeout(c.PocketConfig.RPCTimeout)
 }
 
-func InitEvidenceWorker(_ types.Config, logger log.Logger) {
+// NewWorkerPool - create pond.WorkerPool instance with the right params in place.
+func NewWorkerPool(address string, c types.Config, logger log.Logger) *pond.WorkerPool {
 	panicHandler := func(p interface{}) {
-		logger.Error(fmt.Sprintf("evidence storage task panicked: %v", p))
+		logger.Error(fmt.Sprintf("evidence worker of %s panic a task with: %v", address, p))
 	}
-	GlobalEvidenceWorker = pond.New(
-		1, 0, pond.MinWorkers(1),
+
+	var strategy pond.ResizingStrategy
+
+	switch c.PocketConfig.EvidenceWorker.Strategy {
+	case "lazy":
+		strategy = pond.Lazy()
+		break
+	case "eager":
+		strategy = pond.Eager()
+		break
+	case "balanced":
+		strategy = pond.Balanced()
+		break
+	default:
+		log2.Fatal(
+			fmt.Sprintf(
+				"evidence_worker.strategy %s is not a valid option; allowed values are: lazy|eager|balanced",
+				c.PocketConfig.EvidenceWorker.Strategy,
+			),
+		)
+	}
+
+	return pond.New(
+		// avoid race condition writing evidence in parallel
+		1, c.PocketConfig.EvidenceWorker.MaxCapacity,
+		pond.IdleTimeout(time.Duration(c.PocketConfig.EvidenceWorker.IdleTimeout)*time.Millisecond),
 		pond.PanicHandler(panicHandler),
-		pond.Strategy(pond.Balanced()),
+		pond.Strategy(strategy),
 	)
+}
+
+func InitEvidenceWorker(c types.Config, logger log.Logger) {
+	for address, node := range GlobalPocketNodes {
+		if node == nil {
+			continue
+		}
+
+		logger.Debug(
+			fmt.Sprintf(
+				"starting worker for: Address=%s Strategy=%s MaxCapacity=%d IdleTimeout=%d",
+				address, c.PocketConfig.EvidenceWorker.Strategy,
+				c.PocketConfig.EvidenceWorker.MaxCapacity,
+				c.PocketConfig.EvidenceWorker.IdleTimeout,
+			),
+		)
+
+		GlobalEvidenceWorkerMap.Store(address, NewWorkerPool(address, c, logger))
+	}
 }
 
 func ConvertEvidenceToProto(config types.Config) error {
@@ -83,10 +129,12 @@ func ConvertEvidenceToProto(config types.Config) error {
 }
 
 func StopEvidenceWorker() {
-	if !GlobalEvidenceWorker.Stopped() {
-		GlobalEvidenceWorker.StopAndWait()
-	}
-	GlobalEvidenceWorker = nil
+	GlobalEvidenceWorkerMap.Range(func(address string, worker *pond.WorkerPool) bool {
+		if !worker.Stopped() {
+			worker.StopAndWait()
+		}
+		return true
+	})
 }
 
 func FlushSessionCache() {
