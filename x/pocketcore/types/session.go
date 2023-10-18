6bbf7b37487cd0da77bfe92405c39e59484cf2e7
harry <53987565+h5law@users.noreply.github.com>
2023-10-18 23:31:24 +0100

    chore: negate max chains check

--- x/pocketcore/types/session.go
+++ x/pocketcore/types/session.go
@@ -139,7 +139,7 @@ func NewSessionNodes(sessionCtx, ctx sdk.Ctx, keeper PosKeeper, chain string, se
 			node.IsJailed() ||
 			!NodeHasChain(chain, node) ||
 			sessionNodes.Contains(node.GetAddress()) ||
-			keeper.ValidatorHasLessOrEqualMaxChains(ctx, node) {
+			!keeper.ValidatorHasLessOrEqualMaxChains(ctx, node) {
 			continue
 		}
 		// else add the node to the session
