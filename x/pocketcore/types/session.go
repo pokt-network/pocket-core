pokt-network/pocket-core

commit 02a4e3adaa6d7cb63746cbb4b5dab0f7e9da0460

feat: enforce max chains during new session (#1582)

x/pocketcore/types/session.go
@@ -4,10 +4,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"log"
+
 	sdk "github.com/pokt-network/pocket-core/types"
 	appexported "github.com/pokt-network/pocket-core/x/apps/exported"
 	"github.com/pokt-network/pocket-core/x/nodes/exported"
-	"log"
 )
 
 // "Session" - The relationship between an application and the pocket network
@@ -17,11 +18,17 @@ func (s Session) IsSealable() bool {
 }
 
 func (s Session) HashString() string {
-	return s.HashString()
+	return s.SessionHeader.HashString()
 }
 
 // "NewSession" - create a new session from seed data
-func NewSession(sessionCtx, ctx sdk.Ctx, keeper PosKeeper, sessionHeader SessionHeader, blockHash string, sessionNodesCount int) (Session, sdk.Error) {
+func NewSession(
+	sessionCtx, ctx sdk.Ctx,
+	keeper PosKeeper,
+	sessionHeader SessionHeader,
+	blockHash string,
+	sessionNodesCount int,
+) (Session, sdk.Error) {
 	// first generate session key
 	sessionKey, err := NewSessionKey(sessionHeader.ApplicationPubKey, sessionHeader.Chain, blockHash)
 	if err != nil {
@@ -101,7 +108,17 @@ func (s Session) Key() ([]byte, error) {
 type SessionNodes []sdk.Address
 
 // "NewSessionNodes" - Generates nodes for the session
-func NewSessionNodes(sessionCtx, ctx sdk.Ctx, keeper PosKeeper, chain string, sessionKey SessionKey, sessionNodesCount int) (sessionNodes SessionNodes, err sdk.Error) {
+func NewSessionNodes(
+	sessionCtx, ctx sdk.Ctx,
+	keeper PosKeeper,
+	chain string,
+	sessionKey SessionKey,
+	sessionNodesCount int,
+) (sessionNodes SessionNodes, err sdk.Error) {
+	// retrieve the enforce max chains flag's value from the codec
+	isEnforceMaxChains := ModuleCdc.IsAfterEnforceMaxChainsUpgrade(ctx.BlockHeight())
+	// retrieve the max chains value from the sessionCtx
+	nodeMaxChains := keeper.MaxChains(sessionCtx)
 	// all nodesAddrs at session genesis
 	nodesAddrs, totalNodes := keeper.GetValidatorsByChain(sessionCtx, chain)
 	// validate nodesAddrs
@@ -110,11 +127,11 @@ func NewSessionNodes(sessionCtx, ctx sdk.Ctx, keeper PosKeeper, chain string, se
 	}
 	sessionNodes = make(SessionNodes, sessionNodesCount)
 	var node exported.ValidatorI
-	//unique address map to avoid re-checking a pseudorandomly selected servicer
+	// unique address map to avoid re-checking a pseudorandomly selected servicer
 	m := make(map[string]struct{})
 	// only select the nodesAddrs if not jailed
 	for i, numOfNodes := 0, 0; ; i++ {
-		//if this is true we already checked all nodes we got on getValidatorsBychain
+		// if this is true we already checked all nodes we got on getValidatorsBychain
 		if len(m) >= totalNodes {
 			return nil, NewInsufficientNodesError(ModuleName)
 		}
@@ -124,17 +141,22 @@ func NewSessionNodes(sessionCtx, ctx sdk.Ctx, keeper PosKeeper, chain string, se
 		sessionKey = Hash(sessionKey)
 		// get the node from the array
 		n := nodesAddrs[index.Int64()]
-		//if we already have seen this address we continue as it's either on the list or discarded
+		// if we already have seen this address we continue as it's either on the list or discarded
 		if _, ok := m[n.String()]; ok {
 			continue
 		}
-		//add the node address to the map
+		// add the node address to the map
 		m[n.String()] = struct{}{}
 
 		// cross check the node from the `new` or `end` world state
 		node = keeper.Validator(ctx, n)
-		// if not found or jailed, don't add to session and continue
-		if node == nil || node.IsJailed() || !NodeHasChain(chain, node) || sessionNodes.Contains(node.GetAddress()) {
+		lenNodeChains := int64(len(node.GetChains()))
+		// if not found or jailed or is overstaked to chains
+		if node == nil ||
+			(isEnforceMaxChains && lenNodeChains > nodeMaxChains) ||
+			node.IsJailed() ||
+			!NodeHasChain(chain, node) ||
+			sessionNodes.Contains(node.GetAddress()) {
 			continue
 		}
 		// else add the node to the session
@@ -267,8 +289,8 @@ func BlockHash(ctx sdk.Context) string {
 
 // "MaxPossibleRelays" - Returns the maximum possible amount of relays for an App on a sessions
 func MaxPossibleRelays(app appexported.ApplicationI, sessionNodeCount int64) sdk.BigInt {
-	//GetMaxRelays Max value is bound to math.MaxUint64,
-	//current worse case is 1 chain and 5 nodes per session with a result of 3689348814741910323 which can be used safely as int64
+	// GetMaxRelays Max value is bound to math.MaxUint64,
+	// current worse case is 1 chain and 5 nodes per session with a result of 3689348814741910323 which can be used safely as int64
 	return app.GetMaxRelays().ToDec().Quo(sdk.NewDec(int64(len(app.GetChains())))).Quo(sdk.NewDec(sessionNodeCount)).RoundInt()
 }
 

.circleci/config.yml
@@ -46,8 +46,8 @@ jobs:
           key: pocket-core-build-{{ .Environment.CIRCLE_SHA1 }}
       # Run tests
       - run:
-          name: 'test'
-          command: go test -short -v -p 1 ./...
+          name: "test"
+          command: go test -short -v -p 1 ./... -timeout=15m
           no_output_timeout: 20m
   # Job to trigger the Pocket Core deployments CI with a specific branch
   trigger-pocket-core-deployments-branches:

codec/codec.go
@@ -49,6 +49,7 @@ const (
 	UpgradeCodecUpdateKey        = "CODEC"
 	ValidatorSplitUpdateKey      = "SPLIT"
 	NonCustodialUpdateKey        = "NCUST"
+	EnforceMaxChainsUpdateKey    = "MAXCH"
 	TxCacheEnhancementKey        = "REDUP"
 	MaxRelayProtKey              = "MREL"
 	ReplayBurnKey                = "REPBR"
@@ -291,6 +292,13 @@ func (cdc *Codec) IsOnNonCustodialUpgrade(height int64) bool {
 	return (UpgradeFeatureMap[NonCustodialUpdateKey] != 0 && height == UpgradeFeatureMap[NonCustodialUpdateKey]) || TestMode <= -3
 }
 
+// IsAfterEnforceMaxChainsUpgrade Note: includes the actual upgrade height
+func (cdc *Codec) IsAfterEnforceMaxChainsUpgrade(height int64) bool {
+	return (UpgradeFeatureMap[EnforceMaxChainsUpdateKey] != 0 &&
+		height >= UpgradeFeatureMap[EnforceMaxChainsUpdateKey]) ||
+		TestMode <= -3
+}
+
 // IsAfterNamedFeatureActivationHeight Note: includes the actual upgrade height
 func (cdc *Codec) IsAfterNamedFeatureActivationHeight(height int64, key string) bool {
 	return UpgradeFeatureMap[key] != 0 && height >= UpgradeFeatureMap[key]
@@ -323,7 +331,7 @@ func (cdc *Codec) IsOnNamedFeatureActivationHeightWithTolerance(
 
 // SliceToExistingMap merge slice to existing map
 func SliceToExistingMap(arr []string, m map[string]int64) map[string]int64 {
-	var fmap = make(map[string]int64)
+	fmap := make(map[string]int64)
 	for k, v := range m {
 		fmap[k] = v
 	}
@@ -337,7 +345,7 @@ func SliceToExistingMap(arr []string, m map[string]int64) map[string]int64 {
 
 // SliceToMap converts slice to map
 func SliceToMap(arr []string) map[string]int64 {
-	var fmap = make(map[string]int64)
+	fmap := make(map[string]int64)
 	for _, v := range arr {
 		kv := strings.Split(v, ":")
 		i, _ := strconv.ParseInt(kv[1], 10, 64)
@@ -348,7 +356,7 @@ func SliceToMap(arr []string) map[string]int64 {
 
 // MapToSlice converts map to slice
 func MapToSlice(m map[string]int64) []string {
-	var fslice = make([]string, 0)
+	fslice := make([]string, 0)
 	for k, v := range m {
 		kv := fmt.Sprintf("%s:%d", k, v)
 		fslice = append(fslice, kv)

go.mod
@@ -45,7 +45,7 @@ require (
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.0.0 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
@@ -74,7 +74,7 @@ require (
 	github.com/willf/bitset v1.1.10 // indirect
 	go.etcd.io/bbolt v1.3.3 // indirect
 	golang.org/x/net v0.1.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.1.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20210917145530-b395a37504d4 // indirect

go.sum
@@ -188,8 +188,8 @@ github.com/google/go-cmp v0.5.0/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/
 github.com/google/go-cmp v0.5.1/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
 github.com/google/go-cmp v0.5.4/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
 github.com/google/go-cmp v0.5.5/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
-github.com/google/go-cmp v0.5.6 h1:BKbKCqvP6I+rmFHt06ZmyQtvB8xAkWdhFyr0ZUNZcxQ=
-github.com/google/go-cmp v0.5.6/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
+github.com/google/go-cmp v0.5.8 h1:e6P7q2lk1O+qJJb4BtCQXlK8vWEO8V1ZeuEdJNOqZyg=
+github.com/google/go-cmp v0.5.8/go.mod h1:17dUlkBOakJ0+DkrSSNjCkIjxS6bF9zb3elmeNGIjoY=
 github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf/go.mod h1:HP5RmnzzSNb993RKQDq4+1A4ia9nllfqcQFTQJedwGI=
 github.com/google/gofuzz v1.0.0 h1:A8PeW59pxE9IoFRqBp37U+mSNaQoZ46F1f0f863XSXw=
 github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
@@ -594,8 +594,8 @@ golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44/go.mod h1:h1NjWce9XRLGQEsW7w
 golang.org/x/sys v0.0.0-20210423082822-04245dca01da/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/sys v0.0.0-20210510120138-977fb7262007/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
-golang.org/x/sys v0.1.0 h1:kunALQeHf1/185U1i0GOB/fy1IPRDDpuoOOqRReG57U=
-golang.org/x/sys v0.1.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.14.0 h1:Vz7Qs629MkJkGyHxUlRHizWJRG2j8fbQKjELVSNhy7Q=
+golang.org/x/sys v0.14.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1/go.mod h1:bj7SfCRtBDWHUb9snDiAeCFNEtKQo2Wmx5Cou7ajbmo=
 golang.org/x/term v0.1.0 h1:g6Z6vPFA9dYBAF7DWcH6sCcOntplXsDKcliusYijMlw=
 golang.org/x/term v0.1.0/go.mod h1:jbD1KX2456YbFQfuXm/mYQcufACuNUgVhRMnK/tPxf8=
@@ -662,7 +662,6 @@ golang.org/x/tools v0.1.5/go.mod h1:o0xws9oXOQQZyjljx8fwUC0k7L1pTE6eaCbjGeHmOkk=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
-golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 h1:go1bK/D/BFZV2I8cIQd1NKEZ+0owSTG1fDTci4IqFcE=
 golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 google.golang.org/api v0.4.0/go.mod h1:8k5glujaEP+g9n7WNsDg8QP6cUVNI86fCNMcbazEtwE=
 google.golang.org/api v0.7.0/go.mod h1:WtwebWUNSVBH/HAw79HIFXZNqEvBhG+Ra+ax0hx3E3M=

x/pocketcore/keeper/claim.go
@@ -3,19 +3,26 @@ package keeper
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/pokt-network/pocket-core/codec"
 	"time"
 
+	"github.com/tendermint/tendermint/rpc/client"
+
+	"github.com/pokt-network/pocket-core/codec"
 	"github.com/pokt-network/pocket-core/crypto"
 	sdk "github.com/pokt-network/pocket-core/types"
 	"github.com/pokt-network/pocket-core/x/auth"
 	"github.com/pokt-network/pocket-core/x/auth/util"
 	pc "github.com/pokt-network/pocket-core/x/pocketcore/types"
-	"github.com/tendermint/tendermint/rpc/client"
 )
 
 // "SendClaimTx" - Automatically sends a claim of work/challenge based on relays or challenges stored.
-func (k Keeper) SendClaimTx(ctx sdk.Ctx, keeper Keeper, n client.Client, node *pc.PocketNode, claimTx func(pk crypto.PrivateKey, cliCtx util.CLIContext, txBuilder auth.TxBuilder, header pc.SessionHeader, totalProofs int64, root pc.HashRange, evidenceType pc.EvidenceType) (*sdk.TxResponse, error)) {
+func (k Keeper) SendClaimTx(
+	ctx sdk.Ctx,
+	keeper Keeper,
+	n client.Client,
+	node *pc.PocketNode,
+	claimTx func(pk crypto.PrivateKey, cliCtx util.CLIContext, txBuilder auth.TxBuilder, header pc.SessionHeader, totalProofs int64, root pc.HashRange, evidenceType pc.EvidenceType) (*sdk.TxResponse, error),
+) {
 	// get the private val key (main) account from the keybase
 	address := node.GetAddress()
 	// retrieve the iterator to go through each piece of evidence in storage
@@ -51,7 +58,8 @@ func (k Keeper) SendClaimTx(ctx sdk.Ctx, keeper Keeper, n client.Client, node *p
 		}
 		// if the blockchain in the evidence is not supported then delete it because nodes don't get paid/challenged for unsupported blockchains
 		if !k.IsPocketSupportedBlockchain(sessionCtx.WithBlockHeight(evidence.SessionHeader.SessionBlockHeight), evidence.SessionHeader.Chain) {
-			ctx.Logger().Info(fmt.Sprintf("claim for %s blockchain isn't pocket supported, so will not send. Deleting evidence\n", evidence.SessionHeader.Chain))
+			ctx.Logger().
+				Info(fmt.Sprintf("claim for %s blockchain isn't pocket supported, so will not send. Deleting evidence\n", evidence.SessionHeader.Chain))
 			if err := pc.DeleteEvidence(evidence.SessionHeader, evidenceType, node.EvidenceStore); err != nil {
 				ctx.Logger().Debug(err.Error())
 			}
@@ -70,10 +78,15 @@ func (k Keeper) SendClaimTx(ctx sdk.Ctx, keeper Keeper, n client.Client, node *p
 		}
 		app, found := k.GetAppFromPublicKey(sessionCtx, evidence.ApplicationPubKey)
 		if !found {
-			ctx.Logger().Error(fmt.Sprintf("an error occurred creating the claim transaction with app %s not found with evidence %v", evidence.ApplicationPubKey, evidence))
+			ctx.Logger().
+				Error(fmt.Sprintf("an error occurred creating the claim transaction with app %s not found with evidence %v", evidence.ApplicationPubKey, evidence))
 		}
 		// generate the merkle root for this evidence
-		root := evidence.GenerateMerkleRoot(evidence.SessionHeader.SessionBlockHeight, pc.MaxPossibleRelays(app, k.SessionNodeCount(sessionCtx)).Int64(), node.EvidenceStore)
+		root := evidence.GenerateMerkleRoot(
+			evidence.SessionHeader.SessionBlockHeight,
+			pc.MaxPossibleRelays(app, k.SessionNodeCount(sessionCtx)).Int64(),
+			node.EvidenceStore,
+		)
 		claimTxTotalTime := float64(time.Since(now).Milliseconds())
 		go func() {
 			pc.GlobalServiceMetric().AddClaimTiming(evidence.SessionHeader.Chain, claimTxTotalTime, &address)
@@ -131,6 +144,12 @@ func (k Keeper) ValidateClaim(ctx sdk.Ctx, claim pc.MsgClaim) (err sdk.Error) {
 			return pc.NewOverServiceError(pc.ModuleName)
 		}
 	}
+	// Ensure that the app is not staked to more than the permitted number of chains
+	lenAppChains := int64(len(app.GetChains()))
+	if pc.ModuleCdc.IsAfterEnforceMaxChainsUpgrade(ctx.BlockHeight()) &&
+		lenAppChains > k.appKeeper.MaxChains(sessionContext) {
+		return pc.NewChainsOverLimitError(pc.ModuleName, lenAppChains, k.appKeeper.MaxChains(sessionContext))
+	}
 	// get the session node count for the time of the session
 	sessionNodeCount := int(k.SessionNodeCount(sessionContext))
 	// check cache
@@ -148,7 +167,8 @@ func (k Keeper) ValidateClaim(ctx sdk.Ctx, claim pc.MsgClaim) (err sdk.Error) {
 		// create a new session to validate
 		session, err = pc.NewSession(sessionContext, sessionEndCtx, k.posKeeper, claim.SessionHeader, hex.EncodeToString(hash), sessionNodeCount)
 		if err != nil {
-			ctx.Logger().Error(fmt.Errorf("could not generate session with public key: %s, for chain: %s", app.GetPublicKey().RawString(), claim.SessionHeader.Chain).Error())
+			ctx.Logger().
+				Error(fmt.Errorf("could not generate session with public key: %s, for chain: %s", app.GetPublicKey().RawString(), claim.SessionHeader.Chain).Error())
 			return err
 		}
 	}
@@ -316,7 +336,7 @@ func (k Keeper) ClaimIsMature(ctx sdk.Ctx, sessionBlockHeight int64) bool {
 
 // "DeleteExpiredClaims" - Deletes the expired (claim expiration > # of session passed since claim genesis) claims
 func (k Keeper) DeleteExpiredClaims(ctx sdk.Ctx) {
-	var msg = pc.MsgClaim{}
+	msg := pc.MsgClaim{}
 	store := ctx.KVStore(k.storeKey)
 	iterator, _ := sdk.KVStorePrefixIterator(store, pc.ClaimKey)
 	defer iterator.Close()

x/pocketcore/types/errors.go
@@ -94,6 +94,7 @@ const (
 	CodeInvalidExpirationHeightErr       = 88
 	CodeInvalidMerkleRangeError          = 89
 	CodeEvidenceSealed                   = 90
+	CodeChainsOverLimitError             = 91
 )
 
 var (
@@ -186,6 +187,7 @@ var (
 	InvalidExpirationHeightErr       = errors.New("the expiration height included in the claim message is invalid (should not be set)")
 	InvalidMerkleRangeError          = errors.New("the merkle hash range is invalid")
 	SealedEvidenceError              = errors.New("the evidence is sealed, either max relays reached or claim already submitted")
+	ChainsOverLimitError             = errors.New("the number of staked chains is over the limit")
 )
 
 func NewSealedEvidenceError(codespace sdk.CodespaceType) sdk.Error {
@@ -195,6 +197,7 @@ func NewSealedEvidenceError(codespace sdk.CodespaceType) sdk.Error {
 func NewUnsupportedBlockchainError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeUnsupportedBlockchainError, UnsupportedBlockchainError.Error())
 }
+
 func NewNodeNotInSessionError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeNodeNotInSessionError, NodeNotInSessionError.Error())
 }
@@ -250,9 +253,11 @@ func NewInvalidRootError(codespace sdk.CodespaceType) sdk.Error {
 func NewInvalidHashLengthError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeInvalidHashLengthError, InvalidHashLengthError.Error())
 }
+
 func NewInvalidNetIDLengthError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeInvalidNetworkIDError, InvalidNetworkIDLengthError.Error())
 }
+
 func NewInvalidAppPubKeyError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeInvalidAppPubKeyError, InvalidAppPubKeyError.Error())
 }
@@ -460,6 +465,7 @@ func NewEmptyProofsError(codespace sdk.CodespaceType) sdk.Error {
 func NewEmptyBlockIDError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeEmptyBlockIDError, EmptyBlockIDError.Error())
 }
+
 func NewEmptyChainError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeEmptyChainError, EmptyNonNativeChainError.Error())
 }
@@ -474,4 +480,8 @@ func NewInvalidTokenError(codespace sdk.CodespaceType, err error) sdk.Error {
 
 func NewInvalidPKError(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeInvalidPkFileErr, InvalidPkFileErr.Error())
+}
+
+func NewChainsOverLimitError(codespace sdk.CodespaceType, gotChains, maxChains int64) sdk.Error {
+	return sdk.NewError(codespace, CodeChainsOverLimitError, fmt.Sprintf("%s: got %d, max %d", ChainsOverLimitError.Error(), gotChains, maxChains))
 }

x/pocketcore/types/expectedKeepers.go
@@ -26,6 +26,7 @@ type PosKeeper interface {
 	BlocksPerSession(ctx sdk.Ctx) (res int64)
 	StakeDenom(ctx sdk.Ctx) (res string)
 	GetValidatorsByChain(ctx sdk.Ctx, networkID string) (validators []sdk.Address, total int)
+	MaxChains(ctx sdk.Ctx) (maxChains int64)
 }
 
 type AppsKeeper interface {
@@ -34,6 +35,7 @@ type AppsKeeper interface {
 	AllApplications(ctx sdk.Ctx) (applications []appexported.ApplicationI)
 	TotalTokens(ctx sdk.Ctx) sdk.BigInt
 	JailApplication(ctx sdk.Ctx, addr sdk.Address)
+	MaxChains(ctx sdk.Ctx) (maxChains int64)
 }
 
 type PocketKeeper interface {

commit 99799845fd610c108f2904acc4ddabd9d9eced83

Do not call `node.GetChains()` if node is nil In `NewSessionNodes`, the variable `node` can be `nil` if the node is unstaked between `sessionCtx` and `ctx`.  In such a case, `node.GetChains()` causes a panic.  This patch makes sure we don't call it if the `node` is `nil`. This is a regression from #1582.

x/pocketcore/types/session.go
@@ -150,10 +150,9 @@ func NewSessionNodes(
 
 		// cross check the node from the `new` or `end` world state
 		node = keeper.Validator(ctx, n)
-		lenNodeChains := int64(len(node.GetChains()))
 		// if not found or jailed or is overstaked to chains
 		if node == nil ||
-			(isEnforceMaxChains && lenNodeChains > nodeMaxChains) ||
+			(isEnforceMaxChains && int64(len(node.GetChains())) > nodeMaxChains) ||
 			node.IsJailed() ||
 			!NodeHasChain(chain, node) ||
 			sessionNodes.Contains(node.GetAddress()) {