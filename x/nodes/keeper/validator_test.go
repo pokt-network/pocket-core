pokt-network/pocket-core

commit 58217f26c5e2a09e55123e18004f2c59d6a07a2c

[v0.11 feature] Built-in Reward Share (#1581)
## Description

This patch implements the Built-in Reward Share feature which lets the
network itself to distribute the relay/block rewards into multiple
addresses. The change consists of the following parts.

- Add `RewardDelegators` in `Validator`
- Add `RewardDelegators` in `MsgStake`
- Add a new command `pocket nodes stakeNew`
- Distributes the fee of claim/proof transaction to a node's operator
address
- Add a new config `prevent_negative_reward_claim` not to claim a
potential loss evidence.

This change is consensus-breaking. The new behavior is put behind a new
feature key `RewardDelegator`. The new field is kept unavailable until
activation.

The new structure of `Validator` or `MsgStake` is backward/forward
compatible, meaning the new binary can still unmarshal data marshaled by
an older binary, and vice versa. In other words, the network before
`RewardDelegator` activation accepts an `MsgStake` transaction, ignoring
the `RewardDelegators` field. And the new `Validator` structure can
handle all historical states from genesis. Therefore this patch does not
introduce a structure like `10.0Validaor` as the NCUST patch did before.

<!-- reviewpad:summarize:start -->
### Summary generated by Reviewpad on 26 Dec 23 01:21 UTC
This pull request includes changes to multiple files. Here is a summary
of the diff:

1. The file `common_test.go` was modified to replace the import of
`math/rand` with `crypto/rand`. Additionally, the comment `// : deadcode
unused` was removed.

2. The file `x/nodes/keeper/abci_test.go` was modified to add and remove
import statements, as well as comment out unnecessary code related to
state conversion.

3. The file `x/nodes/types/validator.go` was modified to add an import,
add a new field to the `Validator` struct, add a new function to create
a validator from a message, modify several methods to include a new

proto/x/nodes/nodes.proto
@@ -3,7 +3,6 @@ package x.nodes;
 
 import "gogoproto/gogo.proto";
 import "google/protobuf/timestamp.proto";
-import "google/protobuf/duration.proto";
 
 option go_package = "github.com/pokt-network/pocket-core/x/nodes/types";
 
@@ -21,6 +20,7 @@ message ProtoValidator {
 	string StakedTokens = 7 [(gogoproto.customtype) = "github.com/pokt-network/pocket-core/types.BigInt", (gogoproto.jsontag) = "tokens", (gogoproto.nullable) = false];
 	google.protobuf.Timestamp UnstakingCompletionTime = 8 [(gogoproto.nullable) = false, (gogoproto.stdtime) = true, (gogoproto.jsontag) = "unstaking_time", (gogoproto.moretags) = "yaml:\"unstaking_time\""];
 	bytes OutputAddress = 9 [(gogoproto.casttype) = "github.com/pokt-network/pocket-core/types.Address", (gogoproto.jsontag) = "output_address,omitempty", (gogoproto.moretags) = "yaml:\"output_address\""];
+	map<string, uint32> RewardDelegators = 10 [(gogoproto.jsontag) = "reward_delegators,omitempty", (gogoproto.moretags) = "yaml:\"reward_delegators\""];
 }
 
 message LegacyProtoValidator {

x/nodes/keeper/validator_test.go
@@ -2,11 +2,12 @@ package keeper
 
 import (
 	"fmt"
+	"reflect"
+	"testing"
+
 	sdk "github.com/pokt-network/pocket-core/types"
 	"github.com/pokt-network/pocket-core/x/nodes/types"
 	"github.com/stretchr/testify/assert"
-	"reflect"
-	"testing"
 )
 
 func TestKeeper_GetValidators(t *testing.T) {
@@ -147,4 +148,71 @@ func Test_sortNoLongerStakedValidators(t *testing.T) {
 			}
 		})
 	}
+}
+
+// There are two versions of structs to represent a validator.
+// - LegacyValidator - the original version
+// - Validator - LegacyValidator + OutputAddress + Delegators (since 0.11)
+//
+// The following test verifies marshaling/unmarshaling has backward/forward
+// compatibility, meaning marshaled bytes can be unmarshaled as a newer version
+// or an older version.
+//
+// We cover the Proto marshaler only because Amino marshaler does not support
+// a map type used in handle type.Validator.
+// We used Amino before UpgradeCodecHeight and we no longer use it, so it's
+// ok not to cover Amino.
+func TestValidator_Proto_MarshalingCompatibility(t *testing.T) {
+	_, _, k := createTestInput(t, false)
+	Marshal := k.Cdc.ProtoCodec().MarshalBinaryLengthPrefixed
+	Unmarshal := k.Cdc.ProtoCodec().UnmarshalBinaryLengthPrefixed
+
+	var (
+		val_1, val_2   types.Validator
+		valL_1, valL_2 types.LegacyValidator
+		marshaled      []byte
+		err            error
+	)
+
+	val_1 = getStakedValidator()
+	val_1.OutputAddress = getRandomValidatorAddress()
+	val_1.RewardDelegators = map[string]uint32{}
+	val_1.RewardDelegators[getRandomValidatorAddress().String()] = 10
+	val_1.RewardDelegators[getRandomValidatorAddress().String()] = 20
+	valL_1 = val_1.ToLegacy()
+
+	// Validator --> []byte --> Validator
+	marshaled, err = Marshal(&val_1)
+	assert.Nil(t, err)
+	assert.NotNil(t, marshaled)
+	val_2.Reset()
+	err = Unmarshal(marshaled, &val_2)
+	assert.Nil(t, err)
+	assert.True(t, val_2.ToLegacy().Equals(val_1.ToLegacy()))
+	assert.True(t, val_2.OutputAddress.Equals(val_1.OutputAddress))
+	assert.NotNil(t, val_2.RewardDelegators)
+	assert.True(
+		t,
+		types.CompareStringMaps(val_2.RewardDelegators, val_1.RewardDelegators),
+	)
+
+	// Validator --> []byte --> LegacyValidator
+	marshaled, err = Marshal(&val_1)
+	assert.Nil(t, err)
+	assert.NotNil(t, marshaled)
+	valL_2.Reset()
+	err = Unmarshal(marshaled, &valL_2)
+	assert.Nil(t, err)
+	assert.True(t, valL_2.Equals(val_1.ToLegacy()))
+
+	// LegacyValidator --> []byte --> Validator
+	marshaled, err = Marshal(&valL_1)
+	assert.Nil(t, err)
+	assert.NotNil(t, marshaled)
+	val_2.Reset()
+	err = Unmarshal(marshaled, &val_2)
+	assert.Nil(t, err)
+	assert.True(t, val_2.ToLegacy().Equals(valL_1))
+	assert.Nil(t, val_2.OutputAddress)
+	assert.Nil(t, val_2.RewardDelegators)
 }

x/nodes/types/validator_test.go
@@ -2,8 +2,6 @@ package types
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/tendermint/go-amino"
 	"math/rand"
 	"reflect"
 	"testing"
@@ -11,6 +9,8 @@ import (
 
 	"github.com/pokt-network/pocket-core/crypto"
 	sdk "github.com/pokt-network/pocket-core/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/go-amino"
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
@@ -1156,6 +1156,7 @@ func TestValidators_String(t *testing.T) {
 	}{
 		{"String Test", v, fmt.Sprintf("Address:\t\t%s\nPublic Key:\t\t%s\nJailed:\t\t\t%v\nStatus:\t\t\t%s\nTokens:\t\t\t%s\n"+
 			"ServiceUrl:\t\t%s\nChains:\t\t\t%v\nUnstaking Completion Time:\t\t%v\nOutput Address:\t\t%s"+
+			"\nReward Delegators:\t\t"+
 			"\n----",
 			sdk.Address(pub.Address()), pub.RawString(), false, sdk.Staked, sdk.ZeroInt(), "https://www.google.com:443", []string{"0001"}, time.Unix(0, 0).UTC(), "",
 		)},

app/cmd/cli/node.go
@@ -13,6 +13,7 @@ func init() {
 	rootCmd.AddCommand(nodesCmd)
 	nodesCmd.AddCommand(nodeUnstakeCmd)
 	nodesCmd.AddCommand(nodeUnjailCmd)
+	nodesCmd.AddCommand(stakeNewCmd)
 }
 
 var nodesCmd = &cobra.Command{
@@ -111,6 +112,91 @@ Will prompt the user for the <fromAddr> account passphrase.`,
 		resp, err := QueryRPC(SendRawTxPath, j)
 		if err != nil {
 			fmt.Println(err)
+			return
+		}
+		fmt.Println(resp)
+	},
+}
+
+// stakeNewCmd is an upgraded version of `nodesCmd` that captures newer
+// on-chain functionality in a cleaner way
+var stakeNewCmd = &cobra.Command{
+	Use:   "stakeNew <OperatorPublicKey> <OutputAddress> <SignerAddress> <Stake> <ChainIDs> <ServiceURL> <RewardDelegators> <NetworkID> <Fee> [Memo]",
+	Short: "Stake a node in the network",
+	Long: `Stake a node in the network, promoting it to a servicer or a validator.
+
+The command takes the following parameters.
+
+  OperatorPublicKey Public key to use as the node's operator account
+  OutputAddress     Address to use as the node's output account
+  SignerAddress     Address to sign the transaction
+  Stake             Amount to stake in uPOKT
+  ChainIDs          Comma-separated chain IDs to host on the node
+  ServiceURL        Relay endpoint of the node.  Must include the port number.
+  RewardDelegators  Addresses to share rewards
+  NetworkID         Network ID to submit a transaction to e.g. mainnet or testnet
+  Fee               Transaction fee in uPOKT
+  Memo              Optional. Text to include in the transaction.  No functional effect.
+
+Example:
+$ pocket nodes stakeNew \
+    e237efc54a93ed61689959e9afa0d4bd49fa11c0b946c35e6bebaccb052ce3fc \
+    fe818527cd743866c1db6bdeb18731d04891df78 \
+    1164b9c95638fc201f35eca2af4c35fe0a81b6cf \
+    8000000000000 \
+    DEAD,BEEF \
+    https://x.com:443 \
+    '{"1000000000000000000000000000000000000000":1,"2000000000000000000000000000000000000000":2}' \
+    mainnet \
+    10000 \
+    "new stake with delegators!"
+`,
+	Args: cobra.MinimumNArgs(9),
+	Run: func(cmd *cobra.Command, args []string) {
+		app.InitConfig(datadir, tmNode, persistentPeers, seeds, remoteCLIURL)
+
+		operatorPubKey := args[0]
+		outputAddr := args[1]
+		signerAddr := args[2]
+		stakeAmount := args[3]
+		chains := args[4]
+		serviceUrl := args[5]
+		delegators := args[6]
+		networkId := args[7]
+		fee := args[8]
+		memo := ""
+		if len(args) >= 10 {
+			memo = args[9]
+		}
+
+		fmt.Println("Enter Passphrase:")
+		passphrase := app.Credentials(pwd)
+
+		rawStakeTx, err := BuildStakeTx(
+			operatorPubKey,
+			outputAddr,
+			stakeAmount,
+			chains,
+			serviceUrl,
+			delegators,
+			networkId,
+			fee,
+			memo,
+			signerAddr,
+			passphrase,
+		)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		txBytes, err := json.Marshal(rawStakeTx)
+		if err != nil {
+			fmt.Println("Fail to build a transaction:", err)
+			return
+		}
+		resp, err := QueryRPC(SendRawTxPath, txBytes)
+		if err != nil {
+			fmt.Println("Fail to submit a transaction:", err)
 			return
 		}
 		fmt.Println(resp)

commit e78e940755b7e68d5d0716da743af320686ea4a5

Sort RewardDelegators when marshaling a validator (#1591)
## Description

This is a fix for a regression introduced by #1581.

When a node stores a validator with the new `RewardDelegators` field,
its marshaled product is not deterministic because the order of map
iteration is not deterministic. As a result, the merkle tree of
application.db can differ between nodes, leading to consensus failure.

The proposed fix is to sort the keys of `RewardDelegators` by using the
stable marshaler.

<!-- reviewpad:summarize:start -->
### Summary generated by Reviewpad on 15 Jan 24 18:21 UTC
This pull request includes changes to the `nodes.proto` file and a file
diff.

The changes in the `nodes.proto` file include:
- Added an option `stable_marshaler` with a value of `true`. This option
is used to make the order of `RewardDelegators` deterministic.
- Updated the `Address` field to include additional options
`(gogoproto.casttype)`, `(gogoproto.moretags)`, and
`(gogoproto.jsontag)`.
- Updated the `PublicKey` field to include additional options
`(gogoproto.moretags)` and `(gogoproto.jsontag)`.
- Updated the `jailed` field to include an additional option
`(gogoproto.jsontag)`.

The file diff includes the following changes:
- Imported the "encoding/hex", "math/rand", "reflect", "strconv", and
"time" packages.
- Added a test case for marshalling RewardDelegators.
- Added the "Test_Marshal_RewardDelegators" function.
- Created variables and constants within the test function.
- Generated random numbers to be used as reward delegator addresses.
- Created and initialized a map, "delegatorMap", with random numbers as
keys and their corresponding modulo 10 values as values.
- Created a "Validator" struct with various fields and assigned values
to them.
- Marshaled the validator struct and ensured that the resulting hash is
the same for every iteration of the test.

Please let me know if there is anything specific you would like me to
focus on.

proto/x/nodes/nodes.proto
@@ -11,6 +11,9 @@ message ProtoValidator {
 	option (gogoproto.goproto_stringer) = true;
 	option (gogoproto.goproto_getters) = false;
 
+	// This option is to make the order of RewardDelegators deterministic
+	option (gogoproto.stable_marshaler) = true;
+
 	bytes Address = 1 [(gogoproto.casttype) = "github.com/pokt-network/pocket-core/types.Address", (gogoproto.moretags) = "yaml:\"address\"", (gogoproto.jsontag) = "address"];
 	bytes PublicKey = 2 [(gogoproto.moretags) = "yaml:\"public_key\"", (gogoproto.jsontag) = "public_key"];
 	bool jailed = 3 [(gogoproto.jsontag) = "jailed"];

x/nodes/types/nodes.pb.go
@@ -7,6 +7,7 @@ import (
 	bytes "bytes"
 	fmt "fmt"
 	proto "github.com/cosmos/gogoproto/proto"
+	github_com_cosmos_gogoproto_sortkeys "github.com/cosmos/gogoproto/sortkeys"
 	github_com_cosmos_gogoproto_types "github.com/cosmos/gogoproto/types"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/golang/protobuf/ptypes/timestamp"
@@ -52,16 +53,12 @@ func (m *ProtoValidator) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
 func (m *ProtoValidator) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_ProtoValidator.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
+	b = b[:cap(b)]
+	n, err := m.MarshalToSizedBuffer(b)
+	if err != nil {
+		return nil, err
 	}
+	return b[:n], nil
 }
 func (m *ProtoValidator) XXX_Merge(src proto.Message) {
 	xxx_messageInfo_ProtoValidator.Merge(m, src)
@@ -217,61 +214,61 @@ func init() {
 func init() { proto.RegisterFile("x/nodes/nodes.proto", fileDescriptor_63cb49073b61e33a) }
 
 var fileDescriptor_63cb49073b61e33a = []byte{
-	// 858 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x96, 0xbf, 0x6f, 0xdb, 0x46,
-	0x14, 0xc7, 0xc5, 0xc8, 0xb2, 0xac, 0x93, 0xe2, 0xa6, 0xb4, 0x8d, 0x12, 0x6e, 0xa1, 0x13, 0xd8,
-	0xa1, 0x1a, 0x6a, 0xaa, 0x4d, 0x96, 0xd6, 0x45, 0x81, 0x86, 0x6e, 0x81, 0xba, 0x09, 0xd0, 0xe0,
-	0x6c, 0x77, 0xc8, 0x42, 0x50, 0xe4, 0x89, 0xbe, 0xf0, 0xc7, 0x11, 0xbc, 0x63, 0x62, 0xfd, 0x07,
-	0xed, 0xe6, 0xa9, 0xc8, 0xe8, 0x3f, 0x27, 0x63, 0x3a, 0x14, 0x28, 0x3a, 0x5c, 0x0b, 0x1b, 0x28,
-	0x0a, 0x8e, 0x1a, 0x3b, 0x15, 0xbc, 0xa3, 0x22, 0xd1, 0x51, 0x8b, 0x20, 0xc8, 0x98, 0x45, 0xe4,
-	0x7d, 0xdf, 0xbb, 0xf7, 0x7d, 0xc7, 0xfb, 0x3c, 0x40, 0x60, 0xeb, 0x6c, 0x94, 0x50, 0x1f, 0x33,
-	0xf5, 0x6b, 0xa5, 0x19, 0xe5, 0x54, 0x6f, 0x9f, 0x59, 0x72, 0xb9, 0xbb, 0x1d, 0xd0, 0x80, 0x4a,
-	0x6d, 0x54, 0xbe, 0xa9, 0xf0, 0x2e, 0x0c, 0x28, 0x0d, 0x22, 0x3c, 0x92, 0xab, 0x71, 0x3e, 0x19,
-	0x71, 0x12, 0x63, 0xc6, 0xdd, 0x38, 0x55, 0x09, 0xe6, 0x2f, 0x6d, 0xb0, 0xf9, 0xa0, 0x7c, 0xfb,
-	0xc1, 0x8d, 0x88, 0xef, 0x72, 0x9a, 0xe9, 0x11, 0x68, 0xdf, 0xf5, 0xfd, 0x0c, 0x33, 0x66, 0x68,
-	0x03, 0x6d, 0xd8, 0xb3, 0x51, 0x21, 0x60, 0xdb, 0x55, 0xd2, 0x4c, 0xc0, 0xcd, 0xa9, 0x1b, 0x47,
-	0xfb, 0x66, 0x25, 0x98, 0xff, 0x08, 0xf8, 0x69, 0x40, 0xf8, 0x69, 0x3e, 0xb6, 0x3c, 0x1a, 0x8f,
-	0x52, 0x1a, 0xf2, 0xbd, 0x04, 0xf3, 0x27, 0x34, 0x0b, 0x47, 0x29, 0xf5, 0x42, 0xcc, 0xf7, 0x3c,
-	0x9a, 0xe1, 0x11, 0x9f, 0xa6, 0x98, 0x59, 0x55, 0x65, 0x34, 0xb7, 0xd0, 0xef, 0x82, 0xce, 0x83,
-	0x7c, 0x1c, 0x11, 0xef, 0x1e, 0x9e, 0x1a, 0x37, 0xa4, 0xdf, 0x87, 0x85, 0x80, 0x20, 0x95, 0xa2,
-	0x13, 0xe2, 0xe9, 0x4c, 0xc0, 0x77, 0x95, 0xe5, 0x42, 0x33, 0xd1, 0x62, 0x97, 0x6e, 0x82, 0xf5,
-	0x47, 0x2e, 0x89, 0xb0, 0x6f, 0x34, 0x07, 0xda, 0x70, 0xc3, 0x06, 0x85, 0x80, 0x95, 0x82, 0xaa,
-	0x67, 0x99, 0xc3, 0xb8, 0xcb, 0x73, 0x66, 0xac, 0x0d, 0xb4, 0x61, 0x4b, 0xe5, 0x28, 0x05, 0x55,
-	0xcf, 0x32, 0xe7, 0xe0, 0xd4, 0x25, 0x09, 0x33, 0x5a, 0x83, 0xe6, 0xb0, 0xa3, 0x72, 0x3c, 0xa9,
-	0xa0, 0x2a, 0xa2, 0x8f, 0x00, 0x38, 0xc2, 0xd9, 0x63, 0xe2, 0xe1, 0x13, 0x74, 0xdf, 0x58, 0x1f,
-	0x68, 0xc3, 0x8e, 0xfd, 0x4e, 0x21, 0x60, 0x97, 0x29, 0xd5, 0xc9, 0xb3, 0x08, 0x2d, 0xa5, 0xe8,
-	0x13, 0xd0, 0x3b, 0xe2, 0x6e, 0x88, 0xfd, 0x63, 0x1a, 0xe2, 0x84, 0x19, 0x6d, 0xb9, 0xc5, 0x7e,
-	0x26, 0x60, 0xe3, 0x77, 0x01, 0x3f, 0x79, 0xf5, 0x2f, 0x67, 0x93, 0xe0, 0x30, 0xe1, 0x65, 0x4b,
-	0x5c, 0x56, 0x42, 0xb5, 0xba, 0xfa, 0x4f, 0x1a, 0x78, 0xef, 0x24, 0x61, 0xdc, 0x0d, 0x49, 0x12,
-	0x1c, 0xd0, 0x38, 0x8d, 0x30, 0x27, 0x34, 0x39, 0x26, 0x31, 0x36, 0x36, 0x06, 0xda, 0xb0, 0x7b,
-	0x7b, 0xd7, 0x52, 0x30, 0x58, 0x73, 0x18, 0xac, 0xe3, 0x39, 0x0c, 0xf6, 0x9d, 0xb2, 0x9f, 0x42,
-	0xc0, 0xcd, 0x7c, 0x5e, 0xc2, 0x29, 0x49, 0x99, 0x09, 0xb8, 0xa3, 0x3e, 0x7d, 0x5d, 0x37, 0xcf,
-	0xff, 0x80, 0x1a, 0xfa, 0x2f, 0x3f, 0xfd, 0x5c, 0x03, 0x37, 0xbf, 0xcf, 0x79, 0x9a, 0xf3, 0x39,
-	0x48, 0x1d, 0x79, 0xb1, 0x8f, 0x0a, 0x01, 0x0d, 0x2a, 0x03, 0x4e, 0x85, 0xcf, 0xc7, 0x34, 0x26,
-	0x1c, 0xc7, 0x29, 0x9f, 0x2e, 0xbc, 0xea, 0x19, 0xaf, 0x09, 0x58, 0xbd, 0x01, 0xfd, 0x67, 0x0d,
-	0xdc, 0x42, 0xf8, 0x89, 0x9b, 0xf9, 0x5f, 0xe3, 0x08, 0x07, 0x25, 0xe8, 0xcc, 0x00, 0x83, 0xe6,
-	0xb0, 0x7b, 0x7b, 0xcf, 0xaa, 0x66, 0xc8, 0xaa, 0x0f, 0x82, 0x75, 0x3d, 0xff, 0x9b, 0x84, 0x67,
-	0x53, 0xfb, 0x8b, 0x42, 0xc0, 0xf7, 0x33, 0x19, 0x72, 0xfc, 0x17, 0xb1, 0xda, 0x39, 0x0c, 0x75,
-	0x8e, 0x97, 0x92, 0x4c, 0xf4, 0x52, 0x0f, 0xbb, 0x07, 0x60, 0x67, 0xa5, 0x8f, 0x7e, 0x0b, 0x34,
-	0x43, 0x3c, 0x95, 0x23, 0xd8, 0x41, 0xe5, 0xab, 0xbe, 0x0d, 0x5a, 0x8f, 0xdd, 0x28, 0xc7, 0x72,
-	0x4c, 0x6e, 0x22, 0xb5, 0xd8, 0xbf, 0xf1, 0x99, 0xb6, 0xdf, 0xfb, 0xf1, 0x02, 0x36, 0x9e, 0x5e,
-	0x40, 0xed, 0xef, 0x0b, 0xa8, 0x99, 0x7f, 0xad, 0x81, 0xed, 0xfb, 0x38, 0x70, 0xbd, 0xe9, 0xdb,
-	0xc9, 0x7e, 0x3b, 0xd9, 0x6f, 0x72, 0xb2, 0xaf, 0x81, 0xf6, 0xeb, 0x1a, 0xd8, 0x7e, 0x41, 0xd7,
-	0x11, 0x09, 0x12, 0x92, 0x04, 0x87, 0xc9, 0x84, 0xea, 0x0f, 0xc1, 0x9c, 0xaa, 0x0a, 0xb4, 0xaf,
-	0x96, 0x40, 0x7b, 0x4d, 0xac, 0xaa, 0xdd, 0xfa, 0x77, 0xa0, 0xc7, 0xb8, 0x9b, 0x71, 0xe7, 0x14,
-	0x93, 0xe0, 0x94, 0x4b, 0xb2, 0x9a, 0xf6, 0x47, 0x85, 0x80, 0x35, 0x7d, 0x26, 0xe0, 0x96, 0x3a,
-	0xe0, 0xb2, 0x6a, 0xa2, 0xae, 0x5c, 0x7e, 0x2b, 0x57, 0xfa, 0x97, 0xa0, 0x75, 0x98, 0xf8, 0xf8,
-	0x4c, 0xe2, 0x55, 0x15, 0x21, 0xa5, 0xe0, 0xd0, 0xc9, 0x84, 0xe1, 0xa5, 0x22, 0xcb, 0xaa, 0x89,
-	0xd4, 0x2e, 0x3d, 0x01, 0x3d, 0x05, 0xa1, 0x93, 0x27, 0x9c, 0x44, 0x12, 0xc0, 0xff, 0xbf, 0x8d,
-	0x51, 0x75, 0x1b, 0xb5, 0x7d, 0x0b, 0x97, 0x65, 0x55, 0xdd, 0x44, 0x57, 0x49, 0x27, 0xa5, 0xa2,
-	0xc7, 0x60, 0x27, 0x26, 0x8c, 0x61, 0xdf, 0x19, 0x47, 0xd4, 0x0b, 0x99, 0xe3, 0xd1, 0x3c, 0xe1,
-	0x38, 0x33, 0x5a, 0xb2, 0xfd, 0xcf, 0x0b, 0x01, 0x57, 0x27, 0xcc, 0x04, 0xfc, 0x40, 0x39, 0xac,
-	0x0c, 0x9b, 0x68, 0x4b, 0xe9, 0xb6, 0x94, 0x0f, 0x94, 0x5a, 0xda, 0x55, 0x0d, 0x5d, 0xb3, 0x5b,
-	0x5f, 0xd8, 0xad, 0x4c, 0x58, 0xd8, 0xad, 0x0c, 0x9b, 0x68, 0x4b, 0xe9, 0x35, 0xbb, 0xfd, 0x8d,
-	0xa7, 0x17, 0xb0, 0x51, 0x72, 0x65, 0xdf, 0x7b, 0x76, 0xd9, 0xd7, 0x9e, 0x5f, 0xf6, 0xb5, 0x3f,
-	0x2f, 0xfb, 0xda, 0xf9, 0x55, 0xbf, 0xf1, 0xfc, 0xaa, 0xdf, 0xf8, 0xed, 0xaa, 0xdf, 0x78, 0xf8,
-	0x4a, 0xe0, 0xcc, 0xff, 0x27, 0x49, 0x80, 0xc6, 0xeb, 0xf2, 0x1a, 0xee, 0xfc, 0x1b, 0x00, 0x00,
-	0xff, 0xff, 0x49, 0xf4, 0x43, 0x35, 0x3f, 0x09, 0x00, 0x00,
+	// 864 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x96, 0xcf, 0x6f, 0xe3, 0x44,
+	0x14, 0xc7, 0xe3, 0x4d, 0xd3, 0x34, 0x93, 0x6c, 0x29, 0x6e, 0x2b, 0xac, 0x82, 0x32, 0x91, 0x39,
+	0x90, 0x03, 0xb5, 0x61, 0xf7, 0x02, 0x41, 0x48, 0xac, 0x0b, 0x12, 0x65, 0x57, 0x62, 0x35, 0x6d,
+	0x39, 0xec, 0xc5, 0x72, 0xec, 0x89, 0x3b, 0xeb, 0x1f, 0x63, 0xd9, 0xe3, 0xdd, 0xe6, 0x3f, 0x80,
+	0x5b, 0x4f, 0xa8, 0xc7, 0xf2, 0xdf, 0xec, 0x71, 0x2f, 0x2b, 0x21, 0x0e, 0x03, 0x6a, 0x25, 0x84,
+	0x7c, 0xcc, 0x91, 0x13, 0xf2, 0x8c, 0xb3, 0x89, 0x77, 0x03, 0x5a, 0xad, 0x38, 0xf6, 0x12, 0x7b,
+	0xbe, 0xef, 0xcd, 0xfb, 0xbe, 0xf1, 0x7c, 0x9e, 0x14, 0xb0, 0x7d, 0x66, 0xc6, 0xd4, 0xc3, 0x99,
+	0xfc, 0x35, 0x92, 0x94, 0x32, 0xaa, 0xb6, 0xcf, 0x0c, 0xb1, 0xdc, 0xdb, 0xf1, 0xa9, 0x4f, 0x85,
+	0x66, 0x96, 0x6f, 0x32, 0xbc, 0x07, 0x7d, 0x4a, 0xfd, 0x10, 0x9b, 0x62, 0x35, 0xce, 0x27, 0x26,
+	0x23, 0x11, 0xce, 0x98, 0x13, 0x25, 0x32, 0x41, 0x7f, 0xd1, 0x06, 0x9b, 0x0f, 0xcb, 0xb7, 0x1f,
+	0x9c, 0x90, 0x78, 0x0e, 0xa3, 0xa9, 0x1a, 0x82, 0xf6, 0x3d, 0xcf, 0x4b, 0x71, 0x96, 0x69, 0xca,
+	0x40, 0x19, 0xf6, 0x2c, 0x54, 0x70, 0xd8, 0x76, 0xa4, 0x34, 0xe3, 0x70, 0x73, 0xea, 0x44, 0xe1,
+	0x48, 0xaf, 0x04, 0xfd, 0x6f, 0x0e, 0x3f, 0xf5, 0x09, 0x3b, 0xcd, 0xc7, 0x86, 0x4b, 0x23, 0x33,
+	0xa1, 0x01, 0xdb, 0x8f, 0x31, 0x7b, 0x4a, 0xd3, 0xc0, 0x4c, 0xa8, 0x1b, 0x60, 0xb6, 0xef, 0xd2,
+	0x14, 0x9b, 0x6c, 0x9a, 0xe0, 0xcc, 0xa8, 0x2a, 0xa3, 0xb9, 0x85, 0x7a, 0x0f, 0x74, 0x1e, 0xe6,
+	0xe3, 0x90, 0xb8, 0xf7, 0xf1, 0x54, 0xbb, 0x25, 0xfc, 0x3e, 0x2c, 0x38, 0x04, 0x89, 0x10, 0xed,
+	0x00, 0x4f, 0x67, 0x1c, 0xbe, 0x2b, 0x2d, 0x17, 0x9a, 0x8e, 0x16, 0xbb, 0x54, 0x1d, 0xac, 0x3f,
+	0x76, 0x48, 0x88, 0x3d, 0xad, 0x39, 0x50, 0x86, 0x1b, 0x16, 0x28, 0x38, 0xac, 0x14, 0x54, 0x3d,
+	0xcb, 0x9c, 0x8c, 0x39, 0x2c, 0xcf, 0xb4, 0xb5, 0x81, 0x32, 0x6c, 0xc9, 0x1c, 0xa9, 0xa0, 0xea,
+	0x59, 0xe6, 0x1c, 0x9c, 0x3a, 0x24, 0xce, 0xb4, 0xd6, 0xa0, 0x39, 0xec, 0xc8, 0x1c, 0x57, 0x28,
+	0xa8, 0x8a, 0xa8, 0x26, 0x00, 0x47, 0x38, 0x7d, 0x42, 0x5c, 0x7c, 0x82, 0x1e, 0x68, 0xeb, 0x03,
+	0x65, 0xd8, 0xb1, 0xde, 0x29, 0x38, 0xec, 0x66, 0x52, 0xb5, 0xf3, 0x34, 0x44, 0x4b, 0x29, 0xea,
+	0x04, 0xf4, 0x8e, 0x98, 0x13, 0x60, 0xef, 0x98, 0x06, 0x38, 0xce, 0xb4, 0xb6, 0xd8, 0x62, 0x3d,
+	0xe3, 0xb0, 0xf1, 0x1b, 0x87, 0x9f, 0xbc, 0xf9, 0x97, 0xb3, 0x88, 0x7f, 0x18, 0xb3, 0xb2, 0x25,
+	0x26, 0x2a, 0xa1, 0x5a, 0x5d, 0xf5, 0x27, 0x05, 0xbc, 0x77, 0x12, 0x67, 0xcc, 0x09, 0x48, 0xec,
+	0x1f, 0xd0, 0x28, 0x09, 0x31, 0x23, 0x34, 0x3e, 0x26, 0x11, 0xd6, 0x36, 0x06, 0xca, 0xb0, 0x7b,
+	0x67, 0xcf, 0x90, 0x30, 0x18, 0x73, 0x18, 0x8c, 0xe3, 0x39, 0x0c, 0xd6, 0xdd, 0xb2, 0x9f, 0x82,
+	0xc3, 0xcd, 0x7c, 0x5e, 0xc2, 0x2e, 0x49, 0x99, 0x71, 0xb8, 0x2b, 0x3f, 0x7d, 0x5d, 0xd7, 0xcf,
+	0x7f, 0x87, 0x0a, 0xfa, 0x37, 0x3f, 0xf5, 0x5c, 0x01, 0xb7, 0xbf, 0xcf, 0x59, 0x92, 0xb3, 0x39,
+	0x48, 0x1d, 0x71, 0xb1, 0x8f, 0x0b, 0x0e, 0x35, 0x2a, 0x02, 0x76, 0x85, 0xcf, 0xc7, 0x34, 0x22,
+	0x0c, 0x47, 0x09, 0x9b, 0x2e, 0xbc, 0xea, 0x19, 0x6f, 0x09, 0x58, 0xbd, 0x01, 0xf5, 0x67, 0x05,
+	0x6c, 0x21, 0xfc, 0xd4, 0x49, 0xbd, 0xaf, 0x71, 0x88, 0xfd, 0x12, 0xf4, 0x4c, 0x03, 0x83, 0xe6,
+	0xb0, 0x7b, 0x67, 0xdf, 0xa8, 0x66, 0xc8, 0xa8, 0x0f, 0x82, 0xf1, 0x6a, 0xfe, 0x37, 0x31, 0x4b,
+	0xa7, 0xd6, 0x17, 0x05, 0x87, 0xef, 0xa7, 0x22, 0x64, 0x7b, 0x2f, 0x63, 0xb5, 0x73, 0x68, 0xf2,
+	0x1c, 0xaf, 0x25, 0xe9, 0xe8, 0xb5, 0x1e, 0xf6, 0x0e, 0xc0, 0xee, 0x4a, 0x1f, 0x75, 0x0b, 0x34,
+	0x03, 0x3c, 0x15, 0x23, 0xd8, 0x41, 0xe5, 0xab, 0xba, 0x03, 0x5a, 0x4f, 0x9c, 0x30, 0xc7, 0x62,
+	0x4c, 0x6e, 0x23, 0xb9, 0x18, 0xdd, 0xfa, 0x4c, 0x19, 0x6d, 0xfd, 0x78, 0x09, 0x1b, 0x17, 0x97,
+	0x50, 0xf9, 0xeb, 0x12, 0x2a, 0x17, 0xbf, 0x40, 0x45, 0xff, 0x73, 0x0d, 0xec, 0x3c, 0xc0, 0xbe,
+	0xe3, 0x4e, 0x6f, 0xa6, 0xfb, 0x66, 0xba, 0xff, 0xcf, 0xe9, 0x1e, 0xf5, 0x96, 0x61, 0xd3, 0x5f,
+	0xac, 0x81, 0x9d, 0x97, 0x74, 0x1d, 0x11, 0x3f, 0x26, 0xb1, 0x7f, 0x18, 0x4f, 0xa8, 0xfa, 0x08,
+	0xcc, 0xa9, 0xaa, 0x40, 0xfb, 0x6a, 0x09, 0xb4, 0xb7, 0xc4, 0xaa, 0xda, 0xad, 0x7e, 0x07, 0x7a,
+	0x19, 0x73, 0x52, 0x66, 0x9f, 0x62, 0xe2, 0x9f, 0x32, 0x41, 0x56, 0xd3, 0xfa, 0xa8, 0xe0, 0xb0,
+	0xa6, 0xcf, 0x38, 0xdc, 0x96, 0x07, 0x5c, 0x56, 0x75, 0xd4, 0x15, 0xcb, 0x6f, 0xc5, 0x4a, 0xfd,
+	0x12, 0xb4, 0x0e, 0x63, 0x0f, 0x9f, 0x09, 0xbc, 0xaa, 0x22, 0xa4, 0x14, 0x6c, 0x3a, 0x99, 0x64,
+	0x78, 0xa9, 0xc8, 0xb2, 0xaa, 0x23, 0xb9, 0x4b, 0x8d, 0x41, 0x4f, 0x42, 0x68, 0xe7, 0x31, 0x23,
+	0xa1, 0x00, 0xf0, 0xbf, 0x6f, 0xc3, 0xac, 0x6e, 0xa3, 0xb6, 0x6f, 0xe1, 0xb2, 0xac, 0xca, 0x9b,
+	0xe8, 0x4a, 0xe9, 0xa4, 0x54, 0xd4, 0x08, 0xec, 0x46, 0x24, 0xcb, 0xb0, 0x67, 0x8f, 0x43, 0xea,
+	0x06, 0x99, 0xed, 0xd2, 0x3c, 0x66, 0x38, 0xd5, 0x5a, 0xa2, 0xfd, 0xcf, 0x0b, 0x0e, 0x57, 0x27,
+	0xcc, 0x38, 0xfc, 0x40, 0x3a, 0xac, 0x0c, 0xeb, 0x68, 0x5b, 0xea, 0x96, 0x90, 0x0f, 0xa4, 0x5a,
+	0xda, 0x55, 0x0d, 0xbd, 0x62, 0xb7, 0xbe, 0xb0, 0x5b, 0x99, 0xb0, 0xb0, 0x5b, 0x19, 0xd6, 0xd1,
+	0xb6, 0xd4, 0x6b, 0x76, 0xa3, 0x8d, 0x8b, 0x4b, 0xd8, 0x28, 0xb9, 0xb2, 0xee, 0x3f, 0xbb, 0xea,
+	0x2b, 0xcf, 0xaf, 0xfa, 0xca, 0x1f, 0x57, 0x7d, 0xe5, 0xfc, 0xba, 0xdf, 0x78, 0x7e, 0xdd, 0x6f,
+	0xfc, 0x7a, 0xdd, 0x6f, 0x3c, 0x7a, 0x23, 0x70, 0xe6, 0xff, 0x95, 0x04, 0x40, 0xe3, 0x75, 0x71,
+	0x0d, 0x77, 0xff, 0x09, 0x00, 0x00, 0xff, 0xff, 0x9d, 0x65, 0x4f, 0x26, 0x43, 0x09, 0x00, 0x00,
 }
 
 func (this *ProtoValidator) Equal(that interface{}) bool {
@@ -445,15 +442,20 @@ func (m *ProtoValidator) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	var l int
 	_ = l
 	if len(m.RewardDelegators) > 0 {
+		keysForRewardDelegators := make([]string, 0, len(m.RewardDelegators))
 		for k := range m.RewardDelegators {
-			v := m.RewardDelegators[k]
+			keysForRewardDelegators = append(keysForRewardDelegators, string(k))
+		}
+		github_com_cosmos_gogoproto_sortkeys.Strings(keysForRewardDelegators)
+		for iNdEx := len(keysForRewardDelegators) - 1; iNdEx >= 0; iNdEx-- {
+			v := m.RewardDelegators[string(keysForRewardDelegators[iNdEx])]
 			baseI := i
 			i = encodeVarintNodes(dAtA, i, uint64(v))
 			i--
 			dAtA[i] = 0x10
-			i -= len(k)
-			copy(dAtA[i:], k)
-			i = encodeVarintNodes(dAtA, i, uint64(len(k)))
+			i -= len(keysForRewardDelegators[iNdEx])
+			copy(dAtA[i:], keysForRewardDelegators[iNdEx])
+			i = encodeVarintNodes(dAtA, i, uint64(len(keysForRewardDelegators[iNdEx])))
 			i--
 			dAtA[i] = 0xa
 			i = encodeVarintNodes(dAtA, i, uint64(baseI-i))

x/nodes/keeper/validator_test.go
@@ -1,10 +1,15 @@
 package keeper
 
 import (
+	"encoding/hex"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"strconv"
 	"testing"
+	"time"
 
+	"github.com/pokt-network/pocket-core/crypto"
 	sdk "github.com/pokt-network/pocket-core/types"
 	"github.com/pokt-network/pocket-core/x/nodes/types"
 	"github.com/stretchr/testify/assert"
@@ -147,6 +152,70 @@ func Test_sortNoLongerStakedValidators(t *testing.T) {
 				t.Errorf("sortNoLongerStakedValidators() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+// Verifies the marshaled product of a validator is the same regardless of
+// the order of RewardDelegators.  If it is not, a key of the merkle tree in
+// application.db is not deterministic, that will cause consensus failure.
+func Test_Marshal_RewardDelegators(t *testing.T) {
+	numOfIterations := 100
+	numOfDelegators := 100
+
+	chains := []string{"0001", "0002", "005A", "005B", "005C", "005D"}
+	url := "https://test.pokt.network:443"
+	stake := int64(18000000000)
+	pubKey, _ := crypto.NewPublicKey("0b787e54e66b3db3a3396c2322d8314287e08990f7696c490153b078bada7e94")
+	nodeAddr := sdk.Address(pubKey.PubKey().Address())
+	outputAddr, _ := sdk.AddressFromHex("42846261e1798fc08e1dfd97325af7b280f815b0")
+
+	// Generate a bunch of random numbers to use as reward delegator addressees
+	randNums := make([]int, numOfDelegators)
+	for j := 0; j < numOfDelegators; j++ {
+		randNums[j] = rand.Int()
+	}
+
+	var valHash string
+
+	for i := 0; i < numOfIterations; i++ {
+		// Initialize `delegatorMap` with a different order of the pre-created
+		// random numbers.  In every iteration, the value of `delegatorMap` is the
+		// same, but the order of its range loop (`for k, v := range delegatorMap`)
+		// is usually impacted by the order of values inserted.
+		// This behavior is not guaranteed as https://go.dev/ref/spec#For_statements
+		// says "The iteration order over maps is not specified and is not
+		// guaranteed to be the same from one iteration to the next", but it's
+		// ok for testing purpose.
+		rand.Shuffle(len(randNums), func(i, j int) {
+			randNums[i], randNums[j] = randNums[j], randNums[i]
+		})
+		delegatorMap := map[string]uint32{}
+		for randNum := range randNums {
+			delegatorMap[strconv.Itoa(randNum)] = uint32(randNum % 10)
+		}
+
+		val := types.Validator{
+			Address:                 nodeAddr,
+			PublicKey:               pubKey,
+			Jailed:                  false,
+			Status:                  sdk.Staked,
+			Chains:                  chains,
+			UnstakingCompletionTime: time.Time{},
+			ServiceURL:              url,
+			StakedTokens:            sdk.NewInt(stake),
+			OutputAddress:           outputAddr,
+			RewardDelegators:        delegatorMap,
+		}
+		valBytes, err := val.Marshal()
+		assert.Nil(t, err)
+
+		// First test iteration when valhash hasn't been set yet
+		if len(valHash) == 0 {
+			valHash = hex.EncodeToString(valBytes)
+		}
+
+		// Make sure the hash is always the same regardless of the order of randNums
+		assert.Equal(t, hex.EncodeToString(valBytes), valHash)
 	}
 }
 